6f6b9fa7868933b62a2e3464242eae20afc0f9cc
giangi <giangi>
2007-07-12 10:44:52 +0000

    "aggiunta bozza form gestione Modulo Aree"

---
+++ bedita-app/vendors/_smartyPlugins/function.pr.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Smarty plugin
+ * -------------------------------------------------------------
+ * Type:     function
+ * Name:     dump
+ * Version:  1.0
+ * Author:   giangi
+ * Purpose:  implement pr functions
+ * Input:    var       =  Smarty var name
+ * -------------------------------------------------------------
+ */
+function smarty_function_pr($params, &$smarty)
+{
+	$vars = &$smarty->get_template_vars();
+	if (empty($params["var"])) return ;
+	
+	// Stampa
+	pr($params["var"]);
+}
+
+/* vim: set expandtab: */
+
+?>
