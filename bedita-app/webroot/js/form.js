a9df84a968bf2e46d18ab9702802758806cbcc0d
giangi <giangi>
2007-07-12 10:45:13 +0000

    "aggiunta bozza form gestione Modulo Aree"

---
+++ bedita-app/webroot/js/form.js
@@ -0,0 +1,17 @@
+/*
+Funzioni comuni ai form di tutti i moduli
+*/
+
+// Rende visibili tutti i blocchi di form messi dentro a DIV di classe 'blockForm'
+function showAllBlockPage() {
+	$("DIV[@class='blockForm']").show() ;
+	$("#openAllBlockLabel").hide() ;
+	$("#closeAllBlockLabel").show() ;
+}
+
+// Nasconde tutti i blocchi di form messi dentro a DIV di classe 'blockForm'
+function hideAllBlockPage() {
+	$("DIV.blockForm").hide() ;	
+	$("#closeAllBlockLabel").hide() ;
+	$("#openAllBlockLabel").show() ;
+}
