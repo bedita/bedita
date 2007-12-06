62afe2093b323d5c9ec937e40133e72eded5c3ca
didoda <dante@didoda.com>
2007-12-06 17:28:25 +0000

    patch per la visualizzazione dei tree area/section - in documents (ancora da finire)

--- bedita-app/webroot/js/module.documents.js
+++ bedita-app/webroot/js/module.documents.js
@@ -10,23 +10,12 @@ function designTree() {
 		speed: 'fast',
 		collapsed:false
 	});
-		
-	var url = "" ;
-	try {
-		url = URLBase ;
-	} catch(e) {
-		url = "/documents/index/" ;
-	}
-		
+				
 	$("li/span", "#tree").each(function(i){
 		// Preleva l'ID della sezione
 		var id = $("input[@name='id']", this.parentNode).eq(0).attr('value') ;
 	
 		// Crea il tag per il form
-		$(this).html('<a href="'+url+"id:"+id+'">'+$(this).html()+'</a>') ;
+		$(this).html('<label for="'+id+'">'+$(this).html()+'</label>') ;
 	});
 }
-
-
-
-
