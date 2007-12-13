3308b140c9ba07bb332d1993341fc9ed6b344d04
didoda <dante@didoda.com>
2007-12-13 14:03:17 +0000

    tolta funzione di yav (non utilizzata)

--- bedita-app/webroot/js/common.js
+++ bedita-app/webroot/js/common.js
@@ -1,18 +1,3 @@
-function checkOnSubmit(formName, r) {
-    document.getElementById(errorsdiv).className = '';
-    document.getElementById(errorsdiv).style.display = 'none';
-    /*var alertType = document.getElementById('alertType').value;*/
-    /*var alertType = 'innerHtml';*/
-    /*var alertType = 'jsVar';*/
-    var alertType = 'innerHtml';
-    
-    if (performCheck(formName, r, alertType)) {
-        document.forms[formName].submit();
-    } else if (alertType=='jsVar') {
-        alert('jsErrors variable contains the array of errors:\n\n' +jsErrors + '\n\n Use this variable like you prefer!');
-    }
-}
-
 jQuery.fn.extend({
   check: function() {
      return this.each(function() { this.checked = true; });
@@ -23,6 +8,4 @@ jQuery.fn.extend({
   toggleCheck: function() {
      return this.each(function() { this.checked = !this.checked ; });
    }
-   
 });
-
