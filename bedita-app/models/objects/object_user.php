bc7cc4a31734fdf9b575ad535e5a056cbb0d6970
stefanorosanelli <stefano.rosanelli@gmail.com>
2008-01-15 10:25:57 +0000

    risolto conflitto nomi models

---
+++ bedita-app/models/objects/object_user.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ *
+ * PHP versions 5
+ *
+ * CakePHP :  Rapid Development Framework <http://www.cakephp.org/>
+ * Copyright (c)	2006, Cake Software Foundation, Inc.
+ *								1785 E. Sahara Avenue, Suite 490-204
+ *								Las Vegas, Nevada 89104
+ *
+ * @filesource
+ * @copyright		Copyright (c) 2007
+ * @link			
+ * @package			
+ * @subpackage		
+ * @since			
+ * @version			
+ * @modifiedby		
+ * @lastmodified	
+ * @license
+ * @author 		ste ste@channelweb.it
+*/
+class ObjectUser extends BEAppModel
+{
+	var $name = 'ObjectUser';
+
+	var $actsAs     = array(
+            'CompactResult'         => array(),
+    );
+}
+?>
