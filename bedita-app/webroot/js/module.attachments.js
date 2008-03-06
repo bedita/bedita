b9f6a7ede74811175f5a8f8e2a0f0c1eec023c06
batopa <batopa@gmail.com>
2008-03-06 15:01:12 +0000

    tolto codice replicato

--- bedita-app/webroot/js/module.attachments.js
+++ bedita-app/webroot/js/module.attachments.js
@@ -1,32 +1,4 @@
-/*
-File con la logica utilizzata nel modulo attachments.
-
-@author		giangi@qwerg.com
+/**
+attachments custom js
+@author bato@channelweb.it 	
 */
-// Crea o refresh albero
-function designTree() {
-	$("#tree").Treeview({ 
-		control: false ,
-		speed: 'fast',
-		collapsed:false
-	});
-		
-	var url = "" ;
-	try {
-		url = URLBase ;
-	} catch(e) {
-		url = "/documents/index/" ;
-	}
-		
-	$("li/span", "#tree").each(function(i){
-		// Preleva l'ID della sezione
-		var id = $("input[@name='id']", this.parentNode).eq(0).attr('value') ;
-	
-		// Crea il tag per il form
-		$(this).html('<a href="'+url+"id:"+id+'">'+$(this).html()+'</a>') ;
-	});
-}
-
-
-
-
