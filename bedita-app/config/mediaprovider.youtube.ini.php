f8d9234f081895bd0288fd74d053eaa92134b26e
giangi <giangi>
2008-04-01 12:06:41 +0000

    "media provider, helper embed youtube"

---
+++ bedita-app/config/mediaprovider.youtube.ini.php
@@ -0,0 +1,23 @@
+<?php
+/**
+  * @author giangi@qwerg.com ste@channelweb.it
+ * 
+ *  constants and global variables for media provider
+ * 
+ */
+$config = array() ;
+ 
+$config['youtube'] = array(
+	"width"		=> 320,
+	"height"	=> 200,
+	
+	/**
+ 	* comment, assume default value
+ 	*/
+	// "rel"		=> 0,		// 0/1 altri video relativi a quello corrente (quando si seleziona menu). Default: 0
+	// "loop"	=> 0, 		// 0/1, default: 0
+	// "autoplay"	=> 0,		// 0/1. Default: 0
+	// "fmt"		=> 18,		// Migliora la qualitˆ. Default: empty
+) ;
+
+?>
