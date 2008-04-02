d905bfbf16c61136845a0124d3b532063417b8fb
xho <xhoxho@gmail.com>
2008-04-02 04:41:18 +0000

    image_info smarty plugin e uso in multimedia

---
+++ bedita-app/vendors/_smartyPlugins/function.image_info.php
@@ -0,0 +1,311 @@
+<?php
+
+/*
+ * Smarty plugin
+ * -------------------------------------------------------------
+ * Type:     function
+ * Name:     image_info
+ * Version:  1.0
+ * Author:   Christiano Presutti - aka xho - ChanelWeb srl
+ * Purpose:  get several information from an image file
+ * Input:    fileURI = string pointer to file - (yet to be made access file path on filesystem)
+ * Returns:  filename, w, h, hrtype, attr, mimetype, bits, channels [ todo: , exif as array () ]
+ * -------------------------------------------------------------
+ */
+function smarty_function_image_info ($params, &$smarty)
+{
+	$pluginName = "image_info";
+	$imageInfo	= array(
+		"filename"	=> "",
+		"w"			=> "",
+		"h"			=> "",
+		"hrtype"	=> "",
+		"attr"		=> "",
+		"mimetype"	=> "",
+		"bits"		=> "",
+		"channels"	=> "",
+		"exif"		=> array ()
+	);
+
+	extract($params);
+
+    if (empty($var))
+	{
+        $smarty->trigger_error($pluginName . ": missing 'var' parameter");
+        return;
+    }
+
+	if ( empty($file) )
+	{
+		$smarty->trigger_error ($pluginName . ": missing 'file' parameter", E_USER_NOTICE);
+		return;
+	}
+
+
+	/*
+	 *  File is on remote server, local server or local FileSystem (to do local filesystem recognition)
+	 */
+    if ( substr($file, 0, 6) == 'http:' || substr($file, 0, 6) == 'https:' )
+	{
+		$_image_path_type		= "remote";
+    } else {
+		$_image_path_type		= "local";
+    }
+	/* FS to do
+	{
+		$_image_path_type		= "filesystem";
+		$_image_path			= rawurlencode( $file ); ?? right ??
+		$imageInfo ['filename']	= basename($path);
+	}
+	*/
+
+
+	// sanitize file path & name (why does not work?)
+	$_image_path			= $file ; // rawurlencode( $file );
+
+
+	/*
+	 *  Get data or trigger errors
+	 */	
+	if ( !$_image_data = @getimagesize($_image_path) )
+	{
+		if ( !file_exists($_image_path) )
+		{
+			$smarty -> trigger_error ( $pluginName . ": unable to find '$_image_path'", E_USER_NOTICE ) ;
+			return;
+		}
+		else if ( !is_readable($_image_path) )
+		{
+			$smarty -> trigger_error ( $pluginName . ": unable to read '$_image_path'", E_USER_NOTICE ) ;
+			return;
+		}
+		else
+		{
+			$smarty -> trigger_error ( $pluginName . ": '$_image_path' is not a valid image file", E_USER_NOTICE ) ;
+			return;
+		}
+	}
+
+
+	/*
+	 * EXIF build up
+	 */
+	if ( _getHumanReadableType ( $_image_data[2] ) == "JPG" )
+	{
+		$exifRawData	= _raw_extract_exif ( $_image_path );
+	}
+
+	if ( $exifXMP = @_ee_extract_exif_from_pscs_xmp ( $_image_path ) )
+	{
+		$exifRawData ['XMP'] = $exifXMP;
+	}
+
+
+
+	/*
+	 * build up array
+	 */
+	$_path					= parse_url ( $_image_path, PHP_URL_PATH );
+	$imageInfo['filename']	= end ( explode ( '/', $_path ) );
+	$imageInfo["filesize"]	= ( $_image_path_type == "filesystem" )? _getHumanReadableFileSize ( $_image_path ) : '';
+	$imageInfo["w"]			= $_image_data [0];
+	$imageInfo["h"]			= $_image_data [1];
+	$imageInfo["hrtype"]	= _getHumanReadableType ( ( $_image_data[2] ) );
+	$imageInfo["attr"]		= $_image_data [3];
+	$imageInfo["mimetype"]	= $_image_data ['mime'];
+	$imageInfo["bits"]		= $_image_data ['bits'];
+	$imageInfo["channels"]	= $_image_data ['channels'];
+	$imageInfo["exif"]		= $exifRawData;
+
+	// assign to defined var
+ 	$smarty -> assign ( $var, $imageInfo );
+
+}
+
+
+
+
+/*
+ * helper functions
+ */
+
+// system supported image types
+function _getSupportedImageTypes ()
+{
+    $aSupportedTypes = array();
+
+    $aPossibleImageTypeBits = array (
+        IMG_GIF		=> 'GIF',
+        IMG_JPG		=> 'JPG',
+        IMG_PNG		=> 'PNG',
+        IMG_WBMP	=> 'WBMP'
+    );
+
+    foreach ( $aPossibleImageTypeBits as $iImageTypeBits => $sImageTypeString )
+	{
+        if (imagetypes() & $iImageTypeBits)
+		{
+            $aSupportedTypes[] = $sImageTypeString;
+        }
+    }
+
+    return $aSupportedTypes;
+}
+
+
+// file size
+function _getHumanReadableFileSize ( $file )
+{
+	return $file_size = array_reduce (
+		array (" B", " KB", " MB"), create_function (
+			'$a, $b', 'return is_numeric ($a)? ($a >= 1024 ? $a / 1024 : number_format ($a, 2) . $b) : $a;'
+		), filesize ( $file )
+	);
+}
+
+
+// function _getHumanReadableType
+function _getHumanReadableType ( $type )
+{
+    $types = array (
+		0 => '',
+        1 => 'GIF',
+        2 => 'JPG',
+        3 => 'PNG',
+        4 => 'SWF',
+        5 => 'PSD',
+        6 => 'BMP',
+        7 => 'TIFF (intel byte order)',
+        8 => 'TIFF (motorola byte order)',
+        9 => 'JPC',
+        10 => 'JP2',
+        11 => 'JPX',
+        12 => 'JB2',
+        13 => 'SWC',
+        14 => 'IFF',
+        15 => 'WBMP',
+        16 => 'XBM'
+    );
+	
+	return $types [ $type ] ;	
+}
+
+
+
+/*
+ * EXIF funcs
+ */
+
+
+// raw exif read through PHP exif_read_data
+function _raw_extract_exif ( $file )
+{
+	$exif = exif_read_data( $file, 'IFD0');
+	if ( $exif===false ) return false; // "No header data found in image file.";
+
+	$exif = exif_read_data( $file, 0, true);
+
+	$exif["main"] = @ array (
+		"FileName"				=> $exif ['FILE']['FileName'],
+		"FileSize"				=> $exif ['FILE']['FileSize'],
+		"MimeType"				=> $exif ['FILE']['MimeType'],
+		"Orientation"			=> ($exif ['IFD0']['Orientation'])? "landscape" : "portrait",
+		"Color"					=> ($exif ['COMPUTED']['IsColor'])? "color" : "b/n",
+		"Copyright"				=> $exif ['COMPUTED']['Copyright'],
+		"Copyright (IFD0)"		=> $exif ['IFD0']['Copyright'],
+		"ShotDate"				=> $exif ['IFD0']['DateTime'],
+		"CaptureDeviceBrand"	=> $exif ['IFD0']['Make'],
+		"CaptureDeviceModel"	=> $exif ['IFD0']['Model'],
+		"CCDWidth"				=> $exif ['COMPUTED']['CCDWidth'],
+		"ApertureFNumber"		=> $exif ['COMPUTED']['ApertureFNumber'],
+		"ExposureTime"			=> $exif ['EXIF']['ExposureTime'],
+		"ShutterSpeed"			=> $exif ['EXIF']['ShutterSpeedValue'],
+		"Aperture"				=> $exif ['EXIF']['ApertureValue'],
+		"FNumber"				=> $exif ['EXIF']['FNumber'],
+		"ISO"					=> $exif ['EXIF']['ISOSpeedRatings'],
+		"FocalLength"			=> $exif ['EXIF']['FocalLength'],
+		"Width"					=> $exif ['EXIF']['ExifImageWidth'],
+		"Height"				=> $exif ['EXIF']['ExifImageLength'],
+	);
+	
+	return $exif;
+
+	/*
+	 * printout
+	 * 
+	echo "exif:<br />\n";
+	foreach ($exif as $key => $section) {
+		foreach ($section as $name => $val) {
+			echo "$key.$name: $val<br />\n";
+		}
+	}
+	*/
+}
+
+
+// EXIF from Photoshop CS XMP
+// example: $xmp_parsed = ee_extract_exif_from_pscs_xmp ("CRW_0016b_preview.jpg");
+function _ee_extract_exif_from_pscs_xmp ( $file ) {
+
+	// very straightforward one-purpose utility function which
+	// reads image data and gets some EXIF data (a few) out from its XMP tags (by Adobe Photoshop CS)
+	// returns an array with values
+	// inspired by code by Pekka Saarinen http://photography-on-the.net
+
+	ob_start();
+	readfile($filename);
+	$source = ob_get_contents();
+	ob_end_clean();
+
+	$xmpdata_start = strpos($source,"<x:xmpmeta");
+	$xmpdata_end = strpos($source,"</x:xmpmeta>");
+	$xmplenght = $xmpdata_end-$xmpdata_start;
+	$xmpdata = substr($source,$xmpdata_start,$xmplenght+12);
+	$xmp_parsed = array();
+
+	$regexps = array(
+		array ("name" => "DC creator", "regexp" => "/<dc:creator>\s*<rdf:Seq>\s*<rdf:li>.+<\/rdf:li>\s*<\/rdf:Seq>\s*<\/dc:creator>/"),
+		array ("name" => "TIFF camera model", "regexp" => "/<tiff:Model>.+<\/tiff:Model>/"),
+		array ("name" => "TIFF maker", "regexp" => "/<tiff:Make>.+<\/tiff:Make>/"),
+		array ("name" => "EXIF exposure time", "regexp" => "/<exif:ExposureTime>.+<\/exif:ExposureTime>/"),
+		array ("name" => "EXIF f number", "regexp" => "/<exif:FNumber>.+<\/exif:FNumber>/"),
+		array ("name" => "EXIF aperture value", "regexp" => "/<exif:ApertureValue>.+<\/exif:ApertureValue>/"),
+		array ("name" => "EXIF exposure program", "regexp" => "/<exif:ExposureProgram>.+<\/exif:ExposureProgram>/"),
+		array ("name" => "EXIF iso speed ratings", "regexp" => "/<exif:ISOSpeedRatings>\s*<rdf:Seq>\s*<rdf:li>.+<\/rdf:li>\s*<\/rdf:Seq>\s*<\/exif:ISOSpeedRatings>/"),
+		array ("name" => "EXIF datetime original", "regexp" => "/<exif:DateTimeOriginal>.+<\/exif:DateTimeOriginal>/"),
+		array ("name" => "EXIF exposure bias value", "regexp" => "/<exif:ExposureBiasValue>.+<\/exif:ExposureBiasValue>/"),
+		array ("name" => "EXIF metering mode", "regexp" => "/<exif:MeteringMode>.+<\/exif:MeteringMode>/"),
+		array ("name" => "EXIF focal lenght", "regexp" => "/<exif:FocalLength>.+<\/exif:FocalLength>/"),
+		array ("name" => "AUX lens", "regexp" => "/<aux:Lens>.+<\/aux:Lens>/")
+	);
+
+    foreach ( $regexps as $key => $k )
+	{
+		$name	= $k["name"];
+		$regexp	= $k["regexp"];
+		unset ($r);
+		preg_match ($regexp, $xmpdata, $r);
+		$xmp_item = "";
+		$xmp_item = @$r[0];
+		array_push ( $xmp_parsed, array ("item" => $name, "value" => $xmp_item ) );
+	}
+
+	/*
+	 * printout
+	 * 
+		echo "XML lenght: " . $xmplenght;
+
+		foreach ($xmp_parsed as $key => $k)
+		{
+			$item	= $k["item"];
+			$value	= $k["value"];
+			print "<br><b>" . $item . ":</b> " . $value;
+		}
+	 */
+
+	return ($xmp_parsed);
+
+}
+
+
+?>
--- bedita-app/views/pages/form_file.tpl
+++ bedita-app/views/pages/form_file.tpl
@@ -2,20 +2,19 @@
 <div class="blockForm" style="display:block" id="multimediaitem">
 <fieldset>
 {if (isset($object))}
-	<table class="tableForm" border="0">
-	<tr>
-		<td rowspan="3">
+
+<div style="width: 210px; float: left;">
 {if ($object.ObjectType.name == "image")}
-{assign var="thumbWidth" 		value = 200}
-{assign var="thumbHeight" 		value = 200}
-{assign var="filePath"			value = $object.path}
-{assign var="fileName"			value = $object.filename|default:$obj.name}
-{assign var="fileTitle"			value = $object.title}
-{assign var="mediaPath"         value = $conf->mediaRoot}
-{assign var="mediaUrl"          value = $conf->mediaUrl}
-{assign_concat var="imageAltAttribute"	0="alt='"  1=$object.title 2="'"}
-{assign_concat var="mediaCacheBaseURL"	0=$conf->mediaUrl  1="/" 2=$conf->imgCache 3="/"}
-{assign_concat var="mediaCachePATH"		0=$conf->mediaRoot 1=$conf->DS 2=$conf->imgCache 3=$conf->DS}
+	{assign var="thumbWidth" 		value = 200}
+	{assign var="thumbHeight" 		value = 200}
+	{assign var="filePath"			value = $object.path}
+	{assign var="fileName"			value = $object.filename|default:$obj.name}
+	{assign var="fileTitle"			value = $object.title}
+	{assign var="mediaPath"         value = $conf->mediaRoot}
+	{assign var="mediaUrl"          value = $conf->mediaUrl}
+	{assign_concat var="imageAltAttribute"	0="alt='"  1=$object.title 2="'"}
+	{assign_concat var="mediaCacheBaseURL"	0=$conf->mediaUrl  1="/" 2=$conf->imgCache 3="/"}
+	{assign_concat var="mediaCachePATH"		0=$conf->mediaRoot 1=$conf->DS 2=$conf->imgCache 3=$conf->DS}
 
 	{thumb 
 		width			= $thumbWidth
@@ -28,25 +27,43 @@
 		html			= $imageAltAttribute
 		frame			= ""
 	}
+
 {elseif ($object.provider|default:false)}
 	{assign_associative var="attributes" style="width:30px;heigth:30px;"}
+
 	<a href="{$object.path}" target="_blank">
-	{$mediaProvider->thumbnail($object, $attributes) }
-           </a>
+		{$mediaProvider->thumbnail($object, $attributes) }
+	</a>
 	
 {else}
 	<a href="{$conf->mediaUrl}{$object.path}" target="_blank">
-           <img src="{$session->webroot}img/mime/{$object.type}.gif" />
-           </a>
+		<img src="{$session->webroot}img/mime/{$object.type}.gif" />
+	</a>
 {/if}
-		</td>
-		<td class="label">{t}File name{/t}:</td><td>{$object.name|default:""}</td>
-	</tr>
-	<tr><td class="label">{t}File type{/t}:</td><td>{$object.type|default:""}</td></tr>
-	<tr><td class="label">{t}File size{/t}:</td><td>{math equation="x/y" x=$object.size|default:0 y=1024 format="%d"|default:""} KB</td></tr>
-	</table>
+
+</div>
+
+{assign_concat var="fileUrl"  0=$conf->mediaUrl  1=$object.path}
+{image_info var="imageInfo" file=$fileUrl}
+
+<div style="line-height: 1.6em;">
+	<span class="label">{t}Name{/t}:</span> {$object.name|default:""}<br />
+	<span class="label">{t}Human readable type{/t}:</span> {$imageInfo.hrtype}<br />
+	<span class="label">{t}Mime type{/t}:</span> {$object.type|default:""}<br />
+	<span class="label">{t}Size{/t}:</span> {math equation="x/y" x=$object.size|default:0 y=1024 format="%d"|default:""} KB<br />
+	<span class="label">{t}Width{/t}:</span> {$imageInfo.w}<br />
+	<span class="label">{t}Height{/t}:</span> {$imageInfo.h}<br />
+	<span class="label">{t}Bit depth{/t}:</span> {$imageInfo.bits}<br />
+	<span class="label">{t}Channels{/t}:</span> {$imageInfo.channels}
+</div>
+
+<div style="line-height: 1.4em; clear: left;">
+	<h3>Main EXIF data</h3>
+	{foreach from=$imageInfo.exif.main item="value" key="key"}
+		<span class="label">{$key}</span>: {$value}<br />
+	{/foreach}
+</div>
 {/if}
-<br>
 
 </fieldset>
 </div>
