1fb5226b730672c5d51238bd34857b052441f0fc
didoda <dante@didoda.com>
2008-06-05 08:55:51 +0000

    fix per import/export, con php5 versione minore di 5.2.1 (funzione sys_get_temp_dir)

--- bedita-app/vendors/shells/bedita.php
+++ bedita-app/vendors/shells/bedita.php
@@ -234,7 +234,7 @@ class BeditaShell extends Shell {
 	        $this->out("SQL_SCRIPT_PATH has to be defined in ".APP_DIR."/config/database.php");
 			return;
 		}
-
+		$this->check_sys_get_temp_dir();
 		$tmpBasePath = $this->setupTempDir();
        	$this->out("Using temp dir: $tmpBasePath");
 		
@@ -333,7 +333,7 @@ class BeditaShell extends Shell {
 
 		$dbDump = new DbDump();
 		$tables = $dbDump->tableList();
-		
+		$this->check_sys_get_temp_dir();
 		$tmpBasePath = $this->setupTempDir();
 		$sqlFileName = $tmpBasePath."bedita-data.sql";
 		
@@ -383,7 +383,48 @@ class BeditaShell extends Shell {
 		$this->cleanTempDir();
         $this->out("$expFile created");
     }
-    
+
+	private function check_sys_get_temp_dir() {
+		if ( !function_exists('sys_get_temp_dir') ) {
+		    // Based on http://www.phpit.net/
+		    // article/creating-zip-tar-archives-dynamically-php/2/
+		    function sys_get_temp_dir()
+		    {
+		        // Try to get from environment variable
+		        if ( !empty($_ENV['TMP']) )
+		        {
+		            return realpath( $_ENV['TMP'] );
+		        }
+		        else if ( !empty($_ENV['TMPDIR']) )
+		        {
+		            return realpath( $_ENV['TMPDIR'] );
+		        }
+		        else if ( !empty($_ENV['TEMP']) )
+		        {
+		            return realpath( $_ENV['TEMP'] );
+		        }
+		
+		        // Detect by creating a temporary file
+		        else
+		        {
+		            // Try to use system's temporary directory
+		            // as random name shouldn't exist
+		            $temp_file = tempnam( md5(uniqid(rand(), TRUE)), '' );
+		            if ( $temp_file )
+		            {
+		                $temp_dir = realpath( dirname($temp_file) );
+		                unlink( $temp_file );
+		                return $temp_dir;
+		            }
+		            else
+		            {
+		                return FALSE;
+		            }
+		        }
+		    }
+		}
+	}
+
     private function setupTempDir() {
     	$basePath = sys_get_temp_dir().DS."bedita-export-tmp".DS;
 		if(!is_dir($basePath)) {
@@ -402,6 +443,7 @@ class BeditaShell extends Shell {
 			throw new Exception("Error deleting dir $exportPath");
         }
     }
+	
     
     private function extractMediaZip($zipFile) {
 		$zip = new ZipArchive;
