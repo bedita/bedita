198e76b40706b6bfc8b3453577fe557e22106567
stefanorosanelli <stefano.rosanelli@gmail.com>
2008-07-22 10:13:59 +0000

    script dbadmin checkLangStatus - aggiorna status assenti in lang_texts

--- bedita-app/models/objects/lang_text.php
+++ bedita-app/models/objects/lang_text.php
@@ -1,13 +1,6 @@
 <?php
 /**
  *
- * PHP versions 5
- *
- * CakePHP :  Rapid Development Framework <http://www.cakephp.org/>
- * Copyright (c)	2006, Cake Software Foundation, Inc.
- *								1785 E. Sahara Avenue, Suite 490-204
- *								Las Vegas, Nevada 89104
- *
  * @filesource
  * @copyright		Copyright (c) 2007
  * @link			
@@ -15,21 +8,16 @@
  * @subpackage		
  * @since			
  * @version			
- * @modifiedby		
- * @lastmodified	
  * @license
- * @author 		giangi giangi@qwerg.com			
+ * @author 		giangi giangi@qwerg.com	 ste ste@channelweb.it		
 */
 class LangText extends BEAppModel
 {
-	var $name = 'LangText';
-
 	var $belongsTo = array(
 		'BEObject' =>
 			array(
-				'className'		=> 'BEObject',
-				'fields'		=> 'id',
-				'foreignKey'	=> 'id'
+				'fields'	=> 'id, status',
+				'foreignKey'	=> 'object_id'
 			)
 	);
 
--- bedita-app/vendors/shells/dbadmin.php
+++ bedita-app/vendors/shells/dbadmin.php
@@ -1,7 +1,5 @@
 <?php
 
-App::import("Model", "BEObject");
-App::import("Model", "SearchText");
 App::import('Core', 'String');
 App::import('Core', 'Controller');
 App::import('Controller', 'App'); // BeditaException
@@ -11,8 +9,8 @@ class DbadminShell extends Shell {
 	public function rebuildIndex() {
 		
 		$conf = Configure::getInstance();
-		$searchText = new SearchText();
-		$beObj = new BEObject();
+		$searchText = ClassRegistry::init("SearchText");
+		$beObj = ClassRegistry::init("BEObject");
 		$types = array('Event', 'Document', 'ShortNews');		
 
 		foreach ($types as $t) {
@@ -24,13 +22,65 @@ class DbadminShell extends Shell {
 			$res = $beObj->findObjs(null, null, array($objTypeId));
 			foreach ($res['items'] as $o) {
 				$model->id = $o['id'];
-				$searchText = new SearchText();
+				$searchText = ClassRegistry::init("SearchText");
 				$searchText->deleteAll("object_id=".$model->id);
 				$searchText->createSearchText($model);
 			}
 			$this->out("Objects of type $t indexed");
 		}		
 		$this->out("Search text index build");
+	}
+	
+	/**
+	 * update lang texts 'status' using master object status....
+	 * parameter 'lang' mandatory
+	 */
+	public function checkLangStatus() {
+		
+		$lang = $this->params['lang'];
+		if(empty($lang)) {
+			$this->out("Language parameter -lang mandatory");
+			return;
+		}
+		$this->out('Checking language: '.$lang);
+		$langText = ClassRegistry::init("LangText");
+		$objTrans = $langText->find('all', 
+			array('conditions'=> array("LangText.lang = '$lang'","LangText.name = 'title'")
+			,'fields'=>array('BEObject.id', 'BEObject.status')));
+		if(empty($objTrans)) {
+			$this->out("No translations found");
+			return;
+		}
+		foreach ($objTrans as $obj) {
+			$objId = $obj['BEObject']['id'];
+			$status = $langText->find(array("LangText.name = 'status'", "LangText.lang = '$lang'", 
+				"LangText.object_id = $objId"));
+			if($status === false) {
+				$newStatus = $obj['BEObject']['status'];
+				$l = array(
+		                'object_id' => $objId,
+		                'lang'      => $lang, 
+		                'name'   => 'status',
+						'text' => $newStatus
+	                );
+	            $langText->create();
+	            if(!$langText->save($l)) 
+	                    throw new BeditaException("Error saving lang text");
+				$this->out("Added lang status for obj: $objId - $lang");
+			}
+		}
+		$this->out("Lang texts status updated");
+	}
+
+	function help() {
+ 	
+		$this->out('Available functions:');
+        $this->out('1. rebuildIndex: rebuild search texts index');
+  		$this->out(' ');
+ 		$this->out("2. checkLangStatus: update lang texts 'status' using master object status");
+        $this->out(' ');
+        $this->out('    Usage: checkLangStatus -lang <lang>');
+        $this->out(' ');
 	}
 	
 }
