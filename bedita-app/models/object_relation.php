dd9350140e8f9596b194fad672f303c71b163dd9
stefanorosanelli <stefano.rosanelli@gmail.com>
2008-09-16 10:56:20 +0000

    fix relazioni libere

---
+++ bedita-app/models/object_relation.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @filesource
+ * @copyright		Copyright (c) 2008, Channelweb - Chialab
+ * @link			
+ * @package			
+ * @subpackage		
+ * @since			
+ * @version			
+ * @modifiedby		
+ * @lastmodified	
+ * @license
+ * @author 		ste ste@channelweb.it
+*/
+class ObjectRelation extends BEAppModel
+{
+}
+?>
