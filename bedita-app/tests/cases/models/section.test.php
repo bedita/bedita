e15d9b9ded5d92e652ccb3f647daf5e500cebcdb
stefanorosanelli <stefano.rosanelli@gmail.com>
2008-09-30 10:04:30 +0000

    unit test feed disponibili

--- bedita-app/tests/cases/models/section.test.php
+++ bedita-app/tests/cases/models/section.test.php
@@ -8,7 +8,7 @@ require_once ROOT . DS . APP_DIR. DS. 'tests'. DS . 'bedita_base.test.php';
 
 class SectionTestCase extends BeditaTestCase {
 
- 	var $uses		= array('BEObject','Section') ;
+ 	var $uses		= array('BEObject','Section', 'Tree') ;
     var $dataSource	= 'default' ;	
 
     /////////////////////////////////////////////////
@@ -17,9 +17,16 @@ class SectionTestCase extends BeditaTestCase {
  	
  	function testFeeds() {
 
- 		$result = $this->Section->feedsAvailable();
-		pr("Available feeds:");
- 		pr($result);
+ 		$conf = Configure::getInstance();
+		$tree = $this->Tree->getAll(null, null, null, array($conf->objectTypes['area']['id'])) ;
+ 		
+		foreach ($tree as $area) {
+			pr("Publication: ". $area['id'] . " - ". $area['title']);
+			$result = $this->Section->feedsAvailable($area['id']);
+			pr("Available feeds:");
+	 		pr($result);
+		}
+		
  	} 
 
  	
