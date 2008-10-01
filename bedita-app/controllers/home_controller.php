528d4435dc09b2c4e5c3be4d42afff5684b449dc
stefanorosanelli <stefano.rosanelli@gmail.com>
2008-10-01 10:23:33 +0000

    test svn keywords

--- bedita-app/controllers/home_controller.php
+++ bedita-app/controllers/home_controller.php
@@ -1,24 +1,28 @@
 <?php
-
 /**
- *
- * @filesource
- * @copyright		
- * @link			
- * @package			
- * @subpackage		
- * @since			
- * @version			
- * @modifiedby		
- * @lastmodified	
- * @license			
- * @author
- */
-
-/**
- * Home
+ * BEdita - a semantic content management framework
+ * Copyright 2008 ChannelWeb Srl, Chialab Srl
  * 
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the Affero GNU General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ * 
+ * BEdita is distributed WITHOUT ANY WARRANTY; without even the implied 
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ * See the Affero GNU General Public License for more details.
+ * 
+ * You should have received a copy of the Affero GNU General Public License 
+ * version 3 along with BEdita.  If not, see <http://gnu.org/licenses/agpl-3.0.html>.
+ * 
+ * @link			http://www.bedita.com - BEdita project
+ * @version			$Revision$
+ * @modifiedby		$Author$
+ * @lastmodified	$Date$
+ * 
+ * $Id$
  */
+
 class HomeController extends AppController {
 	
 	var $uses = "BEObject";
