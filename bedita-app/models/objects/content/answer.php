826c87b026f1b6a5e8810797359645e34abef4bc
stefanorosanelli <stefano.rosanelli@gmail.com>
2008-12-16 16:39:09 +0000

    ripulitura models, part 2

--- bedita-app/models/objects/content/answer.php
+++ bedita-app/models/objects/content/answer.php
@@ -30,23 +30,6 @@
  */
 class Answer extends BeditaContentModel
 {
-	var $actsAs 	= array(
-			'CompactResult' 		=> array('Question'),
-			'ForeignDependenceSave' => array('BEObject', 'Content'),
-			'DeleteObject' 			=> 'objects',
-	); 
-
-	var $validate = array(
-		'question_id' 	=> array(array('rule' => VALID_NOT_EMPTY, 'required' => true)),
-	) ;
-
-	var $belongsTo = array(
-		'Question' =>
-			array(
-				'className'		=> 'Question',
-				'foreignKey'	=> 'question_id',
-				'conditions'	=> ''
-			),
-	) ;
+	var $useTable = 'contents';	
 }
 ?>
--- bedita-app/models/objects/content/faq_question.php
+++ bedita-app/models/objects/content/faq_question.php
@@ -30,5 +30,6 @@
  */
 class FaqQuestion extends BeditaContentModel
 {
+	var $useTable = 'contents';	
 }
 ?>
