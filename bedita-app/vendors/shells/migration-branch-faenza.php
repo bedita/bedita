6dddafd7257330076090881f35242d4f86f24993
batopa <batopa@gmail.com>
2008-12-29 16:59:36 +0000

    completato file di migrazione con gallery event e altro

--- bedita-app/vendors/shells/migration-branch-faenza.php
+++ bedita-app/vendors/shells/migration-branch-faenza.php
@@ -34,11 +34,15 @@ var $methodsQueries = array(
 	
 	"documents" => "select ob.*, cb.*, c.*, bd.*, d.* 
 				from objects ob, content_bases cb, contents c, base_documents bd, documents d 
-			where cb.id=ob.id AND c.id=ob.id 
-			AND bd.id=ob.id  AND d.id=ob.id 
-			AND ob.object_type_id=22",
+				where cb.id=ob.id AND c.id=ob.id 
+				AND bd.id=ob.id  AND d.id=ob.id 
+				AND ob.object_type_id=22",
 	"shortNews" => "select ob.*, cb.*, s.* from objects ob, content_bases cb, short_news s 
 			where cb.id=ob.id AND s.id=ob.id AND ob.object_type_id=18",
+	"events" => "select ob.*, cb.*, c.*, e.* 
+				from objects ob, content_bases cb, contents c, events e 
+				where cb.id=ob.id AND c.id=ob.id AND e.id=ob.id AND ob.object_type_id=21",
+	"dateItems" => "select edi.* from event_date_items edi",
 	"objectRelations" => "select cbo.* from content_bases_objects cbo",
 	"categories" => "select oc.* from object_categories oc",
 	"contentObjectCategories" => "select cboc.* from content_bases_object_categories cboc",
@@ -52,9 +56,13 @@ var $methodsQueries = array(
 			where cb.id=ob.id AND st.id=ob.id AND v.id=ob.id AND ob.object_type_id=32",
 	"files" => "select ob.*, cb.*, st.*, f.* from objects ob, content_bases cb, streams st, files f 
 			where cb.id=ob.id AND st.id=ob.id AND f.id=ob.id AND ob.object_type_id=10",
+	"audio" => "select ob.*, cb.*, st.*, a.* from objects ob, content_bases cb, streams st, audio a 
+			where cb.id=ob.id AND st.id=ob.id AND a.id=ob.id AND ob.object_type_id=31",
 	"modules" => "select mo.* from modules mo",
 	"langTexts" => "select lt.* from lang_texts lt",
-	"copy" => "event_logs groups groups_users lang_texts object_types permissions permission_modules question_types trees users"
+	"galleries" => "select ob.*, co.* FROM objects ob, collections co where ob.id=co.id AND ob.object_type_id=29",
+	"trees" => "select tr.* FROM trees tr, objects ob WHERE tr.id=ob.id AND ob.object_type_id <> 29",
+	"copy" => "event_logs groups groups_users object_types permissions permission_modules users"
 );
 
 	
@@ -68,7 +76,7 @@ var $methodsQueries = array(
 		unset($r['ob']['fundo']);		
 		$this->write($this->createInsert($r['ob'], "objects"));
 		$contents = array_merge($r['cb'], $r['c']);
-		$contents = array_merge($contents, $r['bd']);
+//		$contents = array_merge($contents, $r['bd']);
 		$this->write($this->createInsert($contents, "contents"));
 	}
 	
@@ -77,7 +85,21 @@ var $methodsQueries = array(
 		$this->write($this->createInsert($r['ob'], "objects"));
 		$this->write($this->createInsert($r['cb'], "contents"));
 	}
+	
+	protected function events($r) {
+		unset($r['ob']['fundo']);		
+		$this->write($this->createInsert($r['ob'], "objects"));
+		$contents = array_merge($r['cb'], $r['c']);
+		$this->write($this->createInsert($contents, "contents"));
+	}
 
+	protected function dateItems($r) {
+		unset($r["edi"]["id"]);
+		$r["object_id"] = $r["edi"]["event_id"];
+		unset($r["edi"]["event_id"]);
+		$this->write($this->createInsert($r['edi'],"date_items"));
+	}
+	
 	protected function areas($r) {
 		$r['ob']['creator'] = $r['a']['creator'];
 		$r['ob']['publisher'] = $r['a']['publisher'];
@@ -123,11 +145,21 @@ var $methodsQueries = array(
 		$this->write($this->createInsert($r['st'], "streams"));
 	}
 	
+	protected function audio($r) {
+		unset($r['ob']['fundo']);		
+		$this->write($this->createInsert($r['ob'], "objects"));
+		$this->write($this->createInsert($r['cb'], "contents"));
+		$r['st']['mime_type'] = $r['st']['type'];
+		unset($r['st']['type']);		
+		$this->write($this->createInsert($r['st'], "streams"));
+	}
+	
 	protected function objectRelations($r) {
 		$this->write($this->createInsert($r['cbo'], "object_relations"));
 	}
 	
 	protected function categories($r) {
+		$r['oc']['name'] = $r['oc']['label']; 
 		$this->write($this->createInsert($r['oc'], "categories"));
 	}
 	
@@ -146,11 +178,45 @@ var $methodsQueries = array(
 	}
 
 	protected function langTexts($r) {
-		if(empty($r['lt']['text'])) {
-			$r['lt']['text'] = $r['lt']['long_text'];
+		$beObjectModel = ClassRegistry::init("BEObject");
+		$langObj = $beObjectModel->field("lang", array("id" => $r["lt"]["object_id"]));
+		// do not insert main object language in lang_texts 
+		if ($langObj != $r["lt"]["lang"]) {
+			if(empty($r['lt']['text'])) {
+				$r['lt']['text'] = $r['lt']['long_text'];
+			}
+			unset($r['lt']['long_text']);		
+			$this->write($this->createInsert($r['lt'], "lang_texts"));
 		}
-		unset($r['lt']['long_text']);		
-		$this->write($this->createInsert($r['lt'], "lang_texts"));
+	}
+	
+	protected function galleries($r) {
+		unset($r['ob']['fundo']);
+		$this->write($this->createInsert($r['ob'], "objects"));
+		$this->write($this->createInsert(array('id' => $r['co']['id']), "contents"));
+	}
+	
+	protected function trees($r) {
+		$beObjectModel = ClassRegistry::init("BEObject");
+		$galleryCount = $beObjectModel->find("count", array(
+				"conditions" => array("id" => $r["tr"]["parent_id"], "object_type_id" => 29),
+				"contain" => array()
+			)
+		);
+		
+		// if it's not a gallery insert in trees table, else build attach relation
+		if ($galleryCount == 0) {
+			$this->write($this->createInsert($r["tr"], "trees"));
+		} else {
+			$obr["id"] = $r["tr"]["id"];
+			$obr["object_id"] = $r["tr"]["parent_id"];
+			$obr["switch"] = "attach";
+			$obr["priority"] = $r["tr"]["priority"];
+			$this->write($this->createInsert($obr, "object_relations"));
+			$obr["id"] = $r["tr"]["parent_id"];
+			$obr["object_id"] = $r["tr"]["id"];
+			$this->write($this->createInsert($obr, "object_relations"));
+		}
 	}
 	
 };
