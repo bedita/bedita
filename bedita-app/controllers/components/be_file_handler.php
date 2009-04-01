6fdc9a0cf3f822fed24994dbe24fe53c481515ed
batopa <batopa@gmail.com>
2009-04-01 10:49:16 +0000

    magic file in bedita + gestione

--- bedita-app/controllers/components/be_file_handler.php
+++ bedita-app/controllers/components/be_file_handler.php
@@ -400,24 +400,27 @@ class BeFileHandlerComponent extends Object {
 	}
 	
 	public function getMimeType($data) {
-		if (function_exists('finfo_file')) {
-			$handle = finfo_open(FILEINFO_MIME);
-			$mime_type = finfo_file($handle,$data["tmp_name"]);
-		} elseif (function_exists('mime_content_type')) {
-			// deprecated function
-			$mime_type = mime_content_type($data["tmp_name"]);
+		if (function_exists("finfo_open")) {
+			$file_info = finfo_open(FILEINFO_MIME, APP_PATH.'config'.DS.'magic');
+			$mime_type = ($file_info)? finfo_file($file_info,$data["tmp_name"]) : $this->getMimeTypeByExtension($data["name"]);
 		} else {
-			include_once APP_PATH.'config'.DS.'mime.types.php';
-			$extension = strtolower( substr( $data["name"], strrpos($data["name"],".")+1 ) );
-			if (!empty($extension) && array_key_exists($extension,$config["mimeTypes"])) {
-				$mime_type = $config["mimeTypes"][$extension];
-			}
+			$mime_type = $this->getMimeTypeByExtension($data["name"]);
 		}
 		
-		// if not retrieved mime type from file, get mime type passed from browser
+		// if not retrieved mime type from file or extension, get mime type passed by browser
 		if (empty($mime_type))
 			$mime_type = $data['type'];
 		
+		return $mime_type;
+	}
+	
+	public function getMimeTypeByExtension($filename) {
+		$mime_type = false;
+		include_once APP_PATH.'config'.DS.'mime.types.php';
+			$extension = strtolower( substr( $filename, strrpos($filename,".")+1 ) );
+			if (!empty($extension) && array_key_exists($extension,$config["mimeTypes"])) {
+				$mime_type = $config["mimeTypes"][$extension];
+		}
 		return $mime_type;
 	}
 	
