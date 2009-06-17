0a0cb68fa43de5803e2bfab76eb824c73e8251b4
stefanorosanelli <stefano.rosanelli@gmail.com>
2009-06-17 09:37:19 +0000

    new release

--- bedita-app/config/bedita.ini.php
+++ bedita-app/config/bedita.ini.php
@@ -36,7 +36,7 @@ $config["projectName"] = ""; // override in bedita.cfg
 
 $config["userVersion"] = "BEdita 3.0"; // don't override
 
-$config["majorVersion"] = "3.0.beta1"; // don't override -- admin/system
+$config["majorVersion"] = "3.0.beta2"; // don't override -- admin/system
 
  /**
  ** ******************************************
