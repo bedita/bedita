910b1e0a373fbd59a9a9c900d1344aa968bf2d16
stefanorosanelli <stefano.rosanelli@gmail.com>
2009-09-01 08:28:56 +0000

    apidoc changes

--- bedita-app/index.php
+++ bedita-app/index.php
@@ -1,5 +1,4 @@
 <?php
-/* SVN FILE: $Id: index.php 4410 2007-02-02 13:31:21Z phpnut $ */
 /**
  * \mainpage BEdita API documentation
  *
@@ -7,20 +6,16 @@
  *
  * Welcome to the API documentation of BEdita.<BR>
  * You can browse these pages using the menu on the left or executing searches in the form above.
- * <P>Note that, since BEdita development is in beta stage, this documentation is being updated continuously: here and there you could find missing paragraphs, misleading links and anchors and so on.<BR>
+ * <P>Note that, since BEdita3 is not yet stable, this documentation is being updated continuously: here and there you could find missing paragraphs, misleading links and anchors and so on.<BR>
  * Please be patient or contribute ;-)</P> 
  * 
  * Please visit <a href="http://www.bedita.com" target="_blank">www.bedita.com</a> to learn more about BEdita. The official wed site contains also a list of all the resources available online.
  * 
  * <BR><HR>
  *
- *
- * 
- * 
- * @filesource
- * @copyright		Copyright 2007-2009, ChannelWeb srl / Chialab srl
- * @package			bedita
- * @license			AGPL
+ * @version		$Revision$
+ * @modifiedby 		$LastChangedBy$
+ * @lastmodified	$LastChangedDate$
  */
 require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
 ?>
