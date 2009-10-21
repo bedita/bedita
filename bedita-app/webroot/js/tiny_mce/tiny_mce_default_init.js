87ff89c98d7b1c14ec644c5c108b6bed72a4d20c
stefanorosanelli <stefano.rosanelli@gmail.com>
2009-10-21 13:28:49 +0000

    merge from trunk

--- bedita-app/webroot/js/tiny_mce/tiny_mce_default_init.js
+++ bedita-app/webroot/js/tiny_mce/tiny_mce_default_init.js
@@ -3,18 +3,20 @@ tinyMCE.init({
 	mode : "textareas",
 	theme : "advanced",
 	editor_selector : "mce",
-	plugins : "safari,pagebreak,paste,fullscreen",
+	plugins : "safari,pagebreak,paste,fullscreen, xhtmlxtras",
 
 	// Theme options
 	theme_advanced_buttons1 : "bold,italic,underline,strikethrough, | ,formatselect,bullist,numlist, hr, | ,link,unlink,pastetext,pasteword, | ,removeformat,charmap,code,fullscreen",
-	theme_advanced_buttons2 : "",
-	theme_advanced_buttons3 : "", 
+	//theme_advanced_buttons2 : "sub, sup, cite, abbr, acronym, del",
+	theme_advanced_buttons2 : "", 
 	theme_advanced_toolbar_location : "top",
 	theme_advanced_toolbar_align : "left",
 	//theme_advanced_statusbar_location : "bottom",
 	//theme_advanced_resizing : true,
-	theme_advanced_blockformats : "p,h1,h2,h3,h4,blockquote,address",
+	theme_advanced_blockformats : "p,h1,h2,h3,h4,blockquote,address,dt,dd,code,samp",
 	width : "470",
+	
+	remove_redundant_brs : true,
 
 	
 	// Example content CSS (should be your site CSS)
