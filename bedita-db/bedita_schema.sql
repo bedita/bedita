cf364b7da0264dac9e579b1d681e97ddd2eed6d7
batopa <batopa@gmail.com>
2009-10-28 14:56:15 +0000

    added UNSIGNED and INDEX at process_info on mail_jobs

--- bedita-db/bedita_schema.sql
+++ bedita-db/bedita_schema.sql
@@ -787,10 +787,11 @@ CREATE TABLE mail_jobs (
   recipient MEDIUMTEXT NULL COMMENT 'used if card_is and mail_message_id are null, one or more comma separeted addresses',
   mail_params TEXT NULL COMMENT 'serialized array with: reply-to, sender, subject, signature...',
   smtp_err TEXT NULL COMMENT 'SMTP error message on sending failure',
-  process_info INTEGER NULL COMMENT 'pid of process delegates to send this mail job',
+  process_info INTEGER UNSIGNED NULL COMMENT 'pid of process delegates to send this mail job',
   PRIMARY KEY(id),
   INDEX card_id_index(card_id),
   INDEX mail_message_id_index(mail_message_id),
+  INDEX process_info_index(process_info),
   FOREIGN KEY(mail_message_id)
     REFERENCES mail_messages(id)
       ON DELETE CASCADE
