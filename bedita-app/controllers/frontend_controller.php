a76c55034a54d7d249154c38408f7c21387f5b7c
batopa <batopa@gmail.com>
2010-01-13 10:52:25 +0000

    fix contents canonicalPath

--- bedita-app/controllers/frontend_controller.php
+++ bedita-app/controllers/frontend_controller.php
@@ -1029,7 +1029,7 @@ abstract class FrontendController extends AppController {
 				$obj = $this->loadObj($item['id'], false);
 				if ($obj !== self::UNAUTHORIZED) {
 					if(isset($options["sectionPath"])) {
-						$obj["canonicalPath"] = ($options["sectionPath"] != "/") ? $options["sectionPath"] : "" 
+						$obj["canonicalPath"] = (($options["sectionPath"] != "/") ? $options["sectionPath"] : "") 
 							. "/" . $obj["nickname"];
 					}	
 					if ($this->sectionOptions["itemsByType"]) {
