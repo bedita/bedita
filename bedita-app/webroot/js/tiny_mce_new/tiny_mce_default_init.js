2f037c555587ecc3bd1a1ab7cf06b59c558caba0
qwerg <glob.andrea@gmail.com>
2010-03-18 10:50:20 +0000

    tinymce last release

---
+++ bedita-app/webroot/js/tiny_mce_new/tiny_mce_default_init.js
@@ -0,0 +1,45 @@
+tinyMCE.init({
+	// General options
+	mode : "textareas",
+	theme : "advanced",
+	editor_selector : "mce",
+	plugins : "safari,pagebreak,paste,fullscreen,advlink,xhtmlxtras",
+
+	// Theme options
+	theme_advanced_buttons1 : "bold,italic,underline,strikethrough, | ,formatselect,bullist,numlist, hr, | ,link,unlink,pastetext,pasteword, | ,removeformat,charmap,code,fullscreen",
+	theme_advanced_buttons2 : "mybutton,sub, sup, cite, abbr, acronym, del, attribs",
+	theme_advanced_buttons3 : "",
+	theme_advanced_toolbar_location : "top",
+	theme_advanced_toolbar_align : "left",
+	//theme_advanced_statusbar_location : "bottom",
+	//theme_advanced_resizing : true,
+	theme_advanced_blockformats : "p,h1,h2,h3,h4,blockquote,address,dt,dd,code,samp",
+	width : "470",
+	
+	remove_redundant_brs : true,
+	entity_encoding : "raw", //All characters will be stored in non-entity form except these XML default entities: &amp; &lt; &gt; &quot;
+	
+	// Example content CSS (should be your site CSS)
+	content_css : "/css/htmleditor.css",
+    relative_urls : false,
+	convert_urls : false,
+    remove_script_host : false,
+	document_base_url : "/"
+	
+	/*
+	// creare un plugin con i comenadi necessari
+	setup : function(ed) {
+        // Add a custom button
+        ed.addButton('mybutton', {
+            title : 'Definition',
+            image : '../img/iconFuture.png',
+            onclick : function() {
+				ed.focus();
+				ed.selection.setContent('<dfn>' + ed.selection.getContent() + '</dfn>');
+            }
+        });
+    }
+    */
+	
+});
+
