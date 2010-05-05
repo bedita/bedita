d32e2d9c6c95aeec670cc7c2942e4e480669a48d
batopa <batopa@gmail.com>
2010-05-05 15:35:21 +0000

    update changelog and upgrade sql data (delete object types pluginized)

--- bedita-app/config/sql/upgrade/0-3.0-to-3.1-data.sql
+++ bedita-app/config/sql/upgrade/0-3.0-to-3.1-data.sql
@@ -6,3 +6,8 @@ UPDATE `object_types` SET `name` = 'mail_template' WHERE `object_types`.`id` =36
 UPDATE `object_types` SET `name` = 'biblio_item' WHERE `object_types`.`id` =38;
 UPDATE `object_types` SET `name` = 'editor_note' WHERE `object_types`.`id` =39;
 UPDATE `object_types` SET `name` = 'questionnaire_result' WHERE `object_types`.`id` =42;
+
+-- remove `object_types` pluginized
+DELETE FROM `object_types` WHERE `object_types`.`name` = 'biblio_item' AND `object_types`.`id` =38
+DELETE FROM `object_types` WHERE `object_types`.`name` = 'bibliography' AND `object_types`.`id` =19
+DELETE FROM `object_types` WHERE `object_types`.`name` = 'book' AND `object_types`.`id` =20
