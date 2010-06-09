4c62bce809bcc550aa980746b863727fae7656de
qwerg <glob.andrea@gmail.com>
2010-06-09 07:35:11 +0000

    DFN tag in tiny mce

--- bedita-app/webroot/js/tiny_mce/tiny_mce_default_init.js
+++ bedita-app/webroot/js/tiny_mce/tiny_mce_default_init.js
@@ -1,13 +1,28 @@
+/*
+ <script type="text/javascript" src="<your installation path>/tiny_mce/tiny_mce_gzip.js"></script>
+
+// This is where the compressor will load all components, include all components used on the page here
+tinyMCE_GZ.init({
+plugins : 'style,layer,table,save,advhr,advimage,advlink,emotions,iespell,
+insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,
+visualchars,nonbreaking,xhtmlxtras',
+themes : 'advanced',
+languages : 'en',
+disk_cache : true,
+debug : false
+});
+ 
+ */
 tinyMCE.init({
 	// General options
 	mode : "textareas",
 	theme : "advanced",
 	editor_selector : "mce",
-	plugins : "safari,pagebreak,paste,fullscreen,advlink,xhtmlxtras",
-
+	plugins : "safari,paste,fullscreen,xhtmlxtras,inlinepopups",
+   	dialog_type : "modal",
 	// Theme options
 	theme_advanced_buttons1 : "bold,italic,underline,strikethrough, | ,formatselect,bullist,numlist, hr, | ,link,unlink,pastetext,pasteword, | ,removeformat,charmap,code,fullscreen",
-	theme_advanced_buttons2 : "mybutton,sub, sup, cite, abbr, acronym, del, attribs",
+	theme_advanced_buttons2 : "mybutton,sub, sup, cite, abbr, del, attribs",
 	theme_advanced_buttons3 : "",
 	theme_advanced_toolbar_location : "top",
 	theme_advanced_toolbar_align : "left",
@@ -25,8 +40,22 @@ tinyMCE.init({
 	convert_urls : false,
     remove_script_host : false,
 	document_base_url : "/",
-	onchange_callback : "onChangeHandler"
+	onchange_callback : "onChangeHandler",
+
+	setup : function(ed) {
+        // Add a custom button
+        ed.addButton('mybutton', {
+            title : 'definition',
+            image : '/img/dfn.png',
+            onclick : function() {
+				// Add you own code to execute something on click
+				ed.focus();
+                ed.selection.setContent('<dfn>' + ed.selection.getContent() + '</dfn>');
+            }
+        });
+    }
 });
+
 
 tinyMCE.init({
 	mode : "textareas",
@@ -52,6 +81,13 @@ tinyMCE.init({
 	toolbar_align : "right",
 	force_br_newlines : true,
     forced_root_block : '', // Needed for 3.x
-    onchange_callback : "onChangeHandler"
+    onchange_callback : "onChangeHandler",
+	
+
 });
 
+
+
+
+		
+		
