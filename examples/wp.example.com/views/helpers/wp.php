e3684926e4d594421f55233ff37b93de9e7f0d44
batopa <batopa@gmail.com>
2010-09-10 10:41:36 +0000

    svn:keywords

--- examples/wp.example.com/views/helpers/wp.php
+++ examples/wp.example.com/views/helpers/wp.php
@@ -22,11 +22,11 @@
 /**
  * Base annotation
  *
- * @version			$Revision: 2487 $
- * @modifiedby 		$LastChangedBy: ste $
- * @lastmodified	$LastChangedDate: 2009-11-25 17:56:37 +0100 (Wed, 25 Nov 2009) $
+ * @version			$Revision$
+ * @modifiedby 		$LastChangedBy$
+ * @lastmodified	$LastChangedDate$
  *
- * $Id: annotation.php 2487 2009-11-25 16:56:37Z ste $
+ * $Id$
  */
 
 /* 
