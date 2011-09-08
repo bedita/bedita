d1dfe7405d3db8d2af34d3820443bec4219e7dca
qwerg <glob.andrea@gmail.com>
2011-09-08 17:11:18 +0000

    fill the blanks with html5 attr placeholder, TODO: move it to the questionnaire plugin

--- bedita-app/webroot/js/tiny_mce/tiny_mce_BEanswers_init.js
+++ bedita-app/webroot/js/tiny_mce/tiny_mce_BEanswers_init.js
@@ -1,4 +1,5 @@
 tinyMCE.init({
+	
 	// General options
 	mode : "textareas",
 	theme : "advanced",
@@ -16,6 +17,7 @@ tinyMCE.init({
 	theme_advanced_blockformats : "p,h1,h2,h3,h4,blockquote,address",
 	width : "550",
 
+	extended_valid_elements : 'input[accept|accesskey|align<bottom?left?middle?right?top|alt|autocomplete|autofocus|checked<checked|class|dir<ltr?rtl|disabled<disabled|form|id|ismap<ismap|lang|list|max|maxlength|min|name|onblur|onclick|ondblclick|onfocus|onkeydown|onkeypress|onkeyup|onmousedown|onmousemove|onmouseout|onmouseover|onmouseup|onselect|pattern|placeholder|readonly<readonly|required<required|size|src|style|tabindex|title|type<button?checkbox?file?hidden?image?password?radio?reset?submit?text?datetime?datetime-local?date?month?time?week?number?range?email?url?search?tel?color|usemap|value]',
 	
 	// Example content CSS (should be your site CSS)
 	content_css : "/css/htmleditor.css",
@@ -25,7 +27,6 @@ tinyMCE.init({
 	document_base_url : "/",
 
 	force_br_newlines : true,
-	forced_root_block : '' // Needed for 3.x
-
+	forced_root_block : '' // Needed for 3.x,
 
 });
