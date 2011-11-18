822c8153d1249f9d3373ed72a4a83396cdf914c0
didoda <dante@didoda.com>
2011-11-18 15:58:45 +0000

    [bedita-trunk] admin: configuration page (#268) - fix messages in controller

--- bedita-app/controllers/modules/admin_controller.php
+++ bedita-app/controllers/modules/admin_controller.php
@@ -507,19 +507,19 @@ class AdminController extends ModulesController {
 		// check bedita.sys.php
 		$beditaSysPath = CONFIGS . "bedita.sys.php";
 		if (!file_exists($beditaSysPath)) {
-			$this->set("bedita_sys_err",__($beditaSysPath . " doesn't exist",true));
+			$this->set("bedita_sys_err", __("Path not found",true) . ": " . $beditaSysPath);
 		}
 		if (!$besys->checkWritable($beditaSysPath)) {
-			$this->set("bedita_sys_err",__($beditaSysPath . " is not writable: update file permits properly",true));
+			$this->set("bedita_sys_err", __("File not writable, update properly file permits for",true) . " " . $beditaSysPath);
 		}
 
 		// check bedita.cfg.php
 		$beditaCfgPath = CONFIGS . "bedita.cfg.php";
 		if (!file_exists($beditaCfgPath)){
-			$this->set("bedita_cfg_err",__($beditaCfgPath . " doesn't exist",true));
+			$this->set("bedita_cfg_err", __("Path not found",true) . ": " . $beditaCfgPath);
 		}
 		if (!$besys->checkWritable($beditaCfgPath)) {
-			$this->set("bedita_cfg_err",__($beditaCfgPath . " is not writable: update file permits properly",true));
+			$this->set("bedita_cfg_err", __("File not writable, update properly file permits for",true) . " " . $beditaCfgPath);
 		}
 
 		$conf = Configure::getInstance();
