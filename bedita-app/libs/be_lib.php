cdefedd0f3bfbefc7d9a991b990c41f70030b8ee
stefanorosanelli <stefano.rosanelli@gmail.com>
2011-12-14 10:35:35 +0000

    be_lib: allow start digits in nickname / friendlyUrlString

--- bedita-app/libs/be_lib.php
+++ bedita-app/libs/be_lib.php
@@ -226,8 +226,6 @@ class BeLib {
 		$value = preg_replace("/&(.)(acute|grave|cedil|circ|ring|tilde|uml);/", "$1", $value);
 		// replace special chars and space with dash (first decode html entities)
 		$value = preg_replace("/[^a-z0-9\-_]/i", "-", html_entity_decode($value,ENT_NOQUOTES,"UTF-8" ) ) ;
-		// remove digits and dashes in the beginning 
-		$value = preg_replace("/^[0-9\-]{1,}/", "", $value);
 		// replace two or more consecutive dashes with one dash
 		$value = preg_replace("/[\-]{2,}/", "-", $value);
 		// trim dashes in the beginning and in the end of nickname
