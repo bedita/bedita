c6c0cb23b708aa53fe8c4f55792250d6d4bf1f31
didoda <dante@didoda.com>
2012-03-02 10:05:39 +0000

    [bedita-trunk] package release beta populus

--- bedita-app/config/bedita.ini.php
+++ bedita-app/config/bedita.ini.php
@@ -34,7 +34,7 @@ $config = array() ;
  
 $config["projectName"] = "BEdita 3.2"; // override in bedita.cfg
 
-$config["userVersion"] = "BEdita 3.2 alpha"; // don't override
+$config["userVersion"] = "BEdita 3.2 beta"; // don't override
 
 $config["majorVersion"] = "3.2.beta"; // don't override -- admin/system
 
