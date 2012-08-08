bae0f10277f4aed9d8bb7cbe48c29364f36c4e6d
stefanorosanelli <stefano.rosanelli@gmail.com>
2012-08-08 16:14:52 +0000

    move to LGPL - see #327

--- examples/debug.example.com/controllers/pages_controller.php
+++ examples/debug.example.com/controllers/pages_controller.php
@@ -10,7 +10,8 @@ class PagesController extends FrontendController {
 	 */ 
 	protected function beditaBeforeFilter() {
 		$this->set('feedNames', $this->Section->feedsAvailable(Configure::read("frontendAreaId")));
-		$this->set('sectionsTree', $this->loadSectionsTree(Configure::read("frontendAreaId")));
+		$this->set('sectionsTree', $this->loadSectionsTree(Configure::read("frontendAreaId"),
+		false , array(), 2));
 		// uncomment to use ctp file instead of tpl file for templates
 //		$this->view = "View"; 
 	}
