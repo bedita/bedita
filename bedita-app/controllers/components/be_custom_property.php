caf994c1f6dcda6500bc976c10c47011c8022b6f
batopa <batopa@gmail.com>
2012-09-03 14:44:20 +0000

    trunk [merged from ulmus] - allowed to save user and object custom property value equal to "0"

--- bedita-app/controllers/components/be_custom_property.php
+++ bedita-app/controllers/components/be_custom_property.php
@@ -50,15 +50,12 @@ class BeCustomPropertyComponent extends Object {
 		if (!empty($this->controller->data["ObjectProperty"])) {
 			$objProp = array();
 			foreach($this->controller->data["ObjectProperty"] as $key => $value) {
-				
-				if (!empty($value["property_value"])) {
+				if (isset($value["property_value"])) {
 					$value["property_value"] = trim($value["property_value"]);
-					
-					if (!empty($value["property_value"])) {
+					if (strlen($value["property_value"]) !== 0) {
 						$objProp[] = $value;
 					}
-					
-				} 
+				}
 			}
 			
 			$this->controller->data["ObjectProperty"] = $objProp;
@@ -73,14 +70,12 @@ class BeCustomPropertyComponent extends Object {
 		if (!empty($this->controller->data["UserProperty"])) {
 			$objProp = array();
 			foreach($this->controller->data["UserProperty"] as $key => $value) {
-				
-				if (!empty($value["property_value"])) {
+				if (isset($value["property_value"])) {
 					$value["property_value"] = trim($value["property_value"]);
-					
-					if (!empty($value["property_value"])) {
+					if (strlen($value["property_value"]) !== 0) {
 						$objProp[] = $value;
 					}
-				} 
+				}
 			}
 			$this->controller->data["UserProperty"] = $objProp;
 		}
