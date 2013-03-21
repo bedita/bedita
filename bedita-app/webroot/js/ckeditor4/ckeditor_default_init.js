65c3c420e2bc566f1ba5097b6ab67399e357c41b
andrea <glob.andrea@gmail.com>
2013-03-21 10:12:36 -0700

    texeditor minor config fix

--- bedita-app/webroot/js/ckeditor4/ckeditor_default_init.js
+++ bedita-app/webroot/js/ckeditor4/ckeditor_default_init.js
@@ -4,21 +4,19 @@ $(window).load(function() {
 		toolbar: [
 			{ name: 'document', groups: [ 'mode' ], items: [ 'Source'] },
 			{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+			{ name: 'links', items: [ 'Link', 'Unlink'/*, 'Anchor' */] },
 			{ name: 'paragraph', groups: [ 'list','blocks', 'align' ], items: [ 'NumberedList', 'BulletedList', '-', 'Blockquote', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
 			'/',
-			{ name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
 			{ name: 'customTools', items: [ 'x\y', 'Dfn', 'Glo' ] },
 			{ name: 'editAttributes', items: [ 'Attr' ] },
-			{ name: 'editing', groups: [ 'find'], items: [ 'Find', 'Replace' ] },
+			{ name: 'editing', groups: [ 'find'], items: [ 'Find'/*, 'Replace'*/ ] },
 			{ name: 'insert', items: [ 'Table', 'HorizontalRule', 'SpecialChar' ] },
-			{ name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
-			
-			'/',
 			{ name: 'styles', items: [ 'Format' , 'Styles'] },
-			{ name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+			{ name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ /*'Paste', */'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+			{ name: 'tools', items: [/* 'Maximize',*/ 'ShowBlocks' ] },
 		],
 		resize_enabled: true,
-		extraPlugins: 'codemirror,attributes,beButtons,onchange,onchange',
+		extraPlugins: 'codemirror,attributes,beButtons,onchange',
 		language: BEDITA.currLang2,
 		codemirror: { theme: 'lesser-dark' }
 	};
@@ -27,13 +25,12 @@ $(window).load(function() {
 		toolbar: [
 			{ name: 'document', groups: [ 'mode' ], items: [ 'Source'] },
 			{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
-			{ name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+			{ name: 'links', items: [ 'Link', 'Unlink'/*, 'Anchor' */] },
 			{ name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Undo', 'Redo' ] },
-			{ name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
+			{ name: 'tools', items: [/* 'Maximize', */'ShowBlocks' ] },
 		],
-		removePlugins: 'elementspath',
 		resize_enabled: true,
-		extraPlugins: 'codemirror,attributes,beButtons,onchange,onchange',
+		extraPlugins: 'codemirror,onchange',
 		language: BEDITA.currLang2,
 		codemirror: { theme: 'lesser-dark' }
 	}
