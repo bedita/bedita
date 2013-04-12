d20837b658706cd05c918e4e5458987d9c87a814
edoardocavazza <edoardo.cavazza@chialab.it>
2013-04-12 07:59:03 -0700

    fix spaces after tag for CKEditor

--- bedita-app/webroot/js/libs/richtexteditors/ckeditor/plugins/codemirror/plugin.js
+++ bedita-app/webroot/js/libs/richtexteditors/ckeditor/plugins/codemirror/plugin.js
@@ -11,7 +11,7 @@
     CKEDITOR.plugins.add('codemirror', {
         lang: 'af,ar,bg,bn,bs,ca,cs,cy,da,de,el,en-au,en-ca,en-gb,en,eo,es,et,eu,fa,fi,fo,fr-ca,fr,gl,gu,he,hi,hr,hu,is,it,ja,ka,km,ko,ku,lt,lv,mk,mn,ms,nb,nl,no,pl,pt-br,pt,ro,ru,sk,sl,sr-latn,sr,sv,th,tr,ug,uk,vi,zh-cn,zh',
 		init: function (editor) {
-			
+			var getWithoutIndentetion = true;
 			var rootPath = this.path;
 
             // Get Config
@@ -123,7 +123,10 @@
 								codeMirror_Editor.autoFormatRange(range.from, range.to, false);
 							
 						}
-					}
+					},
+					smartIndent: false,
+					tabSize: 1,
+					indentWithTabs: true
                 });
 				
 				if (autoFormatOnStart) {
