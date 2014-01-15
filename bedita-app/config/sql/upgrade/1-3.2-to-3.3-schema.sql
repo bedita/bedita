b173d6de1448cfbdc39a2b18bff9e02fbc5a05ef
stefanorosanelli <stefano.rosanelli@gmail.com>
2014-01-15 19:03:48 +0100

    #425 - add unique constraint

--- bedita-app/config/sql/upgrade/1-3.2-to-3.3-schema.sql
+++ bedita-app/config/sql/upgrade/1-3.2-to-3.3-schema.sql
@@ -1,2 +1,3 @@
 DROP TABLE IF EXISTS `authors`;
 ALTER TABLE `object_relations` ADD  `params` TEXT NULL COMMENT 'relation properties values';
+ALTER TABLE  `permissions` ADD UNIQUE permissions_obj_ug_sw_fl(`object_id`, `ugid`, `switch`,`flag`);
