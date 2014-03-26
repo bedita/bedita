e2a0fa59678c660e655c67671a1356c85727d828
stefanorosanelli <stefano.rosanelli@gmail.com>
2014-03-26 16:50:31 +0100

    code conventions example

--- bedita-app/app_helper.php
+++ bedita-app/app_helper.php
@@ -24,78 +24,70 @@ App::import('Core', 'Helper');
 /**
  * Helper base class
  * contains common helpers methods
- * 
- *
- * @version			$Revision$
- * @modifiedby 		$LastChangedBy$
- * @lastmodified	$LastChangedDate$
- * 
- * $Id$
  */
 class AppHelper extends Helper {
- 	
-	/**
-	 * try to get from the registry the helper instance. Eventually instance a new helper object ant put
-	 * it in the registry
-	 * @param $name, helper name without suffix Helper (i.e. BlipHelper => $name=Blip)
-	 * @return $nameHelper instance  
-	 */
-	public function getHelper($name) {
-		$helper = $name."Helper";
-		if (!$helperObject = ClassRegistry::getObject($helper)) {
-			if (!class_exists($helper)) {
-				if (!App::import("Helper", $name)) {
-					return false;
-				}
-			}
-			$helperObject = new $helper();
-			
-			/* copy class attributes to new helper instance
-			 * see file cake/libs/view/view.php
-			 * class View
-			 * method _loadHelpers
-			 * line 809
-			 */
-			$vars = array('base', 'webroot', 'here', 'params', 'action', 'data', 'theme', 'plugin');
-			$c = count($vars);
-			for ($j = 0; $j < $c; $j++) {
-				$helperObject->{$vars[$j]} = $this->{$vars[$j]};
-			}
-			
-			ClassRegistry::addObject($helper, $helperObject);
-			if (!empty($helperObject->helpers)) {
-				foreach ($helperObject->helpers as $subHelper) {
-					$helperObject->{$subHelper} = $this->getHelper($subHelper);
-				}
-			}
-		}
-		
-		return $helperObject;
-	}
-	
-	/**
-	 * return oEmbed format (see http://www.oembed.com)
-	 * 
-	 * @param $url, URL on third party sites
-	 * @param $arrayFrom, specify which format is expected (json, xml) to build array in the right way
-	 * 					if != "json" and != "xml" return original oEmbed format (i.e. JSON object or XML) 
-	 * @return array
-	 */
-	protected function oEmbedInfo($url, $arrayFrom="json") {
-		if (!$oEmbedInfo = file_get_contents($url)) {
-			return false;
-		}
-		if ($arrayFrom == "json") {
-			$oEmbedInfo = (json_decode($oEmbedInfo,true));
-		} elseif ($arrayFrom == "xml") {
-			$xml = ClassRegistry::init("Xml", "Core");
-			$xml->load($oEmbedInfo);
-			$oEmbedInfo = Set::reverse($xml);
-			$oEmbedInfo = $oEmbedInfo["Oembed"];
-		}
-		return $oEmbedInfo;
-	}
-	
+
+    /**
+     * try to get from the registry the helper instance. Eventually instance a new helper object ant put
+     * it in the registry
+     * @param $name, helper name without suffix Helper (i.e. BlipHelper => $name=Blip)
+     * @return $nameHelper instance  
+     */
+    public function getHelper($name) {
+        $helper = $name . 'Helper';
+        if (!$helperObject = ClassRegistry::getObject($helper)) {
+            if (!class_exists($helper)) {
+                if (!App::import("Helper", $name)) {
+                    return false;
+                }
+            }
+            $helperObject = new $helper();
+
+            /* copy class attributes to new helper instance
+             * see file cake/libs/view/view.php
+             * class View
+             * method _loadHelpers
+             * line 809
+             */
+            $vars = array('base', 'webroot', 'here', 'params',
+                            'action', 'data', 'theme', 'plugin');
+            $c = count($vars);
+            for ($j = 0; $j < $c; $j++) {
+                $helperObject->{$vars[$j]} = $this->{$vars[$j]};
+            }
+
+            ClassRegistry::addObject($helper, $helperObject);
+            if (!empty($helperObject->helpers)) {
+                foreach ($helperObject->helpers as $subHelper) {
+                    $helperObject->{$subHelper} = $this->getHelper($subHelper);
+                }
+            }
+        }
+
+        return $helperObject;
+    }
+
+    /**
+     * return oEmbed format (see http://www.oembed.com)
+     * 
+     * @param $url, URL on third party sites
+     * @param $arrayFrom, specify which format is expected (json, xml) to build array in the right way
+     * 					if != "json" and != "xml" return original oEmbed format (i.e. JSON object or XML) 
+     * @return array
+     */
+    protected function oEmbedInfo($url, $arrayFrom = 'json') {
+        if (!$oEmbedInfo = file_get_contents($url)) {
+            return false;
+        }
+        if ($arrayFrom == 'json') {
+            $oEmbedInfo = (json_decode($oEmbedInfo,true));
+        } elseif ($arrayFrom == 'xml') {
+            $xml = ClassRegistry::init('Xml', 'Core');
+            $xml->load($oEmbedInfo);
+            $oEmbedInfo = Set::reverse($xml);
+            $oEmbedInfo = $oEmbedInfo['Oembed'];
+        }
+        return $oEmbedInfo;
+    }
+
 }
- 
-?>
