5955fbb4a6860831c548642729381819ac53cace
batopa <batopa@gmail.com>
2014-04-11 12:34:23 +0200

    replace old multiupload in multimedia index #456

--- bedita-app/webroot/js/fragments/quick_item_media.js
+++ bedita-app/webroot/js/fragments/quick_item_media.js
@@ -22,8 +22,30 @@ $(document).ready(function() {
     dropzone.on('complete', function(file) {
         // File finished uploading, and there aren't any left in the queue.
         if (this.getQueuedFiles().length == 0 && this.getUploadingFiles().length == 0) {
-            loadObjToAssoc(1, uploadMediaCreated);
+            // if index of multimedia show modalcommands
+            if (BEDITA.currentModule.name == 'multimedia' && BEDITA.action == 'index') {
+                $('#modal').find('div.modalcommands').show();
+            // else reload list objects in modal
+            } else {
+                loadObjToAssoc(1, uploadMediaCreated);
+            }
         }
     });
+
+    // if index of multimedia define button behavior
+    if (BEDITA.currentModule.name == 'multimedia' && BEDITA.action == 'index') {
+
+        // add behavior to 'close' modal button to eventually reload page
+        $('#modalheader .close').one('click', function() {
+            if (uploadMediaCreated.length) {
+                location.reload();
+            }
+        });
+
+        // add behavior to 'done' button
+        $('#uploadDoneButton').one('click', function() {
+            $("#modalheader .close").click();
+        });
+    }
 
 });
