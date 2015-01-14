cebfedeaa2ca2839f1bbcbd125e95c5f1762ddd1
niki corradetti <n.corradetti@channelweb.it>
2015-01-14 18:31:04 +0100

    BeEmbedMedia implementation for html5 Video and Audio

--- bedita-app/views/helpers/be_embed_html5.php
+++ bedita-app/views/helpers/be_embed_html5.php
@@ -46,7 +46,7 @@ public $helpers = array("Html");
 	 * @param array $htmlAttributes
 	 * @return mixed string|boolean, html code of embed media, or false if file extension is not supported
 	 */
-	public function embed($obj , $params, $htmlAttributes ) {
+	public function embed($obj, $params, $htmlAttributes ) {
 		
 		if (empty($obj['uri'])) {
 			return __("No file to embed");
@@ -54,89 +54,118 @@ public $helpers = array("Html");
 
 		$params = empty($params['params']) ? array() : $params['params'];	
 		$extension = $this->getFileExtension($obj['uri']);
-		$output = "";
 		
 		if ($obj["object_type_id"] == Configure::read("objectTypes.audio.id") && $extension == 'mp3') {
 			$fileType = "audio";
 
-			return $this->embedAudio($obj['uri'], $htmlAttributes, $flashvars, $flashParams);
+			return $this->embedAudio($obj['uri'], $params, $htmlAttributes);
 
 		} else {
-			
 			$fileType = "video";	
-			$beditaUrl = Configure::read('beditaUrl');
 			
-			//$swfUrl = empty($attributes['src']) ? $beditaUrl."/swf/".Configure::read("media." . $fileType . ".player") : $beditaUrl."/swf/".$attributes['src'] ;
-			//$html5PlayerUrl = empty($attributes['src']) ? $beditaUrl.
-
-			if (empty($attributes["width"]))
-				$attributes["width"] = Configure::read("media." . $fileType . ".width");
-			if (empty($attributes["height"]))
-				$attributes["height"] = Configure::read("media." . $fileType . ".height");
+			return $this->embedVideo($obj['uri'], $params, $htmlAttributes);
 			
-			$width = (!empty($attributes['width'])) ? $attributes['width'] : $this->widthDef;
-			$height = (!empty($attributes['height'])) ? $attributes['height'] : $this->heightDef;
-
-			//$app_ver = (!empty($attributes['application_version']))? $attributes['application_version'] : $this->appVerDef;
-			
-			if (empty($attributes['id'])) {
-				$attributes['id'] = "be_id_" . rand(10000, 11000) . rand(1, 10000);
-			}
-			if (!empty($attributes['src'])) {
-				unset($attributes['src']);
-			}
-			
-			//$fv  = json_encode($flashvars);
-			$par = json_encode($params);
-			$att = json_encode($attributes);
-			
-			//if ( defined("BEDITA_CORE_PATH") && !file_exists(APP . "/webroot/js/libs/mediaelement/mediaelement-and-player.min.js")) {
-				$output .= $this->Html->script(Configure::read('beditaUrl') . "/js/libs/mediaelement/mediaelement-and-player.min.js",false);
-				$output .= $this->Html->css(Configure::read('beditaUrl') . "/js/libs/mediaelement/mediaelementplayer.css",false);
-			//} 
-			//pr($output);exit;
-			
-			//else {
-			//	$output = $this->Javascript->link("swfobject",false);
-			//}
-			//$output .= '<div id="'.$attributes['id'].'"></div>
-			//	<script type="text/javascript">swfobject.embedSWF("'.$swfUrl.'","'.$attributes['id'].'","'.$width.'","'.$height.'","'.$app_ver.'","expressInstall.swf",'.$fv.','.$par.','.$att.');</script>';
-
-
-			$output .= '<video src="'.$obj['uri'].'" width="'.$width.'" height="'.$height.'" controls="controls" ></video>';
-			$output .= '<script>jQuery(document).ready(function($) {
-							$("video").mediaelementplayer({      					
-        						features: ["playpause","loop","current","progress","duration","volume"]
-    						});
-						});</script>';
-			return $output;
-
-			//$pathParts = pathinfo($swfUrl);
-			//$methodName = "embed".Inflector::camelize($pathParts['filename']);
-			
-			//if (method_exists($this, $methodName ) ) {
-			//	return $this->$methodName($swfUrl, $fileToPlay, $attributes, $flashvars, $params, $fileType);
-			//} 		
-			//return $this->embedSwf( $swfUrl , $attributes, $flashvars, $params );
 		} 
 
-
-		/*($extension == 'flv' || $extension == 'm4v' || $extension == 'mp4') {
-			if (!empty($obj['thumbnail'])) {
-				$flashvars['thumbnail'] = $obj['thumbnail']; 
-			}			
-			return $this->embedFlv($obj['uri'], $htmlAttributes, $flashvars, $flashParams);
-		} else if ($extension == 'swf') {
-			 return $this->embedSwf($obj['uri'], $htmlAttributes, $flashvars, $flashParams);
-		} else {
-			return false;
-		}*/
 		
+	}
+
+	/**
+	 * Embed Video
+	 * 
+	 * @param 
+	 * @return 
+	 */
+	private function embedVideo($urlVideo, $params, $attributes) {
+		
+		$beditaUrl = Configure::read('beditaUrl');
+		$defaultParams = array(
+			'features' => array('playpause', 'loop', 'current', 'progress', 'duration', 'volume')
+			);
+
+		//defaults attributes
+		if (empty($attributes["width"]))
+			$attributes["width"] = Configure::read("media." . $fileType . ".width");
+		if (empty($attributes["height"]))
+			$attributes["height"] = Configure::read("media." . $fileType . ".height");
+		
+		$width = (!empty($attributes['width'])) ? $attributes['width'] : $this->widthDef;
+		$height = (!empty($attributes['height'])) ? $attributes['height'] : $this->heightDef;
+	
+		if (empty($attributes['id'])) {
+			$attributes['id'] = "be_id_" . rand(10000, 11000) . rand(1, 10000);
+		}
+		if (!empty($attributes['src'])) {
+			unset($attributes['src']);
+		}
+		$attr = $this->_parseAttributes($attributes);
+
+		//player params
+		if (empty($params)) {
+			$params = $defaultParams;
+		} else {
+			$params = array_merge($defaultParams, $params);
+		}
+		$output = "";
+		$output .= $this->Html->script(Configure::read('beditaUrl') . "/js/libs/mediaelement/mediaelement-and-player.min.js",false);
+		$output .= $this->Html->css(Configure::read('beditaUrl') . "/js/libs/mediaelement/mediaelementplayer.css",false);
+		$output .= '<video src="'.$urlVideo.'" '.$attr.' controls="controls" >
+					 	<!-- Flash fallback for non-HTML5 browsers without JavaScript -->
+						<object '.$attr.' type="application/x-shockwave-flash" data="flashmediaelement.swf">
+						    <param name="movie" value="flashmediaelement.swf" />
+						    <param name="flashvars" value="controls=true&file='.$urlVideo.'" />
+						    <!-- Image as a last resort -->
+						    <img src="myvideo.jpg" '.$attr.' title="No video playback capabilities" />
+						</object>
+					</video>';
+		$output .= '<script>jQuery(document).ready(function($) {
+						$("video").mediaelementplayer('.json_encode($params).');
+					});</script>';
+		return $output;
+
+	}
+
+
+	/**
+	 * Embed Audio
+	 * 
+	 * @param 
+	 * @return 
+	 */
+	private function embedAudio($urlAudio, $params, $attributes) {
+		
+		$beditaUrl = Configure::read('beditaUrl');
+		
+		if (empty($attributes['id'])) {
+			$attributes['id'] = "be_id_" . rand(10000, 11000) . rand(1, 10000);
+		}
+		if (!empty($attributes['src'])) {
+			unset($attributes['src']);
+		}
+		$attr = $this->_parseAttributes($attributes);
+
+		//player params
+		if (empty($params)) {
+			$params = $defaultParams;
+		} else {
+			$params = array_merge($defaultParams, $params);
+		}
+		
+		$output = "";
+		$output .= $this->Html->script(Configure::read('beditaUrl') . "/js/libs/mediaelement/mediaelement-and-player.min.js",false);
+		$output .= $this->Html->css(Configure::read('beditaUrl') . "/js/libs/mediaelement/mediaelementplayer.css",false);
+		
+		$output .= '<audio src="'.$urlAudio.'" '.$attr.'" controls="controls"></audio>';
+		$output .= '<script>jQuery(document).ready(function($) {
+						$("audio").mediaelementplayer('.json_encode($params).');
+					});</script>';
+		return $output;
+
 	}
 
 
 
-/**
+	/**
 	 * get file extension
 	 * 
 	 * @param string $filePath
