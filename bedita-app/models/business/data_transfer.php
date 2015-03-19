43a44d0541e219d94c0ef3b470a06ed60481d73c
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2015-03-19 12:09:42 +0100

    #625 Data export - Use model bindings.

--- bedita-app/models/business/data_transfer.php
+++ bedita-app/models/business/data_transfer.php
@@ -99,7 +99,8 @@ class DataTransfer extends BEAppModel
             'ObjectType',
             'UserCreated',
             'UserModified',
-            'User'
+            'User',
+            'Version',
         ),
         'contain' => array(
             'BEObject' => array(
@@ -394,7 +395,7 @@ class DataTransfer extends BEAppModel
 
                 $objModel = ClassRegistry::init($model);
                 $objModel->contain(
-                    $this->export['contain']
+                    $this->modelBinding($model, $objModel)
                 );
                 $obj = $objModel->findById($objectId);
                 $this->prepareObjectForExport($obj);
@@ -425,7 +426,7 @@ class DataTransfer extends BEAppModel
                             $this->export['destination']['byType']['ARRAY']['tree']['sections'][] = $sectionItem;
                             $objModel = ClassRegistry::init('Section');
                             $objModel->contain(
-                                $this->export['contain']
+                                $this->modelBinding('Section', $objModel)
                             );
                             $obj = $objModel->findById($section['id']);
                             $this->prepareObjectForExport($obj);
@@ -1457,10 +1458,9 @@ class DataTransfer extends BEAppModel
                         $this->trackResult('WARN', 'unable to export related type: ' . $model . ' tree info may be missing');
                         continue;
                     }
-                    $containLabel = $this->containLabel($model);
                     $relatedObjModel = ClassRegistry::init($model);
                     $relatedObjModel->contain(
-                        $this->export[$containLabel]
+                        $this->modelBinding($model, $relatedObjModel)
                     );
                     $relatedObj = $relatedObjModel->findById($relatedObjectId);
                     $this->prepareObjectForExport($relatedObj, $nextLevel);
@@ -1537,10 +1537,9 @@ class DataTransfer extends BEAppModel
                     } else {
                         $model = $conf->objectTypesExt[$objectTypeId]['model'];
                     }
-                    $containLabel = $this->containLabel($model);
                     $objModel = ClassRegistry::init($model);
                     $objModel->contain(
-                        $this->export[$containLabel]
+                        $this->modelBinding($model, $objModel)
                     );
                     $obj = $objModel->findById($objectId);
                     $this->prepareObjectForExport($obj);
@@ -1550,11 +1549,20 @@ class DataTransfer extends BEAppModel
         $tree->unbindModel(array('belongsTo' => array('BEObject')));
     }
 
-    private function containLabel($model) {
-        if (in_array($model, $this->streamModels)) {
-            return 'contain-stream';
-        } else {
-            return 'contain';
+    /**
+     * Return the proper model binding for model.
+     *
+     * @param string $model Model name.
+     * @param BEAppModel $objModel Instantiated model object.
+     * @return array Model binding.
+     */
+    private function modelBinding($model, BEAppModel $objModel) {
+        try {
+            // Use detailed model binding, if present.
+            return $objModel->containLevel('detailed');
+        } catch (Exception $e) {
+            // Use default basic model bindings.
+            return $this->export[in_array($model, $this->streamModels) ? 'contain-stream' : 'contain'];
         }
     }
 
