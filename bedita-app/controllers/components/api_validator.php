0c9179eb6db9c51cf65c208f93527806d794bf8f
batopa <batopa@gmail.com>
2015-11-09 12:15:04 +0100

    #761 custom properties validation

--- bedita-app/controllers/components/api_validator.php
+++ bedita-app/controllers/components/api_validator.php
@@ -310,6 +310,9 @@ class ApiValidatorComponent extends Object {
             }
             $this->checkDateItems($object['date_items'], $objectId);
         }
+        if (!empty($object['custom_properties'])) {
+            $this->checkCustomProperties($object['custom_properties'], $object['object_type_id']);
+        }
     }
 
     /**
@@ -798,6 +801,73 @@ class ApiValidatorComponent extends Object {
                     if (empty($count)) {
                         throw new BeditaBadRequestException('geo_tags: ' . $field . '=' . $value .' is not valid');
                     }
+                }
+            }
+        }
+    }
+
+    /**
+     * Check if custom properties are valid
+     * The $customProperties array has to be in the form
+     *
+     * ```
+     * array(
+     *     'custom_prop_name_1' => 'value1',
+     *     'custom_prop_name_2' => 'value2',
+     *     'custom_prop_name_3' => array('value3', 'value4') // multiple choice
+     * )
+     * ```
+     *
+     * @param array $customProperties the custom properties to validate
+     * @param int|string $objectTypeId the object type id or name
+     * @return void
+     */
+    public function checkCustomProperties(array $customProperties, $objectTypeId) {
+        if (!is_numeric($objectTypeId)) {
+            $objectTypeId = Configure::read('objectTypes.' . $objectTypeId . '.id');
+        }
+        $property = ClassRegistry::init('Property');
+        $properties = $property->find('all', array(
+            'fields' => array('name', 'property_type', 'multiple_choice'),
+            'conditions' => array(
+                'name' => array_keys($customProperties),
+                'object_type_id' => $objectTypeId
+            ),
+            'contain' => array('PropertyOption')
+        ));
+        $properties = Set::combine($properties, '{n}.name', '{n}');
+
+        // check options closure
+        $checkOptions = function(array $valuesToCheck, array $prop) {
+            $availableOptions = Set::extract('/PropertyOption/property_option', $prop);
+            $valuesForbidden = array_diff($valuesToCheck, $availableOptions);
+            if (!empty($valuesForbidden)) {
+                throw new BeditaBadRequestException(
+                    'Custom property ' . $prop['name'] . ' values allowed are: ' . implode(',', $availableOptions)
+                );
+            }
+        };
+
+        foreach ($customProperties as $name => $value) {
+            if (empty($properties[$name])) {
+                throw new BeditaBadRequestException('Custom property ' . $name . ' not exists');
+            }
+            $propData = $properties[$name];
+            if (is_array($value)) {
+                if ($propData['property_type'] != 'options' || $propData['multiple_choice'] == 0) {
+                    throw new BeditaBadRequestException('Custom property ' . $name . ' does not support multiple values');
+                }
+                // check if property values are options valid
+                $checkOptions($value, $propData);
+            } else {
+                if ($propData['property_type'] == 'date') {
+                    $this->checkDate($value);
+                } elseif ($propData['property_type'] == 'number') {
+                    if (!is_numeric($value)) {
+                        throw new BeditaBadRequestException('Custom property ' . $name . ' must be numeric');
+                    }
+                } elseif ($propData['property_type'] == 'options') {
+                    $checkOptions(array($value), $propData);
                 }
             }
         }
