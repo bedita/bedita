e4ddce3f5655994354ec794bb8bef0010cdac7f1
Paolo Cuffiani <me@fquff.io>
2016-03-02 21:53:47 +0100

    chore: [BEdita/Auth] add Auth plugin structure

---
+++ plugins/BEdita/Auth/README.md
@@ -0,0 +1,2 @@
+# BEdita 4 Auth plugin
+A plugin to provide User authentication in any BEdita application.
---
+++ plugins/BEdita/Auth/tests/bootstrap.php
@@ -0,0 +1,38 @@
+<?php
+// @codingStandardsIgnoreFile
+use Cake\Core\Configure;
+use Cake\Core\Plugin;
+use Cake\Datasource\ConnectionManager;
+$findRoot = function ($root) {
+    do {
+        $lastRoot = $root;
+        $root = dirname($root);
+        if (is_dir($root . '/vendor/cakephp/cakephp')) {
+            return $root;
+        }
+    } while ($root !== $lastRoot);
+    throw new Exception('Cannot find the root of the application, unable to run tests');
+};
+$root = $findRoot(__FILE__);
+unset($findRoot);
+chdir($root);
+require_once 'vendor/cakephp/cakephp/src/basics.php';
+require_once 'vendor/autoload.php';
+define('ROOT', $root . DS . 'tests' . DS . 'test_app' . DS);
+define('APP', ROOT . 'App' . DS);
+define('TMP', sys_get_temp_dir() . DS);
+Configure::write('debug', true);
+Configure::write('App', [
+    'namespace' => 'App',
+    'paths' => [
+        'plugins' => [ROOT . 'Plugin' . DS],
+        'templates' => [ROOT . 'App' . DS . 'Template' . DS]
+    ]
+]);
+if (!getenv('db_dsn')) {
+    putenv('db_dsn=sqlite:///:memory:');
+}
+ConnectionManager::config('test', ['url' => getenv('db_dsn')]);
+Plugin::load('BEdita/Auth', [
+    'path' => dirname(dirname(__FILE__)) . DS,
+]);
