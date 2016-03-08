b3189c4d1ef5141f00f1401aeb19bbd890b845b3
stefanorosanelli <stefano.rosanelli@gmail.com>
2016-03-08 09:46:51 +0100

    #802 feat: DbAdminShell and DbUtils class

---
+++ plugins/BEdita/Core/src/Shell/DbAdminShell.php
@@ -0,0 +1,164 @@
+<?php
+/**
+ ${BEDITA_LICENSE_HEADER}
+ */
+namespace BEdita\Core\Shell;
+
+use Cake\Console\Shell;
+use Cake\Datasource\ConnectionManager;
+use BEdita\Core\Utils\DbUtils;
+
+/**
+ * Database related shell commands like: 
+ *  - initialize a new databasa instance
+ *  - create schema files
+ *  - check schema consistency
+ */
+class DbAdminShell extends Shell
+{
+
+    /**
+     * Default JSON schema file name
+     *
+     * @var string
+     */
+    const JSON_SCHEMA_FILE = 'be4-schema.json';
+
+    /**
+     * Schema files folder path
+     *
+     * @var string
+     */
+    protected $schemaDir = null;
+
+    /**
+     * Overrides Shell::initialize()
+     */
+    public function initialize()
+    {
+        parent::initialize();
+        $this->schemaDir = ROOT . DS . 'plugins' . DS . 'BEdita' . DS . 'Core' . DS . 'config' . DS . 'schema' . DS;
+    }
+
+    /**
+     * Overrides Shell::getOptionParser()
+     * define arguments and display inline help
+     */
+    public function getOptionParser()
+    {
+        $parser = parent::getOptionParser();
+        $parser->addSubcommand('saveSchema', [
+            'help' => 'Save current database schema to JSON file.',
+            'parser' => [
+                'description' => [
+                    'Use this command to generate a JSON file schema.',
+                    'File is built using current database connection.'
+                ],
+                'options' => [
+                    'output' => [
+                        'help' => 'Specifiy output file path', 
+                        'short' => 'o', 
+                        'required' => false,
+                    ],
+                ]
+            ]
+        ]);
+        $parser->addSubcommand('checkSchema', [
+            'help' => 'Check schema differences between current db and schema JSON file.',
+            'parser' => [
+                'description' => [
+                    'A JSON file schema is generated from current DB connection.',
+                    'This file is compared with the default one' . 
+                    ' in BEdita/Core/config/schema/be4-schema.json.'
+                ],
+            ]
+        ]);
+        $parser->addSubcommand('init', [
+            'help' => 'Create a new BE4 schema on current DB connection.',
+            'parser' => [
+                'description' => [
+                    'A new database schema is created using current DB connection.',
+                    'BEWARE: all existing BE4 tables will be dropped!!'
+                ],
+            ]
+        ]);
+        return $parser;
+    }
+
+    /**
+     * Save schema to file (JSON format) from current db
+     * Generated file is BEdita/Core/config/schema/be4-schema.json)
+     */
+    public function saveSchema() 
+    {
+        if (!empty($this->params['output'])) {
+            $schemaFile = $this->params['output'];
+        } else {
+            $schemaFile = $this->schemaDir . self::JSON_SCHEMA_FILE;
+        }
+        if (file_exists($schemaFile)) {
+                $res = $this->in('Overwrite schema file "' . $schemaFile . '"?', ['y', 'n'], 'n');
+                if ($res != 'y') {
+                    $this->info('Schema file not updated');
+                    return;
+                }
+        }
+        $schemaData = DbUtils::currentSchema();
+        $jsonSchema = json_encode($schemaData, JSON_PRETTY_PRINT);
+        $res = file_put_contents($schemaFile, $jsonSchema);
+        if (!$res) {
+            $this->abort('Error writing schema file ' . $schemaFile);
+        }
+        $this->info('Schema file updated ' . $schemaFile);
+    }
+    
+    /**
+     * Check schema differences between current db and schema JSON file 
+     * (in BEdita/Core/config/schema/be4-schema.json)
+     */
+    public function checkSchema() 
+    {
+        $schemaFile = $this->schemaDir . 'be4-schema.json';
+        $json = file_get_contents($schemaFile);
+        $be4Schema = json_decode($json, true);
+        $currentSchema = DbUtils::currentSchema();
+        $schemaDiff = DbUtils::schemaCompare($be4Schema, $currentSchema);
+        if (empty($schemaDiff)) {
+            $this->info('No schema differences found');
+        } else {
+            $this->out(print_r($schemaDiff, true));
+            $this->error('Schema differences found');
+        }
+    }
+
+    /**
+     * Initialize BE4 database schema
+     * SQL schema in BEdita/Core/config/schema/be4-schema-<vendor>.sql
+     */
+    public function init()
+    {
+        $info = DbUtils::basicInfo();
+        $this->warn('You are about to initialize a new database!!');
+        $this->warn('ALL CURRENT BEDITA4 TABLES WILL BE DROPPED!!');
+        $this->info('Host: ' . $info['host']  . '');
+        $this->info('Database: ' . $info['database']  . '');
+        $this->info('Vendor: ' . $info['vendor']  . '');
+        $res = $this->in('Do you want to proceed?', ['y', 'n'], 'n');
+        if ($res != 'y') {
+            $this->out('Database unchanged');
+            return;
+        }
+        $this->out('Creating new database schema...');
+        $schemaFile = $this->schemaDir . 'be4-schema-' . $info['vendor'] . '.sql';
+        if (!file_exists($schemaFile)) {
+            $this->abort('Schema file not found: ' . $schemaFile);
+        }
+        $sqlSchema = file_get_contents($schemaFile);
+        $result = DbUtils::executeTransaction($sqlSchema);
+        if (!$result['success']) {
+            $this->abort('Error creating database schema: ' . $result['error']);
+        }
+        $this->info('New database schema set');
+    }
+
+}
---
+++ plugins/BEdita/Core/src/Utils/DbUtils.php
@@ -0,0 +1,130 @@
+<?php 
+/**
+ ${BEDITA_LICENSE_HEADER}
+ */
+namespace BEdita\Core\Utils;
+
+use Cake\Datasource\ConnectionManager;
+
+/**
+ * Database utilities class
+ * 
+ * Provides static methods to common db related operations  
+ */
+class DbUtils
+{
+
+    /**
+     * Returns an array with current database schema information (tables, columns, indexes, constraints)
+     * Using $dbConfig database connection ('default' as default)
+     *
+     * @param string $dbConfig input database configuration ('default' as default)
+     * @return array containing complete schema information, table names as keys 
+     *     and details on columns, indexes and constraints for every table
+     */
+    public static function currentSchema($dbConfig = 'default') 
+    {
+        $schema = [];
+        $connection = ConnectionManager::get($dbConfig);
+        $collection = $connection->schemaCollection();
+        $tables = $collection->listTables();
+        foreach ($tables as $tableName) {
+            $schema[$tableName] = [];
+            $table = $collection->describe($tableName);
+            $columns = $table->columns();
+            foreach ($columns as $col) {
+                $schema[$tableName]['columns'][$col] = $table->column($col);
+            }
+            $constraints = $table->constraints();
+            foreach ($constraints as $cons) {
+                $schema[$tableName]['constraints'][$cons] = $table->constraint($cons);
+            }
+            $indexes = $table->indexes();
+            foreach ($indexes as $idx) {
+                $schema[$tableName]['indexes'][$idx] = $table->index($idx);
+            }
+        }
+        return $schema;
+    }
+
+    /**
+     * Compare schema arrays betweend $expected and $current schema metadata
+     * Returns an array with difference details 
+     * 
+     * @param array $expected expected db schema
+     * @param array $current db schema from DbUtils::currentSchema()
+     * @return array containing information on differences found
+     */
+    public static function schemaCompare(array $expected, array $current) 
+    {
+        $diff = [];
+        foreach ($expected as $table => $tableMeta) {
+            if (empty($current[$table])) {
+                $diff['missingTables'][] = $table;
+                continue;
+            }
+            foreach ($tableMeta['columns'] as $column => $colData) {
+                if (empty($current[$table]['columns'][$column])) {
+                    $diff['missingColumns'][] = $table . '.' . $column;
+                }
+                // TODO: column details diff 
+            }
+            if (!empty($tableMeta['constraints'])) {
+                foreach ($tableMeta['constraints'] as $constraint => $constData) {
+                    if (empty($current[$table]['constraints'][$constraint])) {
+                        $diff['missingConstraints'][] = $table . '.' . $constraint;
+                    }
+                    // TODO: constraint details diff 
+                }
+            }
+            if (!empty($tableMeta['indexes'])) {
+                foreach ($tableMeta['indexes'] as $index => $idxData) {
+                    if (empty($current[$table]['indexes'][$index])) {
+                        $diff['missingIndexes'][] = $table . '.' . $index;
+                    }
+                    // TODO: index details diff 
+                }
+            }
+        }
+        return $diff;
+    }
+
+    /**
+     * Get basic database connection info
+     *
+     * @param string $dbConfig input database configuration ('default' as default)
+     * @return array containing requested configuration + 'vendor' key (mysql, sqlite, postgres,...)
+     */
+    public static function basicInfo($dbConfig = 'default') 
+    {
+        $connection = ConnectionManager::get($dbConfig);
+        $config = $connection->config();
+        $config['vendor'] = strtolower(substr($config['driver'], strrpos($config['driver'], '\\') + 1));
+        return $config;
+    }
+
+     /**
+      * Executes SQL query using transactions.
+      * Returns an array providing information on SQL query results
+      *
+      * @param string $sql SQL query to execute.
+      * @return array containg keys: 'success' (boolean), 'error' (string with error message), 
+      *      'rowCount' (number of affected rows) 
+      */
+    public static function executeTransaction($sql, $dbConfig = 'default') 
+    {
+        $res = ['success' => false, 'error' => '', 'rowCount' => 0];
+        try {
+            $connection = ConnectionManager::get($dbConfig);
+            $connection->begin();
+            $statement = $connection->query($sql);
+            $connection->commit();
+            $res['success'] = true;
+            $res['rowCount'] = $statement->rowCount();
+        } catch (Exception $e) {
+            $connection->rollback();
+            $res['error'] = $e->getMessage();
+        }
+        return $res;
+    }
+}
