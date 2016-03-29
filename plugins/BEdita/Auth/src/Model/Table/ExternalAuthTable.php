60d78d946a1c83936adb3e0f9676be111cfa0d18
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2016-03-29 19:21:21 +0200

    #806 fix: enforce unique User/AuthProvider assoc in ExternalAuth

--- plugins/BEdita/Auth/src/Model/Table/ExternalAuthTable.php
+++ plugins/BEdita/Auth/src/Model/Table/ExternalAuthTable.php
@@ -77,6 +77,7 @@ class ExternalAuthTable extends Table
         $rules->add($rules->existsIn(['user_id'], 'Users'));
         $rules->add($rules->existsIn(['auth_provider_id'], 'AuthProviders'));
 
+        $rules->add($rules->isUnique(['user_id', 'auth_provider_id']));
         $rules->add($rules->isUnique(['auth_provider_id', 'username']));
 
         return $rules;
