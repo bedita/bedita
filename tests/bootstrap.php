84cd8a1a4623c179bd3a1a0f5aa45b49b357afba
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2016-03-29 10:58:54 +0200

    [minor] test: fix test configuration

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,11 @@
  * Add additional configuration/setup your application needs when running
  * unit tests in this file.
  */
-require dirname(__DIR__) . '/config/bootstrap.php';
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'bootstrap.php';
+
+use Cake\Datasource\ConnectionManager;
+
+if (getenv('db_dsn')) {
+    ConnectionManager::drop('test');
+    ConnectionManager::config('test', ['url' => getenv('db_dsn')]);
+}
