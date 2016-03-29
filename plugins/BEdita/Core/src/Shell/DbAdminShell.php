5d63c246b282fb438383e00ff69712db7ee86a00
stefanorosanelli <stefano.rosanelli@gmail.com>
2016-03-29 17:58:29 +0200

    [minore] style: LGPL license header

--- plugins/BEdita/Core/src/Shell/DbAdminShell.php
+++ plugins/BEdita/Core/src/Shell/DbAdminShell.php
@@ -1,6 +1,16 @@
 <?php
 /**
- ${BEDITA_LICENSE_HEADER}
+ * BEdita, API-first content management framework
+ * Copyright 2016 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ *
+ * @since 4.0.0
  */
 namespace BEdita\Core\Shell;
 
