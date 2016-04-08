b2165e9f698f43fe8e1ea86bef54a3eb98bafbd2
stefanorosanelli <stefano.rosanelli@gmail.com>
2016-04-08 12:46:21 +0200

    #823 feat: basic & minimal /users endpoint

---
+++ plugins/BEdita/API/config/routes.php
@@ -0,0 +1,13 @@
+<?php
+use Cake\Routing\Router;
+
+Router::plugin(
+    'BEdita/API',
+    ['path' => '/'],
+    function ($routes) {
+
+        $routes->connect('/users', ['controller' => 'Users', 'action' => 'index']);
+
+        $routes->fallbacks('DashedRoute');
+    }
+);
---
+++ plugins/BEdita/API/src/Controller/UsersController.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2016 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+namespace BEdita\API\Controller;
+
+use BEdita\Auth\Model\Entity\User;
+use Cake\ORM\TableRegistry;
+
+/**
+ * Controller for /users endpoint
+ */
+class UsersController extends AppController
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function initialize()
+    {
+        parent::initialize();
+        $this->Users = TableRegistry::get(
+            'Users',
+            TableRegistry::exists('Users') ? [] : ['className' => 'BEdita\Auth\Model\Table\UsersTable']
+        );
+    }
+
+    /**
+     * Paginated users index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        $users = $this->Users->find('all');
+        $this->set([
+            'users' => $users,
+            '_serialize' => ['users']
+        ]);
+    }
+}
