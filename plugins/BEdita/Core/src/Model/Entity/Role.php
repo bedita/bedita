d2567f75825c5572b10ed919996ac1702f2c6adf
stefanorosanelli <stefano.rosanelli@gmail.com>
2016-05-06 19:07:25 +0200

    #849 feat: role entity + table + fixtures + tests

---
+++ plugins/BEdita/Core/src/Model/Entity/Role.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2016 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\Core\Model\Entity;
+
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\ORM\Entity;
+
+/**
+ * Role Entity.
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $description
+ * @property bool $immutable
+ * @property bool $backend_auth
+ * @property \Cake\I18n\Time $created
+ * @property \Cake\I18n\Time $modified
+ * @property \BEdita\Core\Model\Entity\User[] $users
+ *
+ * @since 4.0.0
+ */
+class Role extends Entity
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $_accessible = [
+        '*' => true,
+        'id' => false,
+        'immutable' => false,
+    ];
+}
