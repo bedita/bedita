258cf6bb93ee0f544091f5236f39230839bac77b
Paolo Cuffiani <me@fquff.io>
2016-05-08 11:36:36 +0200

    #840 feat: implement basic JSON API error response

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -18,8 +18,22 @@
  */
 
 use BEdita\API\Controller\Component\JsonApiComponent;
+use Cake\Console\ConsoleErrorHandler;
+use Cake\Core\Configure;
+use Cake\Error\ErrorHandler;
 use Cake\Network\Request;
 
+/** Set API exception renderer. This also requires error handler to be reset. */
+Configure::write('Error.exceptionRenderer', 'BEdita\API\Error\ExceptionRenderer');
+restore_error_handler();
+restore_exception_handler();
+if (PHP_SAPI === 'cli') {
+    (new ConsoleErrorHandler(Configure::read('Error')))->register();
+} else {
+    (new ErrorHandler(Configure::read('Error')))->register();
+}
+
+/** Add custom request detectors. */
 Request::addDetector('html', ['accept' => ['text/html', 'application/xhtml+xml', 'application/xhtml', 'text/xhtml']]);
 Request::addDetector('jsonApi', function (Request $request) {
     return $request->accepts(JsonApiComponent::CONTENT_TYPE);
