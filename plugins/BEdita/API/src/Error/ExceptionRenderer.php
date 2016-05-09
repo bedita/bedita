cbc0c9bc0a4eaea289018ab5c0678eff888541e0
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2016-05-09 14:10:37 +0200

    [minor] chore: fix wrong banner in sources

--- plugins/BEdita/API/src/Error/ExceptionRenderer.php
+++ plugins/BEdita/API/src/Error/ExceptionRenderer.php
@@ -1,20 +1,14 @@
 <?php
 /**
- * BEdita - a semantic content management framework
- * Copyright (C) 2008-2016  Chia Lab s.r.l., Channelweb s.r.l.
+ * BEdita, API-first content management framework
+ * Copyright 2016 ChannelWeb Srl, Chialab Srl
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
  */
 
 namespace BEdita\API\Error;
