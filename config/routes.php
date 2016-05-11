485cbb37e79350a403a46bc2302cfc9bc4c5ca20
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2016-05-11 19:05:50 +0200

    #855 refactor: refactor routing to use `RouteBuilder::resources()`

--- config/routes.php
+++ config/routes.php
@@ -18,9 +18,6 @@ Router::plugin(
     'BEdita/API',
     ['path' => '/'],
     function (RouteBuilder $routes) {
-        $routes->connect('/users', ['controller' => 'Users', 'action' => 'index']);
-        $routes->connect('/users/*', ['controller' => 'Users', 'action' => 'view']);
-
-        $routes->fallbacks('DashedRoute');
+        $routes->resources('Users');
     }
 );
