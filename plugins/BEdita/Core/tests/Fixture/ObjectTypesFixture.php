270d59372fe49c90071346775af49cab880677e6
batopa <batopa@gmail.com>
2016-05-13 18:03:09 +0200

    #838 chore: add object type fixture

---
+++ plugins/BEdita/Core/tests/Fixture/ObjectTypesFixture.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2016 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\Core\Test\Fixture;
+
+use BEdita\Core\TestSuite\Fixture\TestFixture;
+
+/**
+ * Fixture for `object_types` table.
+ */
+class ObjectTypesFixture extends TestFixture
+{
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'name' => 'document',
+            'description' => 'none',
+            'plugin' => 'BEdita/Core',
+            'model' => 'Objects'
+        ],
+    ];
+}
