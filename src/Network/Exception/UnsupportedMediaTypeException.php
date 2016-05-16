e0aa84cbeff0851899f5db1eb38359fb5bb5a35c
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2016-05-16 14:22:18 +0200

    [minor] refactor: move `UnsupportedMediaTypeException`

--- src/Network/Exception/UnsupportedMediaTypeException.php
+++ src/Network/Exception/UnsupportedMediaTypeException.php
@@ -10,7 +10,7 @@
  *
  * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
  */
-namespace BEdita\API\Exception;
+namespace BEdita\API\Network\Exception;
 
 use Cake\Network\Exception\HttpException;
 
