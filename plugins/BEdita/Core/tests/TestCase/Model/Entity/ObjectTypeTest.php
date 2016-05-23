4b8291261ece7a972dc8b36d98cbdaa2fb268428
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2016-05-23 16:54:06 +0200

    #859 test: add tests for `ObjectTypes` model

---
+++ plugins/BEdita/Core/tests/TestCase/Model/Entity/ObjectTypeTest.php
@@ -0,0 +1,230 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2016 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\Core\Test\TestCase\Model\Entity;
+
+use BEdita\Core\Model\Entity\ObjectType;
+use BEdita\Core\Model\Table\ObjectTypesTable;
+use Cake\Cache\Cache;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+
+/**
+ * {@see \BEdita\Core\Model\Entity\ObjectType} Test Case
+ *
+ * @coversDefaultClass \BEdita\Core\Model\Entity\ObjectType
+ */
+class ObjectTypeTest extends TestCase
+{
+
+    /**
+     * Test subject's table
+     *
+     * @var \BEdita\Core\Model\Table\ObjectTypesTable
+     */
+    public $ObjectTypes;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.BEdita/Core.object_types',
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        Cache::clear(false, ObjectTypesTable::CACHE_CONFIG);
+
+        $this->ObjectTypes = TableRegistry::get('BEdita/Core.ObjectTypes');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function tearDown()
+    {
+        unset($this->ObjectTypes);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test accessible properties.
+     *
+     * @return void
+     * @coversNothing
+     */
+    public function testAccessible()
+    {
+        $objectType = $this->ObjectTypes->get(1);
+
+        $data = [
+            'id' => 42,
+            'name' => 'patched_name',
+        ];
+        $objectType = $this->ObjectTypes->patchEntity($objectType, $data);
+        if (!($objectType instanceof ObjectType)) {
+            throw new \InvalidArgumentException();
+        }
+
+        $this->assertEquals(1, $objectType->id);
+        $this->assertEquals('patched_name', $objectType->name);
+    }
+
+    /**
+     * Test virtual properties.
+     *
+     * @return void
+     * @coversNothing
+     */
+    public function testVirtual()
+    {
+        $expected = [
+            'id' => 1,
+            'name' => 'document',
+            'pluralized' => 'documents',
+            'alias' => 'Documents',
+            'description' => null,
+            'plugin' => 'BEdita/Core',
+            'model' => 'Objects',
+            'table' => 'BEdita/Core.Objects',
+        ];
+
+        $objectType = $this->ObjectTypes->get(1);
+        if (!($objectType instanceof ObjectType)) {
+            throw new \InvalidArgumentException();
+        }
+
+        $this->assertEquals($expected, $objectType->toArray());
+    }
+
+    /**
+     * Test setter method for `name`.
+     *
+     * @return void
+     * @covers ::_setName()
+     */
+    public function testSetName()
+    {
+        $data = [
+            'name' => 'FooBar',
+        ];
+        $objectType = $this->ObjectTypes->newEntity($data);
+        if (!($objectType instanceof ObjectType)) {
+            throw new \InvalidArgumentException();
+        }
+
+        $this->assertEquals('foo_bar', $objectType->name);
+    }
+
+    /**
+     * Data provider for `testGetSetPluralized` test case.
+     *
+     * @return array
+     */
+    public function getSetPluralizedProvider()
+    {
+        return [
+            'default' => ['foo_bars', 'foo_bar', 'FooBars'],
+            'missing' => ['foo_bars', 'foo_bar', null],
+        ];
+    }
+
+    /**
+     * Test getter/setter method for `pluralized`.
+     *
+     * @param string $expected Expected result.
+     * @param string $name Object type name.
+     * @param string|null $pluralized Object type pluralized name.
+     * @return void
+     *
+     * @dataProvider getSetPluralizedProvider
+     * @covers ::_getPluralized()
+     * @covers ::_setPluralized()
+     */
+    public function testGetSetPluralized($expected, $name, $pluralized)
+    {
+        $data = compact('name', 'pluralized');
+        $objectType = $this->ObjectTypes->newEntity($data);
+        if (!($objectType instanceof ObjectType)) {
+            throw new \InvalidArgumentException();
+        }
+
+        $this->assertEquals($expected, $objectType->pluralized);
+    }
+
+    /**
+     * Test getter method for `alias`.
+     *
+     * @return void
+     * @covers ::_getAlias()
+     */
+    public function testGetAlias()
+    {
+        $data = [
+            'name' => 'foo_bar',
+        ];
+        $objectType = $this->ObjectTypes->newEntity($data);
+        if (!($objectType instanceof ObjectType)) {
+            throw new \InvalidArgumentException();
+        }
+
+        $this->assertEquals('FooBars', $objectType->alias);
+    }
+
+    /**
+     * Data provider for `testGetSetPluralized` test case.
+     *
+     * @return array
+     */
+    public function getSetTableProvider()
+    {
+        return [
+            'pluginSyntax' => ['BEdita/Core.Objects', 'BEdita/Core', 'Objects', 'BEdita/Core.Objects'],
+            'noPlugin' => ['Objects', null, 'Objects', 'Objects'],
+        ];
+    }
+
+    /**
+     * Test getter/setter method for `table`.
+     *
+     * @param string $expected Expected result.
+     * @param string|null $expectedPlugin Expected plugin.
+     * @param string $expectedModel Expected model.
+     * @param string|null $table Object type table.
+     * @return void
+     *
+     * @dataProvider getSetTableProvider
+     * @covers ::_getTable()
+     * @covers ::_setTable()
+     */
+    public function testGetSetTable($expected, $expectedPlugin, $expectedModel, $table)
+    {
+        $data = compact('table');
+        $objectType = $this->ObjectTypes->newEntity($data);
+        if (!($objectType instanceof ObjectType)) {
+            throw new \InvalidArgumentException();
+        }
+
+        $this->assertEquals($expectedPlugin, $objectType->plugin);
+        $this->assertEquals($expectedModel, $objectType->model);
+        $this->assertEquals($expected, $objectType->table);
+    }
+}
---
+++ plugins/BEdita/Core/tests/TestCase/Model/Table/ObjectTypesTableTest.php
@@ -0,0 +1,279 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2016 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\Core\Test\TestCase\Model\Table;
+
+use BEdita\Core\Model\Table\ObjectTypesTable;
+use Cake\Cache\Cache;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+
+/**
+ * {@see \BEdita\Core\Model\Table\ObjectTypesTable} Test Case
+ *
+ * @coversDefaultClass \BEdita\Core\Model\Table\ObjectTypesTable
+ */
+class ObjectTypesTableTest extends TestCase
+{
+
+    /**
+     * Test subject
+     *
+     * @var \BEdita\Core\Model\Table\ObjectTypesTable
+     */
+    public $ObjectTypes;
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.BEdita/Core.users',
+        'plugin.BEdita/Core.object_types',
+        'plugin.BEdita/Core.objects',
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        Cache::clear(false, ObjectTypesTable::CACHE_CONFIG);
+
+        $this->ObjectTypes = TableRegistry::get('BEdita/Core.ObjectTypes');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function tearDown()
+    {
+        unset($this->ObjectTypes);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test initialization.
+     *
+     * @return void
+     * @coversNothing
+     */
+    public function testInitialization()
+    {
+        $this->ObjectTypes->initialize([]);
+
+        $this->assertEquals('object_types', $this->ObjectTypes->table());
+        $this->assertEquals('id', $this->ObjectTypes->primaryKey());
+        $this->assertEquals('name', $this->ObjectTypes->displayField());
+
+        $this->assertInstanceOf('\Cake\ORM\Association\HasMany', $this->ObjectTypes->Objects);
+    }
+
+    /**
+     * Data provider for `testValidation` test case.
+     *
+     * @return array
+     */
+    public function validationProvider()
+    {
+        return [
+            'valid' => [
+                true,
+                [
+                    'name' => 'foo',
+                    'pluralized' => 'foos',
+                    'plugin' => 'BEdita/Core',
+                    'model' => 'Objects',
+                ],
+            ],
+            'notUnique' => [
+                false,
+                [
+                    'name' => 'document',
+                    'pluralized' => 'many_documents',
+                    'plugin' => 'BEdita/Core',
+                    'model' => 'Objects',
+                ],
+            ],
+            'notUnique2' => [
+                false,
+                [
+                    'name' => 'card',
+                    'pluralized' => 'profiles',
+                    'plugin' => 'BEdita/Core',
+                    'model' => 'Objects',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Test validation.
+     *
+     * @param bool $expected Expected result.
+     * @param array $data Data to be validated.
+     *
+     * @return void
+     * @dataProvider validationProvider
+     * @coversNothing
+     */
+    public function testValidation($expected, array $data)
+    {
+        $objectType = $this->ObjectTypes->newEntity();
+        $this->ObjectTypes->patchEntity($objectType, $data);
+
+        $success = $this->ObjectTypes->save($objectType);
+        $this->assertEquals($expected, (bool)$success);
+    }
+
+    /**
+     * Data provider for `testGet` test case.
+     *
+     * @return array
+     */
+    public function getProvider()
+    {
+        return [
+            'id' => [
+                [
+                    'id' => 1,
+                    'name' => 'document',
+                    'pluralized' => 'documents',
+                    'description' => null,
+                    'alias' => 'Documents',
+                    'plugin' => 'BEdita/Core',
+                    'model' => 'Objects',
+                    'table' => 'BEdita/Core.Objects',
+                ],
+                1,
+            ],
+            'stringId' => [
+                [
+                    'id' => 1,
+                    'name' => 'document',
+                    'pluralized' => 'documents',
+                    'description' => null,
+                    'alias' => 'Documents',
+                    'plugin' => 'BEdita/Core',
+                    'model' => 'Objects',
+                    'table' => 'BEdita/Core.Objects',
+                ],
+                '1',
+            ],
+            'singular' => [
+                [
+                    'id' => 1,
+                    'name' => 'document',
+                    'pluralized' => 'documents',
+                    'description' => null,
+                    'alias' => 'Documents',
+                    'plugin' => 'BEdita/Core',
+                    'model' => 'Objects',
+                    'table' => 'BEdita/Core.Objects',
+                ],
+                'document',
+            ],
+            'plural' => [
+                [
+                    'id' => 1,
+                    'name' => 'document',
+                    'pluralized' => 'documents',
+                    'description' => null,
+                    'alias' => 'Documents',
+                    'plugin' => 'BEdita/Core',
+                    'model' => 'Objects',
+                    'table' => 'BEdita/Core.Objects',
+                ],
+                'documents',
+            ],
+            'missingId' => [
+                false,
+                99,
+            ],
+            'missingType' => [
+                false,
+                'missing_type',
+            ],
+        ];
+    }
+
+    /**
+     * Test get method.
+     *
+     * @param array|false $expected Expected result.
+     * @param string|int $primaryKey Primary key.
+     * @return void
+     *
+     * @dataProvider getProvider
+     * @covers ::get()
+     */
+    public function testGet($expected, $primaryKey)
+    {
+        if (!$expected) {
+            $this->setExpectedException('\Cake\Datasource\Exception\RecordNotFoundException');
+        }
+
+        $entity = $this->ObjectTypes->get($primaryKey);
+
+        $this->assertEquals($expected, $entity->toArray());
+    }
+
+    /**
+     * Test after save callback.
+     *
+     * @return void
+     *
+     * @covers ::afterSave()
+     */
+    public function testInvalidateCacheAfterSave()
+    {
+        $entity = $this->ObjectTypes->get('document');
+
+        $this->assertNotFalse(Cache::read('id_1', ObjectTypesTable::CACHE_CONFIG));
+        $this->assertNotFalse(Cache::read('map', ObjectTypesTable::CACHE_CONFIG));
+        $this->assertNotFalse(Cache::read('map_pluralized', ObjectTypesTable::CACHE_CONFIG));
+
+        $entity = $this->ObjectTypes->patchEntity($entity, ['name' => 'foo', 'pluralized' => 'foos']);
+        $this->ObjectTypes->save($entity);
+
+        $this->assertFalse(Cache::read('id_1', ObjectTypesTable::CACHE_CONFIG));
+        $this->assertFalse(Cache::read('map', ObjectTypesTable::CACHE_CONFIG));
+        $this->assertFalse(Cache::read('map_pluralized', ObjectTypesTable::CACHE_CONFIG));
+    }
+
+    /**
+     * Test after delete callback.
+     *
+     * @return void
+     *
+     * @covers ::afterDelete()
+     */
+    public function testInvalidateCacheAfterDelete()
+    {
+        $entity = $this->ObjectTypes->get('document');
+
+        $this->assertNotFalse(Cache::read('id_1', ObjectTypesTable::CACHE_CONFIG));
+        $this->assertNotFalse(Cache::read('map', ObjectTypesTable::CACHE_CONFIG));
+        $this->assertNotFalse(Cache::read('map_pluralized', ObjectTypesTable::CACHE_CONFIG));
+
+        $this->ObjectTypes->delete($entity);
+
+        $this->assertFalse(Cache::read('id_1', ObjectTypesTable::CACHE_CONFIG));
+        $this->assertFalse(Cache::read('map', ObjectTypesTable::CACHE_CONFIG));
+        $this->assertFalse(Cache::read('map_pluralized', ObjectTypesTable::CACHE_CONFIG));
+    }
+}
