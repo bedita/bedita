a9ac03e4dac12fd7adc8e0ddce66b77aaad518c3
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2016-07-15 13:09:11 +0200

    #913 feat: use migrations dump to obtain schema information

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,8 +11,6 @@ require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR 
 
 $_SERVER['PHP_SELF'] = '/';
 
-use Cake\Core\Configure;
-use Cake\Core\Configure\Engine\JsonConfig;
 use Cake\Datasource\ConnectionManager;
 use Cake\Log\Log;
 
@@ -20,9 +18,6 @@ if (getenv('db_dsn')) {
     ConnectionManager::drop('test');
     ConnectionManager::config('test', ['url' => getenv('db_dsn')]);
 }
-
-// #820 use `ROOT/plugins/BEdita/Core/config/be4-schema.json` to obtain schema.
-Configure::write('schema', (new JsonConfig())->read('BEdita/Core.schema/be4-schema'));
 
 if (getenv('DEBUG_LOG_QUERIES')) {
     ConnectionManager::get('test')->logQueries(true);
