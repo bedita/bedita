ed6dc45449adfae3d558dbe20cad8106fbea1847
batopa <batopa@gmail.com>
2016-08-05 17:23:55 +0200

    #947 chore: try to change exception handler
    not work in tests :(

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -15,6 +15,7 @@ use BEdita\API\Controller\Component\JsonApiComponent;
 use Cake\Console\ConsoleErrorHandler;
 use Cake\Core\Configure;
 use Cake\Error\ErrorHandler;
+use Cake\Event\EventManager;
 use Cake\Network\Request;
 
 /**
@@ -24,15 +25,29 @@ if (!defined('UNIT_TEST_RUN') && (PHP_SAPI !== 'cli')) {
     Configure::load('api', 'database');
 }
 
-/** Set API exception renderer. This also requires error handler to be reset. */
-Configure::write('Error.exceptionRenderer', 'BEdita\API\Error\ExceptionRenderer');
-restore_error_handler();
-restore_exception_handler();
-if (PHP_SAPI === 'cli') {
-    (new ConsoleErrorHandler(Configure::read('Error')))->register();
-} else {
-    (new ErrorHandler(Configure::read('Error')))->register();
-}
+// do not work in unit test!! anyway if an exception was triggered in bootstrap it will not be trapped
+EventManager::instance()->on(
+    'Server.buildMiddleware',
+    function ($event, $middleware) {
+        $count = $middleware->count();
+        for ($i = 0; $i < $count; $i++) {
+            $m = $middleware->get($i);
+            if ($m && $m instanceof Cake\Error\Middleware\ErrorHandlerMiddleware) {
+                $m->renderer = 'BEdita\API\Error\ExceptionRenderer';
+            }
+        }
+    }
+);
+
+/** Set API exception renderer. @todo: verify -> This also requires error handler to be reset. */
+// Configure::write('Error.exceptionRenderer', 'BEdita\API\Error\ExceptionRenderer');
+// restore_error_handler();
+// restore_exception_handler();
+// if (PHP_SAPI === 'cli') {
+//     (new ConsoleErrorHandler(Configure::read('Error')))->register();
+// } else {
+//     (new ErrorHandler(Configure::read('Error')))->register();
+// }
 
 /** Add custom request detectors. */
 Request::addDetector('html', ['accept' => ['text/html', 'application/xhtml+xml', 'application/xhtml', 'text/xhtml']]);
