96f167be8db18020343e1f72cb148681f4bf5359
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2016-08-24 15:17:45 +0200

    #926 test: add integration tests for `ResourcesController::relationships`

---
+++ plugins/BEdita/API/tests/TestCase/Controller/ResourcesControllerTest.php
@@ -0,0 +1,284 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2016 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\API\Test\TestCase\Controller;
+
+use Cake\TestSuite\IntegrationTestCase;
+
+/**
+ * @coversDefaultClass \BEdita\API\Controller\ResourcesController
+ */
+class ResourcesControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.BEdita/Core.roles',
+        'plugin.BEdita/Core.users',
+        'plugin.BEdita/Core.roles_users',
+    ];
+
+    /**
+     * Test relationships method to list existing relationships.
+     *
+     * @return void
+     *
+     * @covers ::relationships()
+     */
+    public function testListAssociations()
+    {
+        $expected = [
+            'links' => [
+                'self' => 'http://api.example.com/roles/1/relationships/users',
+                'home' => 'http://api.example.com/home',
+            ],
+            'data' => [
+                [
+                    'id' => '1',
+                    'type' => 'users',
+                    'links' => [
+                        'self' => 'http://api.example.com/users/1',
+                    ],
+                ],
+            ],
+        ];
+
+        $this->configRequest([
+            'headers' => [
+                'Host' => 'api.example.com',
+                'Accept' => 'application/vnd.api+json',
+            ],
+        ]);
+        $this->get('/roles/1/relationships/users');
+        $result = json_decode($this->_response->body(), true);
+
+        $this->assertResponseCode(200);
+        $this->assertContentType('application/vnd.api+json');
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * Test relationships method to add new relationships.
+     *
+     * @return void
+     *
+     * @covers ::relationships()
+     */
+    public function testAddAssociations()
+    {
+        $expected = [
+            'links' => [
+                'self' => 'http://api.example.com/roles/1/relationships/users',
+                'home' => 'http://api.example.com/home',
+            ],
+            'data' => [
+                [
+                    'id' => '1',
+                    'type' => 'users',
+                    'links' => [
+                        'self' => 'http://api.example.com/users/1',
+                    ],
+                ],
+                [
+                    'id' => '2',
+                    'type' => 'users',
+                    'links' => [
+                        'self' => 'http://api.example.com/users/2',
+                    ],
+                ],
+            ],
+        ];
+
+        $data = [
+            [
+                'id' => '2',
+                'type' => 'users',
+            ],
+        ];
+
+        $this->configRequest([
+            'headers' => [
+                'Host' => 'api.example.com',
+                'Accept' => 'application/vnd.api+json',
+                'Content-Type' => 'application/vnd.api+json',
+            ],
+        ]);
+        $this->post('/roles/1/relationships/users', json_encode(compact('data')));
+        $result = json_decode($this->_response->body(), true);
+
+        $this->assertResponseCode(200);
+        $this->assertContentType('application/vnd.api+json');
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * Test relationships method to delete existing relationships.
+     *
+     * @return void
+     *
+     * @covers ::relationships()
+     */
+    public function testDeleteAssociations()
+    {
+        $expected = [
+            'links' => [
+                'self' => 'http://api.example.com/roles/1/relationships/users',
+                'home' => 'http://api.example.com/home',
+            ],
+            'data' => [],
+        ];
+
+        $data = [
+            [
+                'id' => '1',
+                'type' => 'users',
+            ],
+            [
+                'id' => '2',
+                'type' => 'users',
+            ],
+        ];
+
+        $this->configRequest([
+            'headers' => [
+                'Host' => 'api.example.com',
+                'Accept' => 'application/vnd.api+json',
+                'Content-Type' => 'application/vnd.api+json',
+            ],
+        ]);
+        // Cannot use `IntegrationTestCase::delete()`, as it does not allow sending payload with the request.
+        $this->_sendRequest('/roles/1/relationships/users', 'DELETE', json_encode(compact('data')));
+        $result = json_decode($this->_response->body(), true);
+
+        $this->assertResponseCode(200);
+        $this->assertContentType('application/vnd.api+json');
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * Test relationships method to replace existing relationships.
+     *
+     * @return void
+     *
+     * @covers ::relationships()
+     */
+    public function testSetAssociations()
+    {
+        $expected = [
+            'links' => [
+                'self' => 'http://api.example.com/roles/1/relationships/users',
+                'home' => 'http://api.example.com/home',
+            ],
+            'data' => [
+                [
+                    'id' => '2',
+                    'type' => 'users',
+                    'links' => [
+                        'self' => 'http://api.example.com/users/2',
+                    ],
+                ],
+            ],
+        ];
+
+        $data = [
+            [
+                'id' => '2',
+                'type' => 'users',
+            ],
+        ];
+
+        $this->configRequest([
+            'headers' => [
+                'Host' => 'api.example.com',
+                'Accept' => 'application/vnd.api+json',
+                'Content-Type' => 'application/vnd.api+json',
+            ],
+        ]);
+        $this->patch('/roles/1/relationships/users', json_encode(compact('data')));
+        $result = json_decode($this->_response->body(), true);
+
+        $this->assertResponseCode(200);
+        $this->assertContentType('application/vnd.api+json');
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * Test relationships method to update relationships with a non-existing object ID.
+     *
+     * @return void
+     *
+     * @covers ::relationships()
+     */
+    public function testUpdateAssociationsMissingId()
+    {
+        $expected = [
+            'status' => '400',
+            'title' => 'Record not found in table "users"',
+        ];
+
+        $data = [
+            [
+                'id' => '99',
+                'type' => 'users',
+            ],
+        ];
+
+        $this->configRequest([
+            'headers' => [
+                'Host' => 'api.example.com',
+                'Accept' => 'application/vnd.api+json',
+                'Content-Type' => 'application/vnd.api+json',
+            ],
+        ]);
+        $this->patch('/roles/1/relationships/users', json_encode(compact('data')));
+        $result = json_decode($this->_response->body(), true);
+
+        $this->assertResponseCode(400);
+        $this->assertContentType('application/vnd.api+json');
+        $this->assertArrayHasKey('error', $result);
+        $this->assertArraySubset($expected, $result['error']);
+    }
+
+    /**
+     * Test relationships method with a non-existing association.
+     *
+     * @return void
+     *
+     * @covers ::relationships()
+     */
+    public function testWrongAssociation()
+    {
+        $expected = [
+            'status' => '404',
+            'title' => 'Relationship "this_relationship_does_not_exist" does not exist',
+        ];
+
+        $this->configRequest([
+            'headers' => [
+                'Host' => 'api.example.com',
+                'Accept' => 'application/vnd.api+json',
+            ],
+        ]);
+        $this->get('/roles/1/relationships/this_relationship_does_not_exist');
+        $result = json_decode($this->_response->body(), true);
+
+        $this->assertResponseCode(404);
+        $this->assertContentType('application/vnd.api+json');
+        $this->assertArrayHasKey('error', $result);
+        $this->assertArraySubset($expected, $result['error']);
+    }
+}
