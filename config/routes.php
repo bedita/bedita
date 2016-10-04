ed3e0ff6acdb0860aa9bfa48ab67e0147e890313
stefanorosanelli <stefano.rosanelli@gmail.com>
2016-10-04 15:05:01 +0200

    #982 feat: /auth get + tests

--- config/routes.php
+++ config/routes.php
@@ -134,8 +134,13 @@ Router::plugin(
         // Login.
         $routes->connect(
             '/auth',
-            ['controller' => 'Login', 'action' => 'login'],
-            ['_name' => 'login']
+            ['controller' => 'Login', 'action' => 'login', '_method' => 'POST'],
+            ['_name' => 'login:login']
+        );
+        $routes->connect(
+            '/auth',
+            ['controller' => 'Login', 'action' => 'user', '_method' => 'GET'],
+            ['_name' => 'login:user']
         );
     }
 );
