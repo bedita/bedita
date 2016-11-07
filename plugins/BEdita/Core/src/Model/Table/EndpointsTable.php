75d42325bec05b91d8a21ce5979837705fa40ac4
batopa <batopa@gmail.com>
2016-11-07 18:01:40 +0100

    #967 feat: add endpoints models and tests

---
+++ plugins/BEdita/Core/src/Model/Table/EndpointsTable.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2016 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\Core\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Endpoints Model
+ *
+ * @property \Cake\ORM\Association\BelongsTo $ObjectTypes
+ * @property \Cake\ORM\Association\HasMany $EndpointPermissions
+ *
+ * @since 4.0.0
+ */
+class EndpointsTable extends Table
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @codeCoverageIgnore
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->displayField('name');
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('ObjectTypes');
+        $this->hasMany('EndpointPermissions');
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @codeCoverageIgnore
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmpty('id', 'create')
+
+            ->requirePresence('name', 'create')
+            ->notEmpty('name')
+            ->add('name', 'unique', ['rule' => 'validateUnique', 'provider' => 'table'])
+
+            ->allowEmpty('description')
+
+            ->boolean('enabled')
+            ->notEmpty('enabled');
+
+        return $validator;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @codeCoverageIgnore
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->isUnique(['name']));
+        $rules->add($rules->existsIn(['object_type_id'], 'ObjectTypes'));
+
+        return $rules;
+    }
+}
