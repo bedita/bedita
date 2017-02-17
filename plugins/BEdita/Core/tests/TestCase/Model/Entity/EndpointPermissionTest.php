6ab79f0afa68fea9f759c095e9ca9374de139c22
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2017-02-17 13:08:32 +0100

    #968 test: add some unit tests

--- plugins/BEdita/Core/tests/TestCase/Model/Entity/EndpointPermissionTest.php
+++ plugins/BEdita/Core/tests/TestCase/Model/Entity/EndpointPermissionTest.php
@@ -87,4 +87,137 @@ class EndpointPermissionTest extends TestCase
 
         $this->assertEquals(1, $endpointPermission->id);
     }
+
+    /**
+     * Data provider for `testEncode` test case.
+     *
+     * @return array
+     */
+    public function encodeProvider()
+    {
+        return [
+            'no' => [EndpointPermission::PERM_NO, false],
+            'block' => [EndpointPermission::PERM_BLOCK, 'block'],
+            'mine' => [EndpointPermission::PERM_MINE, 'mine'],
+            'yes' => [EndpointPermission::PERM_YES, 'true'],
+            'invalid' => [EndpointPermission::PERM_NO, 'INVALID'],
+        ];
+    }
+
+    /**
+     * Test encoding of permission values.
+     *
+     * @param int $expected Encoded value.
+     * @param string|bool $decoded Decoded value.
+     * @return void
+     *
+     * @covers ::encode()
+     * @dataProvider encodeProvider()
+     */
+    public function testEncode($expected, $decoded)
+    {
+        $encoded = EndpointPermission::encode($decoded);
+
+        static::assertSame($expected, $encoded);
+    }
+
+    /**
+     * Data provider for `testDecode` test case.
+     *
+     * @return array
+     */
+    public function decodeProvider()
+    {
+        return [
+            'no' => [false, EndpointPermission::PERM_NO],
+            'block' => ['block', EndpointPermission::PERM_BLOCK],
+            'mine' => ['mine', EndpointPermission::PERM_MINE],
+            'yes' => [true, (string)EndpointPermission::PERM_YES],
+            'invalid' => [false, 'INVALID'],
+        ];
+    }
+
+    /**
+     * Test decoding of permission values.
+     *
+     * @param string|bool $expected Decoded value.
+     * @param int $encoded Encoded value.
+     * @return void
+     *
+     * @covers ::decode()
+     * @dataProvider decodeProvider()
+     */
+    public function testDecode($expected, $encoded)
+    {
+        $decoded = EndpointPermission::decode($encoded);
+
+        static::assertSame($expected, $decoded);
+    }
+
+    public function setPermissionProvider()
+    {
+        return [
+            'integer' => [
+                0b0101,
+                0b0101,
+                0b1111,
+            ],
+            'array' => [
+                0b1101,
+                [
+                    'read' => 'mine',
+                    'write' => true,
+                ],
+            ],
+            'read' => [
+                0b0101,
+                [
+                    'read' => 'mine',
+                ],
+                0b0111,
+            ],
+            'write' => [
+                0b0111,
+                [
+                    'write' => 'mine',
+                ],
+                0b1011,
+            ],
+            'invalid' => [
+                0,
+                'invalid',
+            ],
+            'negative' => [
+                0,
+                -1,
+            ],
+            'decimal' => [
+                0b0011,
+                pi(),
+            ],
+            'tooHigh' => [
+                0b1111,
+                PHP_INT_MAX,
+            ],
+        ];
+    }
+
+    /**
+     * @param int $expected Expected permission value.
+     * @param mixed $permission Permission to set.
+     * @param int $initial Initial value.
+     *
+     * @covers ::_setPermission()
+     * @dataProvider setPermissionProvider()
+     */
+    public function testSetPermission($expected, $permission, $initial = 0)
+    {
+        $entity = new EndpointPermission([
+            'permission' => $initial,
+        ]);
+
+        $entity->set('permission', $permission);
+
+        static::assertEquals($expected, $entity->get('permission'));
+    }
 }
