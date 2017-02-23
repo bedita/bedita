60af7bd84780155e27e7fb3376bf4d614dfc34a6
stefanorosanelli <stefano.rosanelli@gmail.com>
2017-02-23 16:51:24 +0100

    #1066 chore: header

--- plugins/BEdita/Core/src/Model/Table/LocationsTable.php
+++ plugins/BEdita/Core/src/Model/Table/LocationsTable.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2017 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
 namespace BEdita\Core\Model\Table;
 
 use BEdita\Core\ORM\Inheritance\Table;
