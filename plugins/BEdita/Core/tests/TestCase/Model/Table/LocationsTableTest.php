5d5a71e2f81285e55affef31f519e57e97692a21
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2017-02-27 18:27:24 +0100

    #1076 test: load missing fixtures

--- plugins/BEdita/Core/tests/TestCase/Model/Table/LocationsTableTest.php
+++ plugins/BEdita/Core/tests/TestCase/Model/Table/LocationsTableTest.php
@@ -25,6 +25,8 @@ class LocationsTableTest extends TestCase
      */
     public $fixtures = [
         'plugin.BEdita/Core.object_types',
+        'plugin.BEdita/Core.relations',
+        'plugin.BEdita/Core.relation_types',
         'plugin.BEdita/Core.objects',
         'plugin.BEdita/Core.locations'
     ];
