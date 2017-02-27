f91d9876cc9cc5e35410bba2e9bad082c2ecdfe4
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2017-02-27 16:45:35 +0100

    refactor: by default allow requests without an API key & be permissive
    If no records are found in `endpoint_permissions`, all permissions are automatically granted.

--- plugins/BEdita/API/src/Auth/EndpointAuthorize.php
+++ plugins/BEdita/API/src/Auth/EndpointAuthorize.php
@@ -36,6 +36,7 @@ class EndpointAuthorize extends BaseAuthorize
      * {@inheritDoc}
      */
     protected $_defaultConfig = [
+        'disallowAnonymousApplications' => false,
         'apiKeyHeaderName' => 'X-Api-Key',
     ];
 
@@ -103,10 +104,13 @@ class EndpointAuthorize extends BaseAuthorize
     {
         $application = CurrentApplication::getApplication();
         if ($application === null) {
+            $header = $this->request->getHeaderLine($this->_config['apiKeyHeaderName']);
+            if (empty($header) && empty($this->_config['disallowAnonymousApplications'])) {
+                return null;
+            }
+
             try {
-                CurrentApplication::setFromApiKey(
-                    $this->request->getHeaderLine($this->_config['apiKeyHeaderName'])
-                );
+                CurrentApplication::setFromApiKey($header);
             } catch (\BadMethodCallException $e) {
                 throw new ForbiddenException(__d('bedita', 'Missing API key'));
             } catch (RecordNotFoundException $e) {
@@ -153,15 +157,15 @@ class EndpointAuthorize extends BaseAuthorize
      * Get list of applicable permissions.
      *
      * @param mixed $user Authenticated (or anonymous) user.
-     * @param \BEdita\Core\Model\Entity\Application $application Current application.
+     * @param \BEdita\Core\Model\Entity\Application|null $application Current application.
      * @param \BEdita\Core\Model\Entity\Endpoint|null $endpoint Current endpoint.
      * @return \Cake\ORM\Query
      * @todo Future optimization: Permissions that are `0` on the two bits that are interesting for the current request can be excluded...
      */
-    protected function getPermissions($user, Application $application, Endpoint $endpoint = null)
+    protected function getPermissions($user, Application $application = null, Endpoint $endpoint = null)
     {
         $roleIds = Hash::extract($user, 'roles.{n}.id');
-        $applicationId = $application->id;
+        $applicationId = $application ? $application->id : null;
         $endpointIds = $endpoint ? [$endpoint->id] : [];
 
         return TableRegistry::get('EndpointPermissions')
@@ -178,12 +182,20 @@ class EndpointAuthorize extends BaseAuthorize
      */
     protected function checkPermissions(array $permissions)
     {
+        $result = EndpointPermission::PERM_NO;
+        if (empty($permissions)) {
+            $count = TableRegistry::get('EndpointPermissions')->find()->count();
+            if ($count === 0) {
+                $result = EndpointPermission::PERM_YES;
+            }
+            return EndpointPermission::decode($result);
+        }
+
         $shift = EndpointPermission::PERM_READ;
         if (!$this->request->is(['get', 'head'])) {
             $shift = EndpointPermission::PERM_WRITE;
         }
 
-        $result = EndpointPermission::PERM_NO;
         foreach ($permissions as $permission) {
             $permission = $permission->permission >> $shift & EndpointPermission::PERM_YES;
             $result = $result | $permission;
