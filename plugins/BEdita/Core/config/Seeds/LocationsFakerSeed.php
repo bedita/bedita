a78109da2c482a282f3198076b57a7beb743063a
stefanorosanelli <stefano.rosanelli@gmail.com>
2017-03-06 11:23:51 +0100

    [minor] chore: locations faker seed

---
+++ plugins/BEdita/Core/config/Seeds/LocationsFakerSeed.php
@@ -0,0 +1,25 @@
+<?php
+use BEdita\Core\Utility\ObjectsHandler;
+use Faker\Factory;
+use Migrations\AbstractSeed;
+
+class LocationsFakerSeed extends AbstractSeed
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function run()
+    {
+        $faker = Factory::create();
+        $data = [
+            'title' => $faker->name,
+            'coords' => 'POINT (' . $faker->latitude . ', ' . $faker->longitude . ')',
+            'address' => $faker->streetAddress,
+            'locality' => $faker->city,
+            'postal_code' => $faker->postcode,
+            'country' => $faker->country,
+            'region' => $faker->stateAbbr,
+        ];
+        ObjectsHandler::create('locations', $data);
+    }
+}
