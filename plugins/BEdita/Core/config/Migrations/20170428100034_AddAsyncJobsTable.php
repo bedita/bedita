d8fe638a9b326ef6a27e6fc9322dd29c42ac613d
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2017-05-02 10:52:46 +0200

    #1201 docs: add comments to schema columns

--- plugins/BEdita/Core/config/Migrations/20170428100034_AddAsyncJobsTable.php
+++ plugins/BEdita/Core/config/Migrations/20170428100034_AddAsyncJobsTable.php
@@ -17,57 +17,68 @@ class AddAsyncJobsTable extends AbstractMigration
             ->addColumn('uuid', 'uuid', [
                 'limit' => null,
                 'null' => false,
+                'comment' => 'Unique identifier for the job',
             ])
             ->addColumn('service', 'string', [
                 'limit' => 255,
                 'null' => false,
+                'comment' => 'Service type',
             ])
             ->addColumn('priority', 'integer', [
                 'default' => '0',
                 'limit' => 3,
                 'null' => false,
                 'signed' => false,
+                'comment' => 'Job priority - jobs with higher priority run first',
             ])
             ->addColumn('payload', 'text', [
                 'default' => null,
                 'limit' => null,
                 'null' => true,
+                'comment' => 'JSON payload being passed to job runner',
             ])
             ->addColumn('not_before', 'timestamp', [
                 'default' => null,
                 'limit' => null,
                 'null' => true,
+                'comment' => 'Timestamp at which this job becomes valid',
             ])
             ->addColumn('not_after', 'timestamp', [
                 'default' => null,
                 'limit' => null,
                 'null' => true,
+                'comment' => 'Timestamp at which this job becomes expired',
             ])
             ->addColumn('max_attempts', 'integer', [
                 'default' => '1',
                 'limit' => 3,
                 'null' => false,
                 'signed' => false,
+                'comment' => 'Maximum number of attempts left for this job - it is decremented after each try',
             ])
             ->addColumn('locked_until', 'timestamp', [
                 'default' => null,
                 'limit' => null,
                 'null' => true,
+                'comment' => 'Timestamp at which the lock expires - runners lock jobs for a given amount of time',
             ])
             ->addColumn('created', 'timestamp', [
                 'default' => 'CURRENT_TIMESTAMP',
                 'limit' => null,
                 'null' => false,
+                'comment' => 'Timestamp at which this job was created',
             ])
             ->addColumn('modified', 'timestamp', [
                 'default' => 'CURRENT_TIMESTAMP',
                 'limit' => null,
                 'null' => false,
+                'comment' => 'Timestamp at which this job was last modified',
             ])
             ->addColumn('completed', 'timestamp', [
                 'default' => null,
                 'limit' => null,
                 'null' => true,
+                'comment' => 'Timestamp at which this job was marked as completed',
             ])
             ->addIndex(
                 [
