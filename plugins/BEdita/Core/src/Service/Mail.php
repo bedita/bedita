75b129c1f7532f78e0f9783ce5a56dc6e71c81bd
stefanorosanelli <stefano.rosanelli@gmail.com>
2017-05-03 12:42:17 +0200

    chore: fake email service + test

---
+++ plugins/BEdita/Core/src/Service/Mail.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2017 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\Core\Service;
+
+use BEdita\Core\Job\JobService;
+
+/**
+ * Send single mail
+ *
+ * @since 4.0.0
+ */
+class Mail implements JobService
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public function run($payload, $options = [])
+    {
+        return true;
+    }
+}
