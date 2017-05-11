4fdab20ebd98270f3f761a69e18716e3d7155b57
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2017-05-11 20:31:07 +0200

    #1227 refactor: refactor async jobs running policy

--- plugins/BEdita/Core/src/Shell/JobsShell.php
+++ plugins/BEdita/Core/src/Shell/JobsShell.php
@@ -12,16 +12,23 @@
  */
 namespace BEdita\Core\Shell;
 
-use BEdita\Core\Job\ServiceRunner;
 use Cake\Console\Shell;
+use Cake\Datasource\Exception\RecordNotFoundException;
 
 /**
  * Shell class to run pending jobs
  *
  * @since 4.0.0
+ *
+ * @property \BEdita\Core\Model\Table\AsyncJobsTable $AsyncJobs
  */
 class JobsShell extends Shell
 {
+
+    /**
+     * {@inheritDoc}
+     */
+    public $modelClass = 'AsyncJobs';
 
     /**
      * {@inheritDoc}
@@ -30,46 +37,130 @@ class JobsShell extends Shell
      */
     public function getOptionParser()
     {
-        $parser = parent::getOptionParser();
-        $parser
+        $options = [
+            'exit-on-error' => [
+                'help' => 'Enable "fail hard" mode',
+                'short' => 'F',
+                'boolean' => true,
+            ],
+        ];
+
+        return parent::getOptionParser()
             ->addSubcommand('run', [
+                'help' => 'Process a job',
+                'parser' => [
+                    'arguments' => [
+                        'uuid' => [
+                            'help' => 'UUID of job to be processed',
+                            'required' => true,
+                        ],
+                    ],
+                    'options' => $options,
+                ],
+            ])
+            ->addSubcommand('pending', [
                 'help' => 'Run pending async jobs.',
                 'parser' => [
-                    'options' => [
+                    'options' => $options + [
                         'limit' => [
-                            'help' => 'Max number of jobs to run',
+                            'help' => 'Limit number of jobs being run',
                             'short' => 'l',
+                            'required' => false,
+                            'default' => null,
+                        ],
+                        'min-priority' => [
+                            'help' => 'Run only pending jobs with a priority higher than this value',
+                            'short' => 'p',
+                            'required' => false,
+                            'default' => null,
+                        ],
+                        'service' => [
+                            'help' => 'Run only pending jobs for the supplied service',
+                            'short' => 's',
                             'required' => false,
                             'default' => null,
                         ],
                     ],
                 ],
             ]);
-
-        return $parser;
     }
 
     /**
-     * Run async pending jobs
+     * Process a single pending job.
      *
-     * @return bool False on at least a job failure, true otherwise
+     * @param string $uuid Job UUID.
+     * @return void
      */
-    public function run()
+    public function run($uuid)
     {
-        $limit = $this->param('limit');
-        if (!$limit) {
-            $this->out('Running all pending jobs...');
-        } else {
-            $this->out(sprintf('Running pending jobs, limit number: %s', $limit));
-        }
-        $result = ServiceRunner::runPending($limit);
-        $this->out('<info>Jobs terminated</info>');
-        $this->hr();
-        $this->out(sprintf('Executed: %s', $result['count']));
-        $this->out(sprintf('Success:  %s', count($result['success'])));
-        $this->out(sprintf('Failure:  %s', count($result['failure'])));
-        $this->hr();
+        try {
+            $this->verbose(sprintf('=====> Locking job "<info>%s</info>"...', $uuid));
+            $asyncJob = $this->AsyncJobs->lock($uuid);
+        } catch (RecordNotFoundException $e) {
+            $this->out(sprintf('=====> <warning>Could not obtain lock on job "%s".</warning>', $uuid));
 
-        return empty($result['failure']);
+            return;
+        }
+
+        $this->out(sprintf('=====> Processing job "<info>%s</info>" [%s]...', $asyncJob->uuid, $asyncJob->service));
+        try {
+            $success = $asyncJob->run();
+        } catch (\Exception $e) {
+            $success = false;
+
+            $this->log($e);
+            $this->err(sprintf('=====> %s with message "%s"', get_class($e), $e->getMessage()));
+        } finally {
+            if ($success === false) {
+                $this->log(sprintf('Job "%s" [%s] failed', $asyncJob->uuid, $asyncJob->service));
+
+                $this->err(sprintf('=====> Job "%s" [%s] failed', $asyncJob->uuid, $asyncJob->service));
+            } else {
+                $this->out(
+                    sprintf(
+                        '=====> <success>Job "%s" [%s] completed successfully</success>',
+                        $asyncJob->uuid,
+                        $asyncJob->service
+                    )
+                );
+            }
+
+            $this->verbose(sprintf('=====> Unlocking job "<info>%s</info>"...', $asyncJob->uuid));
+            $this->AsyncJobs->unlock($asyncJob->uuid, $success);
+        }
+
+        if ($success === false && $this->param('exit-on-error')) {
+            $this->abort('=====> Aborting');
+        }
+    }
+
+    /**
+     * Run pending jobs, optionally filtered by status or priority.
+     *
+     * @return void
+     */
+    public function pending()
+    {
+        $this->out('=====> <info>Finding pending jobs...</info>');
+        $query = $this->AsyncJobs
+            ->find('list', ['valueField' => $this->AsyncJobs->getPrimaryKey()])
+            ->find('priority', [
+                'priority' => $this->param('min-priority'),
+                'service' => $this->param('service'),
+            ])
+            ->enableBufferedResults(false);
+        if ($this->param('limit') !== null) {
+            $query = $query->limit($this->param('limit'));
+        }
+
+        if ($query->isEmpty()) {
+            $this->out('=====> <info>Nothing to do</info>');
+
+            return;
+        }
+
+        $query->each([$this, 'run']);
+
+        $this->out('=====> <success>Operation complete</success>');
     }
 }
--- plugins/BEdita/Core/tests/TestCase/Shell/JobsShellTest.php
+++ plugins/BEdita/Core/tests/TestCase/Shell/JobsShellTest.php
@@ -1,26 +1,30 @@
 <?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2017 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
 namespace BEdita\Core\Test\TestCase\Shell;
 
 use BEdita\Core\Job\JobService;
-use BEdita\Core\Job\ServiceRunner;
-use BEdita\Core\Shell\JobsShell;
+use BEdita\Core\Job\ServiceRegistry;
 use BEdita\Core\TestSuite\ShellTestCase;
-use Cake\ORM\TableRegistry;
 
-class Example implements JobService
-{
-    public function run($payload, $options = [])
-    {
-        return true;
-    }
-}
 /**
- * \BEdita\Core\Shell\JobsShell Test Case
+ * {@see \BEdita\Core\Shell\JobsShell} Test Case
  *
  * @coversDefaultClass \BEdita\Core\Shell\JobsShell
  */
 class JobsShellTest extends ShellTestCase
 {
+
     /**
      * Fixtures
      *
@@ -31,41 +35,176 @@ class JobsShellTest extends ShellTestCase
     ];
 
     /**
-     * Test run method
+     * {@inheritDoc}
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        ServiceRegistry::reset();
+    }
+
+    /**
+     * Get mock service.
+     *
+     * @param bool|\Exception $return Return value for `run()` method.
+     * @return \BEdita\Core\Job\JobService
+     */
+    protected function getMockService($return = true)
+    {
+        $service = $this->getMockBuilder(JobService::class)
+            ->getMock();
+
+        $method = $service->method('run');
+        $method->will(static::returnValue($return));
+        if ($return instanceof \Exception) {
+            $method->willThrowException($return);
+        }
+
+        return $service;
+    }
+
+    /**
+     * Test run method.
      *
      * @return void
      * @covers ::run()
      */
     public function testRun()
     {
-        ServiceRunner::register('example', new Example());
-        $io = $this->getMockBuilder('Cake\Console\ConsoleIo')->getMock();
+        $uuid = 'd6bb8c84-6b29-432e-bb84-c3c4b2c1b99c';
+        ServiceRegistry::set('example', $this->getMockService());
 
-        // invoke without limit
-        $result = $this->invoke(['jobs', 'run'], [], $io);
-        $this->assertTrue($result);
+        $result = $this->invoke(['jobs', 'run', $uuid]);
 
-        // invoke with limit
-        $this->invoke(['jobs', 'run', '--limit', '10'], [], $io);
-        $this->assertTrue($result);
+        $this->assertNotAborted();
+        $this->assertOutputContains('completed successfully');
+        static::assertNull($result);
     }
 
     /**
-     * Test run failure
+     * Test run method with invalid job.
      *
      * @return void
      * @covers ::run()
      */
-    public function testRunFail()
+    public function testRunInvalid()
     {
-        $mockService = $this->getMockBuilder(Example::class)->getMock();
-        $mockService->method('run')
-             ->will($this->returnValue(false));
-        ServiceRunner::register('example', $mockService);
+        $uuid = 'gustavo';
+        ServiceRegistry::set('example', $this->getMockService());
 
-        $io = $this->getMockBuilder('Cake\Console\ConsoleIo')->getMock();
+        $result = $this->invoke(['jobs', 'run', $uuid]);
 
-        $result = $this->invoke(['jobs', 'run'], [], $io);
-        $this->assertFalse($result);
+        $this->assertNotAborted();
+        $this->assertOutputContains('Could not obtain lock');
+        static::assertNull($result);
+    }
+
+    /**
+     * Test run method with smooth failure.
+     *
+     * @return void
+     * @covers ::run()
+     */
+    public function testRunFailException()
+    {
+        $exception = new \BadMethodCallException('example');
+        $uuid = 'd6bb8c84-6b29-432e-bb84-c3c4b2c1b99c';
+        ServiceRegistry::set('example', $this->getMockService($exception));
+
+        $result = $this->invoke(['jobs', 'run', $uuid]);
+
+        $this->assertNotAborted();
+        $this->assertErrorContains('BadMethodCallException with message "example"');
+        $this->assertErrorContains('failed');
+        static::assertNull($result);
+    }
+
+    /**
+     * Test run method with smooth failure.
+     *
+     * @return void
+     * @covers ::run()
+     */
+    public function testRunFailSmooth()
+    {
+        $uuid = 'd6bb8c84-6b29-432e-bb84-c3c4b2c1b99c';
+        ServiceRegistry::set('example', $this->getMockService(false));
+
+        $result = $this->invoke(['jobs', 'run', $uuid]);
+
+        $this->assertNotAborted();
+        $this->assertErrorContains('failed');
+        static::assertNull($result);
+    }
+
+    /**
+     * Test run method with fail hard mode.
+     *
+     * @return void
+     * @covers ::run()
+     */
+    public function testRunFailHard()
+    {
+        $uuid = 'd6bb8c84-6b29-432e-bb84-c3c4b2c1b99c';
+        ServiceRegistry::set('example', $this->getMockService(false));
+
+        $result = $this->invoke(['jobs', 'run', '-F', $uuid]);
+
+        $this->assertAborted();
+        $this->assertErrorContains('failed');
+        static::assertSame(1, $result);
+    }
+
+    /**
+     * Test run pending jobs.
+     *
+     * @return void
+     * @covers ::pending()
+     */
+    public function testPending()
+    {
+        ServiceRegistry::set('example', $this->getMockService());
+
+        $result = $this->invoke(['jobs', 'pending']);
+
+        $this->assertNotAborted();
+        $this->assertOutputContains('completed successfully');
+        $this->assertOutputContains('Operation complete');
+        static::assertNull($result);
+    }
+
+    /**
+     * Test run pending jobs with no pending jobs to run.
+     *
+     * @return void
+     * @covers ::pending()
+     */
+    public function testPendingEmpty()
+    {
+        ServiceRegistry::set('example', $this->getMockService());
+
+        $result = $this->invoke(['jobs', 'pending', '--limit', '0']);
+
+        $this->assertNotAborted();
+        $this->assertOutputContains('Nothing to do');
+        static::assertNull($result);
+    }
+
+    /**
+     * Test run pending jobs with fail hard mode.
+     *
+     * @return void
+     * @covers ::pending()
+     */
+    public function testPendingFailHard()
+    {
+        ServiceRegistry::set('example', $this->getMockService(false));
+
+        $result = $this->invoke(['jobs', 'pending', '-F']);
+
+        $this->assertAborted();
+        $this->assertErrorContains('failed');
+        static::assertSame(1, $result);
     }
 }
