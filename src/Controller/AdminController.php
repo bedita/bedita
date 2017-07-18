70eb060ae938d44b4e06924a21761be75d2da1f3
stefanorosanelli <stefano.rosanelli@gmail.com>
2017-07-18 14:53:39 +0200

    #1292 feat: /admin endpoint handle `applications`

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2017 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+namespace BEdita\API\Controller;
+
+use Cake\Routing\Exception\MissingRouteException;
+use Cake\Routing\Router;
+use Cake\Utility\Inflector;
+
+/**
+ * Controller for `/admin` endpoint.
+ *
+ * @since 4.0.0
+ *
+ * @property \BEdita\Core\Model\Table\RolesTable $Roles
+ */
+class AdminController extends ResourcesController
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public $modelClass = null;
+
+    /**
+     * Resource name, one of `$this->config('allowedResources')`
+     */
+    public $resourceName = null;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $_defaultConfig = [
+        'allowedResources' => [
+            'applications',
+            // 'endpoints',
+            // 'config'
+        ],
+    ];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function initialize()
+    {
+        $this->resourceName = $this->request->getParam('item');
+        if (!in_array($this->resourceName, $this->config('allowedResources'))) {
+            throw new MissingRouteException(['url' => $this->request->getRequestTarget()]);
+        }
+
+        $this->modelClass = Inflector::camelize($this->resourceName);
+        if (in_array($this->request->getParam('action'), ['related', 'relationships'])) {
+            $name = $this->request->getParam('relationship');
+            $this->setConfig(sprintf('allowedAssociations.%s', $name), []);
+        }
+
+        parent::initialize();
+
+        if (isset($this->JsonApi)) {
+            $this->JsonApi->setConfig('resourceTypes', [$this->resourceName]);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function resourceUrl($id)
+    {
+        return Router::url(
+            [
+                '_name' => 'api:admin:resource',
+                'item' => $this->resourceName,
+                'id' => $id,
+                'controller' => $this->name,
+            ],
+            true
+        );
+    }
+}
