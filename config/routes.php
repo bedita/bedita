c0d37993fc78d2e21cedd84f09c3ed5798b94ac0
Paolo Cuffiani <paolo.cuffiani@gmail.com>
2017-07-28 16:21:52 +0200

    refactor: replace deprecated methods with new ones

--- config/routes.php
+++ config/routes.php
@@ -29,7 +29,7 @@ Router::plugin(
             'streams',
             'users',
         ];
-        $routes->routeClass(InflectedRoute::class);
+        $routes->setRouteClass(InflectedRoute::class);
 
         // Home.
         $routes->redirect(
