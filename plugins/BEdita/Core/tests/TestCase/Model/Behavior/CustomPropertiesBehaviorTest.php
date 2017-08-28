b5d57bd62933dc7468269b07ba871a52b97af525
stefanorosanelli <stefano.rosanelli@gmail.com>
2017-08-28 18:19:24 +0200

    #1327 test: avoid fixture insert error on postgres

--- plugins/BEdita/Core/tests/TestCase/Model/Behavior/CustomPropertiesBehaviorTest.php
+++ plugins/BEdita/Core/tests/TestCase/Model/Behavior/CustomPropertiesBehaviorTest.php
@@ -31,12 +31,13 @@ class CustomPropertiesBehaviorTest extends TestCase
      */
     public $fixtures = [
         'plugin.BEdita/Core.object_types',
+        'plugin.BEdita/Core.property_types',
+        'plugin.BEdita/Core.properties',
         'plugin.BEdita/Core.relations',
         'plugin.BEdita/Core.relation_types',
         'plugin.BEdita/Core.objects',
         'plugin.BEdita/Core.profiles',
         'plugin.BEdita/Core.users',
-        'plugin.BEdita/Core.properties',
     ];
 
     /**
