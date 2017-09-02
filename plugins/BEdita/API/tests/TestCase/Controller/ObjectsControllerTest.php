3917ec63b027f4cd375ce8111732dd940ff7dfd5
Paolo Cuffiani <paolo.cuffiani@gmail.com>
2017-09-02 15:08:51 +0200

    #1340 test: fix integration test cases after fixture update

--- plugins/BEdita/API/tests/TestCase/Controller/ObjectsControllerTest.php
+++ plugins/BEdita/API/tests/TestCase/Controller/ObjectsControllerTest.php
@@ -225,6 +225,8 @@ class ObjectsControllerTest extends IntegrationTestCase
                         'lang' => 'eng',
                         'publish_start' => null,
                         'publish_end' => null,
+                        'another_username' => 'synapse', // custom property
+                        'another_email' => 'synapse@example.org', // custom property
                     ],
                     'meta' => [
                         'locked' => false,
