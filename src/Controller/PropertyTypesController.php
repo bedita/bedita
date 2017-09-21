18c2726f48e1b29e049f59b558a22868fdb90580
stefanorosanelli <stefano.rosanelli@gmail.com>
2017-09-21 12:35:13 +0200

    #1348 feat: add `/model/property_types`

---
+++ src/Controller/PropertyTypesController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2017 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+namespace BEdita\API\Controller;
+
+/**
+ * Controller for `/model/property_types` endpoint.
+ *
+ * @since 4.0.0
+ *
+ * @property \BEdita\Core\Model\Table\PropertyTypesTable $PropertyTypes
+ */
+class PropertyTypesController extends ResourcesController
+{
+
+    /**
+     * {@inheritDoc}
+     */
+    public $modelClass = 'PropertyTypes';
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $_routeNamePrefix = 'api:model';
+
+    /**
+     * {@inheritDoc}
+     */
+    protected $_defaultConfig = [
+        'allowedAssociations' => [
+            'properties' => ['properties'],
+        ],
+    ];
+}
