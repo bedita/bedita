02ec25781ec68fd5df81f63b8518bf034b47842e
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2017-11-07 18:13:06 +0100

    #1345 refactor: properly hide relationship btw object types & properties

--- plugins/BEdita/Core/src/Model/Entity/ObjectType.php
+++ plugins/BEdita/Core/src/Model/Entity/ObjectType.php
@@ -79,6 +79,7 @@ class ObjectType extends Entity implements JsonApiSerializable
      */
     protected $_hidden = [
         'objects',
+        'properties',
         'parent_id',
         'tree_left',
         'tree_right',
