3b8d61a63acc4d22fc2383b1368b49a0016e71b7
Paolo Cuffiani <paolo.cuffiani@gmail.com>
2017-11-08 19:01:55 +0100

    #1379 feat: introduce validation + cache invalidation for property types
    (cherry picked from commit 8aec537)

--- plugins/BEdita/Core/src/Model/Table/PropertyTypesTable.php
+++ plugins/BEdita/Core/src/Model/Table/PropertyTypesTable.php
@@ -13,13 +13,27 @@
 
 namespace BEdita\Core\Model\Table;
 
+use BEdita\Core\Model\Validation\Validation;
+use Cake\Cache\Cache;
 use Cake\Database\Schema\TableSchema;
+use Cake\Datasource\EntityInterface;
+use Cake\Event\Event;
+use Cake\Network\Exception\ForbiddenException;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
 
 /**
  * Property Types - available property types
+ *
+ * @method \BEdita\Core\Model\Entity\Property newEntity($data = null, array $options = [])
+ * @method \BEdita\Core\Model\Entity\Property[] newEntities(array $data, array $options = [])
+ * @method \BEdita\Core\Model\Entity\Property|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \BEdita\Core\Model\Entity\Property patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \BEdita\Core\Model\Entity\Property[] patchEntities($entities, array $data, array $options = [])
+ * @method \BEdita\Core\Model\Entity\Property findOrCreate($search, callable $callback = null, $options = [])
+ *
+ * @property \Cake\ORM\Association\HasMany $Properties
  *
  * @since 4.0.0
  */
@@ -38,10 +52,7 @@ class PropertyTypesTable extends Table
         $this->setTable('property_types');
         $this->setPrimaryKey('id');
 
-        $this->hasMany('Properties', [
-            'foreignKey' => 'property_type_id',
-            'className' => 'BEdita/Core.Properties',
-        ]);
+        $this->hasMany('Properties');
     }
 
     /**
@@ -52,11 +63,17 @@ class PropertyTypesTable extends Table
     public function validationDefault(Validator $validator)
     {
         $validator
+            ->setProvider('bedita', Validation::class)
+
             ->requirePresence('name', 'create')
             ->notEmpty('name')
             ->alphaNumeric('name')
 
-            ->allowEmpty('params');
+            ->allowEmpty('params')
+            ->add('params', 'valid', [
+                'rule' => ['jsonSchema', 'http://json-schema.org/draft-06/schema#'],
+                'provider' => 'bedita',
+            ]);
 
         return $validator;
     }
@@ -83,5 +100,41 @@ class PropertyTypesTable extends Table
         $rules->add($rules->isUnique(['name']));
 
         return $rules;
+    }
+
+    /**
+     * Invalidate object types cache after updating a property type.
+     *
+     * @return void
+     */
+    public function afterSave()
+    {
+        Cache::clear(false, ObjectTypesTable::CACHE_CONFIG);
+    }
+
+    /**
+     * Check that no properties exist linked to the property type before deleting it.
+     *
+     * @param \Cake\Event\Event $event Dispatched event.
+     * @param \Cake\Datasource\EntityInterface $entity Entity being deleted.
+     * @return void
+     * @throws \Cake\Network\Exception\ForbiddenException Throws an exception if one or more properties exist
+     *      with the property type being deleted.
+     */
+    public function beforeDelete(Event $event, EntityInterface $entity)
+    {
+        if ($this->Properties->exists([$this->Properties->getForeignKey() => $entity->get($this->Properties->getBindingKey())])) {
+            throw new ForbiddenException(__d('bedita', 'Property type with existing properties'));
+        }
+    }
+
+    /**
+     * Invalidate object types cache after deleting a property type.
+     *
+     * @return void
+     */
+    public function afterDelete()
+    {
+        Cache::clear(false, ObjectTypesTable::CACHE_CONFIG);
     }
 }
