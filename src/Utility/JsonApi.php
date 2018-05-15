fab3ede627e679f27f1fc24aa481acee2172d3b0
batopa <batopa@gmail.com>
2018-05-15 14:48:51 +0200

    #1454 chore: update @param annotation

--- src/Utility/JsonApi.php
+++ src/Utility/JsonApi.php
@@ -30,7 +30,7 @@ class JsonApi
     /**
      * Format single or multiple data items in JSON API format.
      *
-     * @param \BEdita\Core\Utility\JsonApiSerializable|\BEdita\Core\Utility\JsonApiSerializable[] $items Items to be formatted.
+     * @param \BEdita\Core\Utility\JsonApiSerializable|\BEdita\Core\Utility\JsonApiSerializable[]|null $items Items to be formatted.
      * @param int $options Serializer options.
      * @param array $fields Selected fields to view in `attributes` and `meta`, if empty (default) all fields are serialized
      * @param array $included Array to be populated with included resources.
