954be8303af054f9bc3ad43d06013a6c2ce53df7
stefanorosanelli <stefano.rosanelli@gmail.com>
2018-06-27 11:48:18 +0200

    #1493 feat: `related` and `relationships` actions

--- plugins/BEdita/API/tests/TestCase/Controller/TranslationsControllerTest.php
+++ plugins/BEdita/API/tests/TestCase/Controller/TranslationsControllerTest.php
@@ -13,6 +13,8 @@
 namespace BEdita\API\Test\TestCase\Controller;
 
 use BEdita\API\TestSuite\IntegrationTestCase;
+use BEdita\API\Test\TestConstants;
+use Cake\ORM\TableRegistry;
 
 /**
  * @coversDefaultClass \BEdita\API\Controller\TranslationsController
@@ -54,7 +56,15 @@ class TranslationsControllerTest extends IntegrationTestCase
                         'status' => 'on',
                         'lang' => 'it-IT',
                         'object_id' => 2,
-                        'translated_fields' => null,
+                        'translated_fields' => [
+                            'title' => 'titolo uno',
+                            'description' => 'descrizione qui',
+                            'body' => 'contenuto qui',
+                            'extra' => [
+                                'abstract' => 'estratto qui',
+                                'list' => ['uno', 'due', 'tre'],
+                            ],
+                        ],
                     ],
                     'meta' => [
                         'created' => '2018-01-01T00:00:00+00:00',
@@ -81,7 +91,12 @@ class TranslationsControllerTest extends IntegrationTestCase
                         'status' => 'on',
                         'lang' => 'fr',
                         'object_id' => 2,
-                        'translated_fields' => null,
+                        'translated_fields' => [
+                            'description' => 'description ici',
+                            'extra' => [
+                                'list' => ['on', 'deux', 'trois'],
+                            ],
+                        ],
                     ],
                     'meta' => [
                         'created' => '2018-01-01T00:00:00+00:00',
@@ -134,7 +149,15 @@ class TranslationsControllerTest extends IntegrationTestCase
                     'status' => 'on',
                     'lang' => 'it-IT',
                     'object_id' => 2,
-                    'translated_fields' => null,
+                    'translated_fields' => [
+                        'title' => 'titolo uno',
+                        'description' => 'descrizione qui',
+                        'body' => 'contenuto qui',
+                        'extra' => [
+                            'abstract' => 'estratto qui',
+                            'list' => ['uno', 'due', 'tre'],
+                        ],
+                    ],
                 ],
                 'meta' => [
                     'created' => '2018-01-01T00:00:00+00:00',
@@ -160,5 +183,221 @@ class TranslationsControllerTest extends IntegrationTestCase
         $this->assertResponseCode(200);
         $this->assertContentType('application/vnd.api+json');
         $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * Test add method.
+     *
+     * @return void
+     *
+     * @coversNothing
+     */
+    public function testAdd()
+    {
+        $data = [
+            'type' => 'translations',
+            'attributes' => [
+                'object_id' => 2,
+                'lang' => 'sp',
+                'status' => 'draft',
+                'fields' => [
+                    'title' => 'titulo uno',
+                ],
+            ],
+        ];
+
+        $this->configRequestHeaders('POST', $this->getUserAuthHeader());
+        $this->post('/translations', json_encode(compact('data')));
+        $result = json_decode((string)$this->_response->getBody(), true);
+
+        $this->assertResponseCode(201);
+        $this->assertContentType('application/vnd.api+json');
+        static::assertArrayHasKey('data', $result);
+        $this->assertHeader('Location', 'http://api.example.com/translations/3');
+    }
+
+    /**
+     * Test edit method.
+     *
+     * @return void
+     *
+     * @coversNothing
+     */
+    public function testEdit()
+    {
+        $data = [
+            'id' => '2',
+            'type' => 'translations',
+            'attributes' => [
+                'fields' => [
+                    'description' => 'la descripcion',
+                ],
+            ],
+        ];
+
+        $this->configRequestHeaders('PATCH', $this->getUserAuthHeader());
+        $this->patch('/translations/2', json_encode(compact('data')));
+
+        $this->assertResponseCode(200);
+        $this->assertContentType('application/vnd.api+json');
+    }
+
+    /**
+     * Test delete method.
+     *
+     * @return void
+     *
+     * @coversNothing
+     */
+    public function testDelete()
+    {
+        // delete translation 2
+        $this->configRequestHeaders('DELETE', $this->getUserAuthHeader());
+        $this->delete('/translations/2');
+        $this->assertResponseCode(204);
+        $this->assertContentType('application/vnd.api+json');
+        $this->assertFalse(TableRegistry::get('Translations')->exists(['id' => 2]));
+    }
+
+    /**
+     * Test related objects.
+     *
+     * @return void
+     *
+     * @covers ::getAvailableUrl()
+     */
+    public function testRelated()
+    {
+        $expected = [
+            'links' => [
+                'self' => 'http://api.example.com/translations/1/object',
+                'home' => 'http://api.example.com/home',
+                'available' => 'http://api.example.com/objects',
+            ],
+            'data' => [
+                'id' => '2',
+                'type' => 'documents',
+                'attributes' => [
+                    'status' => 'on',
+                    'uname' => 'title-one',
+                    'title' => 'title one',
+                    'description' => 'description here',
+                    'body' => 'body here',
+                    'extra' => [
+                        'abstract' => 'abstract here',
+                        'list' => ['one', 'two', 'three'],
+                    ],
+                    'lang' => 'eng',
+                    'publish_start' => '2016-05-13T07:09:23+00:00',
+                    'publish_end' => '2016-05-13T07:09:23+00:00',
+                ],
+                'meta' => [
+                    'locked' => true,
+                    'created_by' => 1,
+                    'modified_by' => 1,
+                    'created' => '2016-05-13T07:09:23+00:00',
+                    'modified' => '2016-05-13T07:09:23+00:00',
+                    'published' => '2016-05-13T07:09:23+00:00',
+                ],
+                'relationships' => [
+                    'test' => [
+                        'links' => [
+                            'self' => 'http://api.example.com/documents/2/relationships/test',
+                            'related' => 'http://api.example.com/documents/2/test',
+                        ],
+                    ],
+                    'inverse_test' => [
+                        'links' => [
+                            'self' => 'http://api.example.com/documents/2/relationships/inverse_test',
+                            'related' => 'http://api.example.com/documents/2/inverse_test',
+                        ],
+                    ],
+                    'parents' => [
+                        'links' => [
+                            'self' => 'http://api.example.com/documents/2/relationships/parents',
+                            'related' => 'http://api.example.com/documents/2/parents',
+                        ],
+                    ],
+                    'translations' => [
+                        'links' => [
+                            'self' => 'http://api.example.com/documents/2/relationships/translations',
+                            'related' => 'http://api.example.com/documents/2/translations',
+                        ],
+                    ],
+                ],
+            ],
+            'meta' => [
+                'schema' => [
+                    'documents' => [
+                        '$id' => 'http://api.example.com/model/schema/documents',
+                        'revision' => TestConstants::SCHEMA_REVISIONS['documents'],
+                    ],
+                ],
+            ],
+        ];
+
+        $this->configRequestHeaders();
+        $this->get('/translations/1/object');
+        $result = json_decode((string)$this->_response->getBody(), true);
+
+        $this->assertResponseCode(200);
+        $this->assertContentType('application/vnd.api+json');
+        static::assertEquals($expected, $result);
+    }
+
+    /**
+     * Test relationships method to list existing relationships.
+     *
+     * @return void
+     *
+     * @covers ::getAssociatedAction()
+     */
+    public function testRelationships()
+    {
+        $expected = [
+            'links' => [
+                'self' => 'http://api.example.com/translations/1/relationships/object',
+                'home' => 'http://api.example.com/home',
+                'available' => 'http://api.example.com/objects',
+            ],
+            'data' => [
+                'id' => '2',
+                'type' => 'documents',
+                'relationships' => [
+                    'test' => [
+                        'links' => [
+                            'related' => 'http://api.example.com/documents/2/test',
+                            'self' => 'http://api.example.com/documents/2/relationships/test',
+                        ],
+                    ],
+                    'inverse_test' => [
+                        'links' => [
+                            'related' => 'http://api.example.com/documents/2/inverse_test',
+                            'self' => 'http://api.example.com/documents/2/relationships/inverse_test',
+                        ],
+                    ],
+                    'parents' => [
+                        'links' => [
+                            'related' => 'http://api.example.com/documents/2/parents',
+                            'self' => 'http://api.example.com/documents/2/relationships/parents',
+                        ],
+                    ],
+                    'translations' => [
+                        'links' => [
+                            'related' => 'http://api.example.com/documents/2/translations',
+                            'self' => 'http://api.example.com/documents/2/relationships/translations',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $this->configRequestHeaders();
+        $this->get('/translations/1/relationships/object');
+        $result = json_decode((string)$this->_response->getBody(), true);
+
+        $this->assertResponseCode(200);
+        $this->assertContentType('application/vnd.api+json');
+        static::assertEquals($expected, $result);
     }
 }
