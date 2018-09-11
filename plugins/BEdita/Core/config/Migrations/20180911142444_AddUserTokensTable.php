adb704543d0dd67747720e1940c4f6df4dc66405
stefanorosanelli <stefano.rosanelli@gmail.com>
2018-09-11 17:55:23 +0200

    #1535 feat: `user_tokens` table migration

---
+++ plugins/BEdita/Core/config/Migrations/20180911142444_AddUserTokensTable.php
@@ -0,0 +1,133 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddUserTokensTable extends AbstractMigration
+{
+    /**
+     * {@inheritDoc}
+     */
+    public $autoId = false;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function change()
+    {
+        $columnTypes = $this->getAdapter()->getColumnTypes();
+        $enum = in_array('enum', $columnTypes) ? 'enum' : 'string';
+
+        $this->table('user_tokens')
+            ->addColumn('id', 'integer', [
+                'autoIncrement' => true,
+                'default' => null,
+                'limit' => 10,
+                'null' => false,
+                'signed' => false,
+            ])
+            ->addPrimaryKey(['id'])
+            ->addColumn('user_id', 'integer', [
+                'comment' => 'reference to system user',
+                'default' => null,
+                'limit' => 10,
+                'null' => false,
+                'signed' => false,
+            ])
+            ->addColumn('application_id', 'integer', [
+                'comment' => 'link to applications.id - may be null',
+                'default' => null,
+                'limit' => 5,
+                'null' => true,
+                'signed' => false,
+            ])
+            ->addColumn('client_token', 'string', [
+                'comment' => 'token sent to application',
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('secret_token', 'string', [
+                'comment' => 'secret token sent to user in a secure way',
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('token_type', $enum, [
+                'comment' => 'type of token: otp, refresh, recovery, 2fa, access',
+                'default' => 'refresh',
+                'limit' => 255,
+                'values' => ['otp', 'refresh', 'recovery', '2fa', 'access'],
+                'null' => false,
+            ])
+            ->addColumn('expires', 'timestamp', [
+                'comment' => 'token expiry time',
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('created', 'timestamp', [
+                'comment' => 'creation date',
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('used', 'timestamp', [
+                'comment' => 'token used time',
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addIndex(
+                [
+                    'user_id',
+                ],
+                [
+                    'name' => 'usertokens_userid_idx',
+                ]
+            )
+            ->addIndex(
+                [
+                    'application_id',
+                ],
+                [
+                    'name' => 'usertokens_applicationid_idx',
+                ]
+            )
+            ->addIndex(
+                [
+                    'client_token',
+                ],
+                [
+                    'name' => 'clienttoken_applicationid_idx',
+                ]
+            )
+            ->addIndex(
+                [
+                    'secret_token',
+                ],
+                [
+                    'name' => 'secrettoken_applicationid_idx',
+                ]
+            )
+            ->addForeignKey(
+                'application_id',
+                'applications',
+                'id',
+                [
+                    'constraint' => 'usertokens_applicationid_fk',
+                    'update' => 'NO_ACTION',
+                    'delete' => 'CASCADE',
+                ]
+            )
+            ->addForeignKey(
+                'user_id',
+                'users',
+                'id',
+                [
+                    'constraint' => 'usertokens_userid_fk',
+                    'update' => 'NO_ACTION',
+                    'delete' => 'CASCADE',
+                ]
+            )
+            ->create();
+    }
+}
