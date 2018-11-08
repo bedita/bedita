2480decf694e1a9e9b46f5adeb6e461980281800
Paolo Cuffiani <paolo.cuffiani@gmail.com>
2018-11-08 10:04:09 +0100

    refactor: change object type ID!!!

--- bedita-app/config/sql/upgrade/1-3.8-to-3.9-schema.sql
+++ bedita-app/config/sql/upgrade/1-3.8-to-3.9-schema.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `objects` ADD INDEX `objects_publisher_idx` (`publisher`);
 
-INSERT INTO `object_types` (`id`, `name`, `module_name`) VALUES (42, 'caption', 'multimedia') ON DUPLICATE KEY UPDATE `id` = 42;
+INSERT INTO `object_types` (`id`, `name`, `module_name`) VALUES (51, 'caption', 'multimedia') ON DUPLICATE KEY UPDATE `id` = 51;
