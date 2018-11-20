15b26fbb7495ee9e642cfef9a6c46fb7afb3f487
Dante Di Domenico <d.didomenico@channelweb.it>
2018-11-20 14:27:14 +0100

    feat: set perms for webroot files in entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -15,8 +15,6 @@ ENV DEBUG ${DEBUG:-false}
 
 # Setup `webroot/_files` for media files
 WORKDIR /var/www/html
-RUN chmod a+rwx /var/www/html/webroot/_files
-RUN chown -R www-data:www-data /var/www/html/webroot/_files
 VOLUME /var/www/html/webroot/_files
 
 # Install dependencies
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,6 +18,8 @@ if [ ! -z "${DATABASE_URL}" ]; then
         bin/cake migrations seed -p BEdita/Core --seed AdminFromEnvSeed
     fi
 
+    chmod a+rwx /var/www/html/webroot/_files
+    chown -R www-data:www-data /var/www/html/webroot/_files
     chmod -R a+rwX tmp
     chmod -R a+rwX logs
 
