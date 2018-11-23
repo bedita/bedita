e5b6368b20d32626ed52aed7b042662576d4d76e
Dante Di Domenico <d.didomenico@channelweb.it>
2018-11-23 16:10:20 +0100

    feat: chmod webroot files, execution bit only on directories

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,7 +18,7 @@ if [ ! -z "${DATABASE_URL}" ]; then
         bin/cake migrations seed -p BEdita/Core --seed AdminFromEnvSeed
     fi
 
-    chmod a+rwx /var/www/html/webroot/_files
+    chmod a+rwX /var/www/html/webroot/_files
     chown -R www-data:www-data /var/www/html/webroot/_files
     chmod -R a+rwX tmp
     chmod -R a+rwX logs
