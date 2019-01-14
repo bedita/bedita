dacb48ad1e0d8be86c4b7ada629b42e4a1ed5c90
stefanorosanelli <stefano.rosanelli@gmail.com>
2019-01-14 17:55:19 +0100

    chore: add signup config comment [ci skip]

--- config/app.default.php
+++ config/app.default.php
@@ -428,6 +428,7 @@ return [
      * - 'roles' - allowed role names on user signup (this config should be set normally at application level),
      *      requested user roles MUST be included in this array
      * - 'requireEmail' - require email upon signup (default: true)
+     * - 'activationUrl' => default activation URL to use if not set by application
      * - 'requirePassword' - require password upon signup (default: true), can be false in some AUTH schemas like One Time Password
      * - 'defaultRoles` - roles to add upon signup as default if no roles are passed; they MUST be in allowed `roles` in order to be set
      */
