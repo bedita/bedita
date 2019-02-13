bf694809dc110244c6fa2d4ab28623760ca3a636
Dante Di Domenico <d.didomenico@channelweb.it>
2019-02-13 15:46:09 +0100

    feat: avoid userid in logs, use ID instead or partially hide userid

--- bedita-app/app_controller.php
+++ bedita-app/app_controller.php
@@ -393,13 +393,26 @@ class AppController extends Controller {
     protected function beditaAfterFilter() {
     }
 
+    /**
+     * Event log creation
+     *
+     * @param string $level The log level
+     * @param string $msg The message
+     * @return void
+     */
     protected function eventLog($level, $msg) {
-        $u = $this->BeAuth->userid();
-        if (empty($u)) {
-            $u = '-';
+        $userid = $this->BeAuth->userid();
+        if (empty($userid)) {
+            $userid = '-';
         }
-        $event = array('EventLog'=>array('log_level'=>$level,
-            'userid'=>$u, 'msg'=>$msg, 'context'=>strtolower($this->name)));
+        $event = array(
+            'EventLog' => array(
+                'log_level' => $level,
+                'userid' => $userid,
+                'msg' => $msg,
+                'context' => strtolower($this->name)
+            )
+        );
         $this->EventLog->create();
         $this->EventLog->save($event);
     }
@@ -481,7 +494,8 @@ class AppController extends Controller {
                 if ($extUserId != true) {
                     $msg .= ': ' . $extUserId;
                 }
-                $this->eventWarn($msg);
+                // avoid userid in logs / use ID instead or partially hide userid
+                $this->eventWarn(sprintf('external login failed: %s******', substr($userid, 0, -3)));
                 $this->userWarnMessage($msg);
                 $this->Session->delete('externalLoginRequestFailed');
             };
