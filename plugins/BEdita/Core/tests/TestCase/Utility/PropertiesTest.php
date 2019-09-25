47a154e502854fcd1d6fbf5fb5f52e219009e6b3
stefanorosanelli <stefano.rosanelli@gmail.com>
2019-09-25 09:42:21 +0200

    feat: `relations` and `properties` utils + tests

---
+++ plugins/BEdita/Core/tests/TestCase/Utility/PropertiesTest.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2019 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\Core\Test\TestCase\Utility;
+
+use BEdita\Core\Utility\Properties;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+
+/**
+ * {@see \BEdita\Core\Utility\Properties} Test Case
+ *
+ * @coversDefaultClass \BEdita\Core\Utility\Properties
+ */
+class PropertiesTest extends TestCase
+{
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.BEdita/Core.object_types',
+        'plugin.BEdita/Core.property_types',
+        'plugin.BEdita/Core.properties',
+        'plugin.BEdita/Core.relations',
+        'plugin.BEdita/Core.relation_types',
+    ];
+
+    /**
+     * Test properties
+     *
+     * @return array
+     */
+    protected $properties = [
+        [
+            'name' => 'custom_one',
+            'object' => 'documents',
+            'property' => 'boolean',
+            'description' => 'my custom description',
+        ],
+        [
+            'name' => 'custom_two',
+            'object' => 'documents',
+            'property' => 'string',
+        ],
+    ];
+
+    /**
+     * Test `create` method.
+     *
+     * @covers ::create()
+     */
+    public function testCreate()
+    {
+        Properties::create($this->properties);
+
+        $newProperties = TableRegistry::getTableLocator()->get('Properties')
+            ->find()
+            ->where(['name IN' => ['custom_one', 'custom_two']])
+            ->toArray();
+        static::assertEquals(2, count($newProperties));
+    }
+
+    /**
+     * Test `remove` method.
+     *
+     * @covers ::remove()
+     */
+    public function testRemove()
+    {
+        Properties::create($this->properties);
+
+        Properties::remove($this->properties);
+        $newProperties = TableRegistry::getTableLocator()->get('Properties')
+            ->find()
+            ->where(['name IN' => ['custom_one', 'custom_two']])
+            ->toArray();
+        static::assertEquals(0, count($newProperties));
+    }
+}
---
+++ plugins/BEdita/Core/tests/TestCase/Utility/RelationsTest.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2019 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\Core\Test\TestCase\Utility;
+
+use BEdita\Core\Utility\Relations;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+
+/**
+ * {@see \BEdita\Core\Utility\Relations} Test Case
+ *
+ * @coversDefaultClass \BEdita\Core\Utility\Relations
+ */
+class RelationsTest extends TestCase
+{
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'plugin.BEdita/Core.object_types',
+        'plugin.BEdita/Core.relations',
+        'plugin.BEdita/Core.relation_types',
+    ];
+
+    /**
+     * Test relations
+     *
+     * @return array
+     */
+    protected $relations = [
+        [
+            'name' => 'poster',
+            'label' => 'Poster',
+            'inverse_name' => 'poster_of',
+            'inverse_label' => 'Poster of',
+            'description' => 'Document or event has a poster file',
+            'left' => ['documents', 'events'],
+            'right' => ['files'],
+        ]
+    ];
+
+    /**
+     * Test `create` method.
+     *
+     * @covers ::create()
+     * @covers ::addTypes()
+     */
+    public function testCreate()
+    {
+        Relations::create($this->relations);
+
+        $allRelations = TableRegistry::getTableLocator()->get('Relations')->find()->toArray();
+        static::assertEquals(4, count($allRelations));
+        static::assertEquals('poster', $allRelations[3]['name']);
+    }
+
+    /**
+     * Test `remove` method.
+     *
+     * @covers ::remove()
+     * @covers ::removeTypes()
+     */
+    public function testRemove()
+    {
+        Relations::create($this->relations);
+
+        Relations::remove($this->relations);
+        $allRelations = TableRegistry::getTableLocator()->get('Relations')->find()->toArray();
+        static::assertEquals(3, count($allRelations));
+    }
+}
