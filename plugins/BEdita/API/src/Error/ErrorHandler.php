72871727a8a0d99949b1d075b6f6130d8eacc454
stefanorosanelli <stefano.rosanelli@gmail.com>
2019-10-01 11:14:19 +0200

    feat introduce custom error handler

---
+++ plugins/BEdita/API/src/Error/ErrorHandler.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2019 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\API\Error;
+
+use Cake\Error\ErrorHandler as CakeErrorHandler;
+use Cake\Utility\Hash;
+
+class ErrorHandler extends CakeErrorHandler
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function _displayError($error, $debug)
+    {
+        if (!$debug) {
+            return;
+        }
+        $msg = Hash::get($error, 'error');
+        $code = Hash::get($error, 'code');
+        $description = Hash::get($error, 'description');
+        throw new \LogicException(sprintf('%s [%s] %s', $msg, $code, $description));
+    }
+}
