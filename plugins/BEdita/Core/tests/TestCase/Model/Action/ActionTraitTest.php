f690b2b73a0fdd1ee991c874618cd2eba2e397fb
stefanorosanelli <stefano.rosanelli@gmail.com>
2020-03-30 17:28:19 +0200

    chore: remove comments [ci skip]

--- plugins/BEdita/Core/tests/TestCase/Model/Action/ActionTraitTest.php
+++ plugins/BEdita/Core/tests/TestCase/Model/Action/ActionTraitTest.php
@@ -35,20 +35,9 @@ class ActionTraitTest extends TestCase
      */
     public $fixtures = [
         'plugin.BEdita/Core.ObjectTypes',
-        // 'plugin.BEdita/Core.Properties',
         'plugin.BEdita/Core.Objects',
-        // 'plugin.BEdita/Core.Profiles',
-        // 'plugin.BEdita/Core.Users',
-        // 'plugin.BEdita/Core.AsyncJobs',
-        // 'plugin.BEdita/Core.Roles',
-        // 'plugin.BEdita/Core.Trees',
-        // 'plugin.BEdita/Core.RolesUsers',
-        // 'plugin.BEdita/Core.ExternalAuth',
-        // 'plugin.BEdita/Core.AuthProviders',
         'plugin.BEdita/Core.Relations',
         'plugin.BEdita/Core.RelationTypes',
-        // 'plugin.BEdita/Core.ObjectRelations',
-        // 'plugin.BEdita/Core.History',
     ];
 
     /**
