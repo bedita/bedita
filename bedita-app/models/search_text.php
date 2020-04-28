188914dc2d9dfb08cea18cea7b72e61f14bf2132
Dante Di Domenico <d.didomenico@channelweb.it>
2020-04-28 15:15:16 +0200

    fix: pass parents to indexObject

--- bedita-app/models/search_text.php
+++ bedita-app/models/search_text.php
@@ -67,11 +67,16 @@ class SearchText extends BEAppModel
 		}
 		
 		$this->checkIndexModel();
-		if($this->indexModel) {
+		if ($this->indexModel) {
 			$res = $this->indexModel->indexObject($searchFields, $data);
-			if(!empty($res["error"])) {
-			    throw new BeditaException("crete object index error: " . 
-			            $res["error"]);
+			$parents = array();
+			if (!empty($model->data[$model->alias]['destination'])) {
+				$excludeIds = (array)Configure::read('excludeFromTreeIds');
+				$parents = array_unique(array_diff((array)$model->data[$model->alias]['destination'], $excludeIds));
+			}
+			$res = $this->indexModel->indexObject($searchFields, $data, $parents);
+			if (!empty($res['error'])) {
+			    throw new BeditaException(sprintf('create object index error: %s', $res['error']));
 			}
 		} else {
 			$this->saveSearchTexts($searchFields, $data);
