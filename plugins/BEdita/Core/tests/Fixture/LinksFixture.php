97afb8a874d4d8ca5aee82821bb2cc287db1a3a9
Dante Di Domenico <d.didomenico@channelweb.it>
2020-04-29 08:04:38 +0200

    fix: links fixture

---
+++ plugins/BEdita/Core/tests/Fixture/LinksFixture.php
@@ -0,0 +1,27 @@
+<?php
+namespace BEdita\Core\Test\Fixture;
+
+use BEdita\Core\TestSuite\Fixture\TestFixture;
+
+/**
+ * LinksFixture
+ *
+ */
+class LinksFixture extends TestFixture
+{
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'id' => 15,
+            'title' => 'Gustavo Homepage',
+            'http_status' => '200 OK',
+            'url' => 'https://www.gustavo.com',
+            'last_update' => '2020-04-29 08:05:15',
+        ],
+    ];
+}
