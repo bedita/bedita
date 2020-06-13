30913cf7abc19b73641b62875934ccf47a1c2232
Stefano Rosanelli <stefano.rosanelli@gmail.com>
2020-06-13 17:55:26 +0200

    feat: add from_date/to_date filtes

--- plugins/BEdita/Core/src/Model/Table/DateRangesTable.php
+++ plugins/BEdita/Core/src/Model/Table/DateRangesTable.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * BEdita, API-first content management framework
- * Copyright 2017 ChannelWeb Srl, Chialab Srl
+ * Copyright 2020 ChannelWeb Srl, Chialab Srl
  *
  * This file is part of BEdita: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
@@ -16,6 +16,7 @@ namespace BEdita\Core\Model\Table;
 use BEdita\Core\Exception\BadFilterException;
 use BEdita\Core\Model\Validation\Validation;
 use BEdita\Core\ORM\QueryFilterTrait;
+use Cake\Database\Expression\QueryExpression;
 use Cake\ORM\Query;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
@@ -108,17 +109,25 @@ class DateRangesTable extends Table
      *
      * Create a query to filter objects using start and end date conditions.
      * Accepted options are:
-     *   - 'start_date' or 'end_date'
-     *   - mandatory sub-option must be one of 'gt' (greather than), 'lt' (less than),
+     *   - 'start_date', 'end_date', 'from_date' and 'to_date'
+     *   - when using 'from_date' or 'to_date' dates must be provided
+     *   - when using 'start_date' or 'end_date' mandatory sub-option
+     *          must be one of 'gt' (greather than), 'lt' (less than),
      *          'ge' (greater or equal), 'le' (less or equal) with a date
      *
      * Examples
      * ```
-     * // find events with a start date after '2017-03-01'
+     * // find objects with a start date after '2017-03-01'
      * $table->find('dateRanges', ['start_date' => ['gt' => '2017-03-01']]);
      *
-     * // find events with an ending date before '2017-05-01 22:00:00'
+     * // find objects with an ending date before '2017-05-01 22:00:00'
      * $table->find('dateRanges', ['end_date' => ['lt' => '2017-05-01 22:00:00']]);
+     *
+     * // find objects with valid date ranges from '2018-05-01 22:00:00' onwards
+     * $table->find('dateRanges', ['from_date' => '2018-05-01 22:00:00']);
+     *
+     * // find objects with valid date ranges until '2018-05-01 22:00:00'
+     * $table->find('dateRanges', [to_date' => '2018-05-01 22:00:00']);
      * ```
      *
      * @param \Cake\ORM\Query $query Query object instance.
@@ -128,7 +137,13 @@ class DateRangesTable extends Table
      */
     protected function findDateRanges(Query $query, array $options)
     {
-        $options = array_intersect_key($options, array_flip(['start_date', 'end_date', 'date']));
+        $allowed = array_flip([
+            'start_date',
+            'end_date',
+            'from_date',
+            'to_date'
+        ]);
+        $options = array_intersect_key($options, $allowed);
         if (empty($options)) {
             throw new BadFilterException([
                 'title' => __d('bedita', 'Invalid data'),
@@ -136,10 +151,10 @@ class DateRangesTable extends Table
             ]);
         }
 
-        $query = $this->customDateFilter($query, $options);
-        unset($options['date']);
+        $query = $this->fromToDateFilter($query, $options);
+        unset($options['from_date'], $options['to_date']);
 
-        // create filter with `start_date` and `end_date` if present in options
+        // filter on `start_date` and `end_date` fields
         $options = array_combine(
             array_map([$this, 'aliasField'], array_keys($options)),
             array_values($options)
@@ -148,39 +163,90 @@ class DateRangesTable extends Table
         return $this->fieldsFilter($query, $options);
     }
 
-    protected function customDateFilter(Query $query, array $options): Query
+    /**
+     * Modify query object with `from_date`and `to_date` params
+     *
+     * @param \Cake\ORM\Query $query Query object instance.
+     * @param array $options Array of date conditions.
+     * @return \Cake\ORM\Query
+     */
+    protected function fromToDateFilter(Query $query, array $options): Query
     {
-        $custom = (string)Hash::get($options, 'date');
-        if (empty($custom)) {
+        $from = (string)Hash::get($options, 'from_date');
+        $to = (string)Hash::get($options, 'to_date');
+        if (empty($from) && empty($to)) {
             return $query;
         }
-        $method = sprintf('%sCustomFilter', $custom);
-        if (!method_exists($this, $method)) {
-            throw new BadFilterException([
-                'title' => __d('bedita', 'Invalid data'),
-                'detail' => __d('bedita', 'Custom date filter "{0}" not available', $custom)
-            ]);
+
+        if (empty($to)) {
+            return $this->fromDateFilter($query, $from);
+        }
+        if (empty($from)) {
+            return $this->toDateFilter($query, $to);
         }
 
-        return $this->{$method}($query);
+        return $this->betweenDatesFilter($query, $from, $to);
     }
 
-    protected function todayCustomFilter(Query $query): Query
+    /**
+     * Add `from_date` query condition
+     *
+     * @param \Cake\ORM\Query $query Query object instance.
+     * @param string $from From date.
+     * @return \Cake\ORM\Query
+     */
+    protected function fromDateFilter(Query $query, string $from): Query
     {
-        $today = date('Y-m-d');
+        return $query->where(function (QueryExpression $exp, Query $q) use ($from) {
+                return $exp->or_([
+                    $q->newExpr()
+                        ->gte($this->aliasField('start_date'), $from)
+                        ->isNull($this->aliasField('end_date')),
+                    $q->newExpr()
+                        ->gte($this->aliasField('end_date'), $from),
+                ]);
+            });
+    }
 
-        return $query->where([$this->aliasField('start_date') => $today])
-            ->orWhere([
-                $this->aliasField('start_date') . ' <' => $today,
-                $this->aliasField('end_date') . ' >=' => $today,
+    /**
+     * Add `to_date` query condition
+     *
+     * @param \Cake\ORM\Query $query Query object instance.
+     * @param string $to To date.
+     * @return \Cake\ORM\Query
+     */
+    protected function toDateFilter(Query $query, string $to): Query
+    {
+        return $query->where(function (QueryExpression $exp, Query $q) use ($to) {
+                return $exp->or_([
+                    $q->newExpr()
+                        ->lte($this->aliasField('start_date'), $to)
+                        ->isNull($this->aliasField('end_date')),
+                    $q->newExpr()
+                        ->lte($this->aliasField('end_date'), $to),
+                ]);
+            });
+    }
+
+    /**
+     * Add `from_date`/`to_date` query condition
+     *
+     * @param \Cake\ORM\Query $query Query object instance.
+     * @param string $from From date.
+     * @param string $to To date.
+     * @return \Cake\ORM\Query
+     */
+    protected function betweenDatesFilter(Query $query, string $from, string $to): Query
+    {
+        return $query->where(function (QueryExpression $exp, Query $q) use ($from, $to) {
+            return $exp->or_([
+                $q->newExpr()
+                    ->gte($this->aliasField('start_date'), $from)
+                    ->lte($this->aliasField('start_date'), $to),
+                $q->newExpr()
+                    ->lte($this->aliasField('start_date'), $to)
+                    ->gte($this->aliasField('end_date'), $from),
             ]);
-    }
-
-    protected function futureCustomFilter(Query $query): Query
-    {
-        $today = date('Y-m-d');
-
-        return $query->where([$this->aliasField('start_date') . ' >=' => $today])
-            ->orWhere([$this->aliasField('end_date') . ' >=' => $today]);
+        });
     }
 }
