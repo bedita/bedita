384324bd3d9f6f14d5d7f1e6dae670d3993c36c2
batopa <batopa@gmail.com>
2020-12-14 21:53:46 +0100

    #1655 fix: count all related objects
    [skip ci]

--- plugins/BEdita/Core/src/Model/Action/CountRelatedObjectsAction.php
+++ plugins/BEdita/Core/src/Model/Action/CountRelatedObjectsAction.php
@@ -14,7 +14,7 @@
 namespace BEdita\Core\Model\Action;
 
 use BEdita\Core\Model\Action\BaseAction;
-use Cake\Database\Expression\QueryExpression;
+use BEdita\Core\Model\Entity\ObjectEntity;
 use Cake\Datasource\EntityInterface;
 use Cake\ORM\Locator\LocatorAwareTrait;
 use Cake\ORM\Query;
@@ -62,6 +62,19 @@ class CountRelatedObjectsAction extends BaseAction
     protected $ObjectRelations = null;
 
     /**
+     * The relations list described as
+     *
+     * ```
+     * [
+     *     'direct_name' => 'inverse_name',
+     * ]
+     * ```
+     *
+     * @var array
+     */
+    protected $relationsList = null;
+
+    /**
      * {@inheritDoc}
      */
     protected function initialize(array $config): void
@@ -77,6 +90,8 @@ class CountRelatedObjectsAction extends BaseAction
      * - `entities` an array/collection of entity representing objects
      * - `count` a comma separated list of relations you want to count.
      *   Set to 'all' to count all relations.
+     *
+     * The returned result will be an array with id as keys and containg count by relations as values.
      */
     public function execute(array $data = []): array
     {
@@ -88,12 +103,11 @@ class CountRelatedObjectsAction extends BaseAction
             return $entities;
         }
 
-        // extract all objects' ids
-        $ids = (array)Hash::extract($entities, '{n}.id');
-        foreach ($count as $relation) {
-            $ids = array_merge($ids, (array)Hash::extract($entities, "{n}.$relation.{n}.id"));
-        }
-        $ids = array_unique($ids);
+        $allRelations = $this->getRelationsList();
+        $allRelations = array_merge(array_keys($allRelations), array_values($allRelations));
+
+        // extract all entity ids
+        $ids = $this->extractIds($entities, $allRelations);
 
         $directResult = $inverseResult = [];
         if (!empty($directCount)) {
@@ -104,14 +118,54 @@ class CountRelatedObjectsAction extends BaseAction
             $inverseResult = $this->countRelations($inverseCount, $ids, true)->toArray();
         }
 
-        $result = array_merge($directResult, $inverseResult);
+        $result = $this->groupResultCountById($ids, array_merge($directResult, $inverseResult), $count);
 
         if ($this->getConfig('hydrate') === true) {
-            $this->hydrateCount($entities, $result);
-            $this->fillMissingCount($entities, $count);
+            $this->hydrateCount($entities, $result, $allRelations);
         }
 
         return $result;
+    }
+
+    /**
+     * Extract ids from a list of entities.
+     * The related properties ids are extracted too.
+     *
+     * @param array $entities The list of entities
+     * @param array $properties Properties on which search on
+     * @return array
+     */
+    protected function extractIds(array $entities, array $properties): array
+    {
+        $ids = [];
+        foreach ($entities as $entity) {
+            if (!$entity instanceof ObjectEntity) {
+                continue;
+            }
+
+            $data = array_filter($entity->extract($properties));
+            $ids = array_merge($ids, [$entity->id], (array)Hash::extract($data, '{s}.{n}.id'));
+        }
+
+        return array_unique($ids);
+    }
+
+    /**
+     * Get list of relations as an array with names as keys
+     * and inverse names as values.
+     *
+     * @return array
+     */
+    protected function getRelationsList(): array
+    {
+        if ($this->relationsList !== null) {
+            return $this->relationsList;
+        }
+
+        return $this->Relations->find('list', [
+            'keyField' => 'name',
+            'valueField' => 'inverse_name',
+        ])->toArray();
     }
 
     /**
@@ -126,14 +180,9 @@ class CountRelatedObjectsAction extends BaseAction
      */
     protected function filterCount($count): array
     {
-        $query = $this->Relations->find('list', [
-            'keyField' => 'name',
-            'valueField' => 'inverse_name',
-        ]);
+        $relations = $this->getRelationsList();
 
         if ($count === static::COUNT_ALL) {
-            $relations = $query->toArray();
-
             return [array_keys($relations), array_values($relations)];
         }
 
@@ -143,14 +192,6 @@ class CountRelatedObjectsAction extends BaseAction
         if (empty($count)) {
             return [];
         }
-
-        $relations = $query
-            ->where(function (QueryExpression $exp) use ($count) {
-                return $exp
-                    ->or($exp->in($this->Relations->aliasField('name'), $count))
-                    ->in($this->Relations->aliasField('inverse_name'), $count);
-            })
-            ->toArray();
 
         $direct = array_intersect($count, array_flip($relations));
         $inverse = array_intersect($count, $relations);
@@ -207,17 +248,15 @@ class CountRelatedObjectsAction extends BaseAction
      * Hydrate count result into entities.
      *
      * @param \BEdita\Core\Model\Entity\ObjectEntity[] $entities The collection of entities
-     * @param array $countResult The count result
+     * @param array $countById The count result by id
+     * @param array $properties A list of properties on which search
      * @return void
      */
-    protected function hydrateCount(array $entities, array $countResult): void
+    protected function hydrateCount(array $entities, array $countById, array $properties = []): void
     {
-        $relations = array_unique(Hash::extract($countResult, '{n}.relation_name'));
-        $groupById = Hash::combine($countResult, '{n}.relation_name', '{n}.count', '{n}.id');
-
-        foreach ($groupById as $id => $count) {
+        foreach ($countById as $id => $count) {
             /** @var \BEdita\Core\Model\Entity\ObjectEntity $object */
-            $object = $this->searchEntityById($id, $entities, $relations);
+            $object = $this->searchEntityById($id, $entities, $properties);
             if (!$object) {
                 continue;
             }
@@ -227,27 +266,58 @@ class CountRelatedObjectsAction extends BaseAction
     }
 
     /**
-     * Fill entities with count = 0 where missing, for the relationships passed.
+     * Group count result by id and relation.
+     * All relations that missing from `$countData` will be filled to zero.
      *
-     * @param \BEdita\Core\Model\Entity\ObjectEntity[] $entities The collection of entities
-     * @param array $relations The relations to fill
-     * @return void
+     * The count data as
+     *
+     * ```
+     * [
+     *     [
+     *         'id' => 15,
+     *         'relation_name' => 'relation_one',
+     *         'count' => 2,
+     *     ],
+     *     [
+     *         'id' => 15,
+     *         'relation_name' => 'relation_two',
+     *         'count' => 6,
+     *     ],
+     * ]
+     * ```
+     *
+     * will be returned as
+     *
+     * ```
+     * [
+     *     15 => [
+     *         'relation_one' => 2,
+     *         'relation_two' => 6,
+     *         'relation_three' => 0, // present in $relations but missing from count data
+     *     ],
+     * ]
+     * ```
+     *
+     * @param array $ids An array of ids that needs count
+     * @param array $countData The count data
+     * @param array $relations List of relations that need to be counted
+     * @return array
      */
-    protected function fillMissingCount(array $entities, array $relations)
+    protected function groupResultCountById(array $ids, array $countData, array $relations): array
     {
+        $countById = Hash::combine($countData, '{n}.relation_name', '{n}.count', '{n}.id');
         $count = array_fill_keys($relations, 0);
-        foreach ($entities as $entity) {
-            $countData = (array)$entity->get('_countData');
-            $entity->set('_countData', $countData + $count);
+        foreach ($ids as $id) {
+            if (array_key_exists($id, $countById)) {
+                $countById[$id] += $count;
 
-            foreach ($relations as $rel) {
-                if (!$entity->has($rel)) {
-                    continue;
-                }
-
-                $this->fillMissingCount((array)$entity->get($rel), $relations);
+                continue;
             }
+
+            $countById[$id] = $count;
         }
+
+        return $countById;
     }
 
     /**
@@ -271,6 +341,8 @@ class CountRelatedObjectsAction extends BaseAction
                 return $found;
             }
         }
+
+        return null;
     }
 
     /**
