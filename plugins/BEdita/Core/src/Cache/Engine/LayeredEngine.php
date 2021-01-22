869b59dc7749c11bbc3695b1627a65f718e5bbbd
leo <mainardi.leo@gmail.com>
2021-01-22 16:58:08 +0100

    fix: namespace

--- plugins/BEdita/Core/src/Cache/Engine/LayeredEngine.php
+++ plugins/BEdita/Core/src/Cache/Engine/LayeredEngine.php
@@ -10,7 +10,7 @@
  *
  * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
  */
-namespace BEdita\Core;
+namespace BEdita\Core\Cache\Engine;
 
 use Cake\Cache\Cache;
 use Cake\Cache\CacheEngine;
