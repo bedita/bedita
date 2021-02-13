16e834e522056d93057cd3261dee1144cddd76e1
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2021-02-13 17:15:52 +0100

    refactor: heavily refactor management of associations, extraction and parameters

--- plugins/BEdita/Core/src/Model/Behavior/PlaceholdersBehavior.php
+++ plugins/BEdita/Core/src/Model/Behavior/PlaceholdersBehavior.php
@@ -13,11 +13,16 @@
 
 namespace BEdita\Core\Model\Behavior;
 
-use BEdita\Core\Utility\JsonSchema;
+use BEdita\Core\Exception\LockedResourceException;
+use BEdita\Core\Model\Action\SetRelatedObjectsAction;
+use Cake\Database\Expression\QueryExpression;
 use Cake\Datasource\EntityInterface;
 use Cake\Event\Event;
+use Cake\ORM\Association;
 use Cake\ORM\Behavior;
-use Cake\ORM\Exception\PersistenceFailedException;
+use Cake\ORM\Locator\LocatorAwareTrait;
+use Cake\ORM\Table;
+use InvalidArgumentException;
 use RuntimeException;
 
 /**
@@ -27,12 +32,111 @@ use RuntimeException;
  */
 class PlaceholdersBehavior extends Behavior
 {
+    use LocatorAwareTrait;
+
     /**
-     * The regex to use to interpolate placeholders data.
+     * The default regex to use to interpolate placeholders data.
      *
      * @var string
      */
-    const REGEX = '/<!--\s+BE-PLACEHOLDER\.(?P<id>\d+)\.(?P<params>[A-Za-z0-9+=-]+)\s+-->/';
+    const REGEX = '/<!--\s+BE-PLACEHOLDER\.(?P<id>\d+)(?:\.(?P<params>[A-Za-z0-9+=-]+))?\s+-->/';
+
+    /**
+     * Default configurations. Available configurations include:
+     *
+     * - `relation`: name of the BEdita relation to use.
+     * - `fields`: list of fields from which placeholders should be extracted.
+     * - `extract`: extract function that will be called on each entity; it will receive
+     *      the entity instance and an array of fields as input, and is expected to return
+     *      a list of associative arrays with `id` and `params` fields.
+     *      If `null`, uses {@see \BEdita\Core\Model\Behavior\PlaceholdersBehavior::extractPlaceholders()}.
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [
+        'relation' => 'placeholder',
+        'fields' => ['description', 'body'],
+        'extract' => null,
+    ];
+
+    /**
+     * Relation instance.
+     *
+     * @var \BEdita\Core\Model\Entity\Relation|null
+     */
+    protected $relation = null;
+
+    /**
+     * Extract placeholders from an entity.
+     *
+     * @param EntityInterface $entity The entity from which to extract placeholder references.
+     * @param string[] $fields Field names.
+     * @return array[] A list of arrays, each with `id` and `params` set.
+     */
+    public static function extractPlaceholders(EntityInterface $entity, array $fields): array
+    {
+        $placeholders = [];
+        foreach ($fields as $field) {
+            $datum = $entity->get($field);
+            if (empty($datum)) {
+                continue;
+            }
+
+            if (!is_string($datum) || preg_match_all(static::REGEX, $datum, $matches, PREG_SET_ORDER | PREG_OFFSET_CAPTURE) === false) {
+                throw new RuntimeException(__d('bedita', 'Error extracting placeholders'));
+            }
+
+            foreach ($matches as $match) {
+                $offsetBytes = $match[0][1]; // This is the offset in bytes!!
+                $offset = mb_strlen(substr($datum, 0, $offsetBytes)); // Turn bytes offset into character offset.
+                $length = mb_strlen($match[0][0]);
+                $id = (int)$match['id'][0];
+                $params = null;
+                if (!empty($match['params'][0])) {
+                    $params = base64_decode($match['params'][0]);
+                }
+
+                if (!isset($placeholders[$id])) {
+                    $placeholders[$id] = [
+                        'id' => $id,
+                        'params' => [],
+                    ];
+                }
+                $placeholders[$id]['params'][$field][] = compact('offset', 'length', 'params');
+            }
+        }
+
+        return array_values($placeholders);
+    }
+
+    /**
+     * Get Association.
+     *
+     * @param bool $direct `true` for direct association, or `false` to use inverse.
+     * @return \Cake\ORM\Association
+     */
+    protected function getAssociation(bool $direct): Association
+    {
+        $relName = $this->getConfigOrFail('relation');
+        if (!isset($this->relation)) {
+            /** @var \BEdita\Core\Model\Table\RelationsTable $Relations */
+            $Relations = $this->getTableLocator()->get('Relations');
+
+            $this->relation = $Relations->get($relName);
+        }
+
+        $assoc = $this->relation->alias;
+        if ((!$direct && $this->relation->name === $relName) || ($direct && $this->relation->inverse_name === $relName)) {
+            $assoc = $this->relation->inverse_alias;
+        }
+
+        $association = $this->getTable()->getAssociation($assoc);
+        if (!in_array($association->type(), [Association::ONE_TO_MANY, Association::MANY_TO_MANY])) {
+            throw new InvalidArgumentException(sprintf('Invalid association type "%s"', get_class($association)));
+        }
+
+        return $association;
+    }
 
     /**
      * Add associations using placeholder relation.
@@ -41,87 +145,121 @@ class PlaceholdersBehavior extends Behavior
      * @param \Cake\Datasource\EntityInterface $entity Entity.
      * @return void
      */
-    public function afterSave(Event $event, EntityInterface $entity)
+    public function afterSave(Event $event, EntityInterface $entity): void
     {
-        $properties = $this->getHTMLProperties($entity);
-
-        $placeholders = array_unique(array_reduce(
-            $properties,
-            function (array $placeholders, string $property) use ($entity): array {
-                return array_merge($placeholders, static::extractPlaceholders($entity->get($property)));
-            },
-            []
-        ));
-
-        $relatedEntities = array_map(function (int $id): array {
-            return compact('id');
-        }, $placeholders);
-        $Association = $this->getTable()->getAssociation('Placeholder');
-        if (!$Association->replaceLinks($entity, $relatedEntities)) {
-            throw new PersistenceFailedException($entity, __d('bedita', 'Could not save placeholder relations'));
+        $fields = $this->getConfigOrFail('fields');
+        if (empty($fields)) {
+            return;
         }
+
+        $association = $this->getAssociation(true);
+
+        $extract = $this->getConfig('extract');
+        if (!isset($extract)) {
+            $extract = [static::class, 'extractPlaceholders'];
+        }
+        $placeholders = $extract($entity, $fields);
+        $relatedEntities = $this->prepareEntities($association->getTarget(), $placeholders);
+
+        $action = new SetRelatedObjectsAction(compact('association'));
+        $action(compact('entity', 'relatedEntities'));
     }
 
     /**
-     * Parse HTML content and extracts media references.
+     * Prepare target entities.
      *
-     * @param string $content The content to parse.
-     * @return int[] A list of ids.
+     * @param \Cake\ORM\Table $table Target table.
+     * @param array[] $placeholders Placeholders data.
+     * @return \Cake\Datasource\EntityInterface[]
      */
-    private static function extractPlaceholders(string $content): array
+    protected function prepareEntities(Table $table, array $placeholders): array
     {
-        if (preg_match_all(static::REGEX, $content, $matches) === false) {
-            throw new RuntimeException('Error extracting placeholders');
-        }
-
-        return $matches['id'];
-    }
-
-    /**
-     * Get a list of properties names that accept text/html.
-     *
-     * @return array A list of properties.
-     */
-    private function getHTMLProperties()
-    {
-        $Table = $this->getTable();
-        $objectType = $Table->behaviors()->call('objectType', [$Table->getAlias()]);
-        $schema = JsonSchema::generate($objectType['name'], '/');
-
-        if (empty($schema['properties'])) {
+        $pk = $table->getPrimaryKey();
+        $ids = array_column($placeholders, 'id');
+        if (empty($ids)) {
             return [];
         }
 
-        $properties = [];
-        foreach ($schema['properties'] as $name => $propertySchema) {
-            if ($this->isHTMLType($propertySchema)) {
-                $properties[] = $name;
-            }
-        }
+        return $table->find()
+            ->select($table->aliasField($pk))
+            ->where(function (QueryExpression $exp) use ($table, $pk, $ids): QueryExpression {
+                return $exp->in($table->aliasField($pk), $ids);
+            })
+            ->map(function (EntityInterface $entity) use ($pk, $placeholders): EntityInterface {
+                $id = $entity->get($pk);
+                foreach ($placeholders as $datum) {
+                    if ($datum['id'] == $id) {
+                        $entity->set('_joinData', [
+                            'params' => $datum['params'],
+                        ]);
 
-        return $properties;
+                        break;
+                    }
+                }
+
+                return $entity;
+            })
+            ->toList();
     }
 
     /**
-     * Check if a property schema accepts text/html.
+     * Lock entity from being soft-deleted if it is placeholded somewhere.
      *
-     * @param array $propertySchema The property schema.
-     * @return bool
+     * @param \Cake\Event\Event $event Dispatched event.
+     * @param \Cake\Datasource\EntityInterface $entity Entity being saved.
+     * @return void
      */
-    private function isHTMLType($propertySchema)
+    public function beforeSave(Event $event, EntityInterface $entity): void
     {
-        if (!empty($propertySchema['oneOf'])) {
-            return !empty(
-                array_filter($propertySchema['oneOf'], function ($subProp) {
-                    return $this->isHTMLType($subProp);
-                })
-            );
+        if (!$entity->isDirty('deleted') || !$entity->get('deleted')) {
+            return;
         }
 
-        if (empty($propertySchema['contentMediaType'])) {
-            return false;
+        $this->ensureNotPlaceholded($entity);
+    }
+
+    /**
+     * Lock entity from being hard-deleted if it is placeholded somewhere.
+     *
+     * @param \Cake\Event\Event $event Dispatched event.
+     * @param \Cake\Datasource\EntityInterface $entity Entity being saved.
+     * @return void
+     */
+    public function beforeDelete(Event $event, EntityInterface $entity): void
+    {
+        $this->ensureNotPlaceholded($entity);
+    }
+
+    /**
+     * Ensure an entity does not appear as a placeholder.
+     *
+     * @param \Cake\Datasource\EntityInterface $entity Entity being checked.
+     * @return void
+     *
+     * @throws \BEdita\Core\Exception\LockedResourceException
+     */
+    protected function ensureNotPlaceholded(EntityInterface $entity): void
+    {
+        $Table = $this->getTable();
+        $association = $this->getAssociation(false);
+
+        $refCount = $Table->find()
+            ->select(['existing' => 1])
+            ->where(array_combine(
+                array_map([$Table, 'aliasField'], (array)$Table->getPrimaryKey()),
+                $entity->extract((array)$Table->getPrimaryKey())
+            ))
+            ->innerJoinWith($association->getName())
+            ->count();
+        if ($refCount === 0) {
+            return;
         }
 
-        return $propertySchema['contentMediaType'] === 'text/html';
+        throw new LockedResourceException(__d(
+            'bedita',
+            'Cannot delete object {0} because it is still placeholded in {1,plural,=1{one object} other{# objects}}',
+            $entity->id,
+            $refCount
+        ));
     }
 }
--- plugins/BEdita/Core/tests/TestCase/Model/Behavior/PlaceholdersBehaviorTest.php
+++ plugins/BEdita/Core/tests/TestCase/Model/Behavior/PlaceholdersBehaviorTest.php
@@ -13,8 +13,14 @@
 
 namespace BEdita\Core\Test\TestCase\Model\Behavior;
 
-use Cake\ORM\TableRegistry;
+use BEdita\Core\Exception\LockedResourceException;
+use BEdita\Core\Filesystem\FilesystemRegistry;
+use BEdita\Core\Model\Action\AddRelatedObjectsAction;
+use BEdita\Core\Model\Behavior\PlaceholdersBehavior;
+use Cake\Core\Configure;
+use Cake\ORM\Entity;
 use Cake\TestSuite\TestCase;
+use Cake\Utility\Hash;
 
 /**
  * {@see \BEdita\Core\Model\Behavior\PlaceholdersBehavior} Test Case
@@ -40,76 +46,375 @@ class PlaceholdersBehaviorTest extends TestCase
         'plugin.BEdita/Core.Media',
         'plugin.BEdita/Core.Profiles',
         'plugin.BEdita/Core.Users',
+        'plugin.BEdita/Core.Streams',
+        'plugin.BEdita/Core.Trees',
         'plugin.BEdita/Core.Categories',
         'plugin.BEdita/Core.ObjectCategories',
         'plugin.BEdita/Core.History',
     ];
 
+    /** @inheritDoc */
+    public function setUp()
+    {
+        parent::setUp();
+
+        FilesystemRegistry::setConfig(Configure::read('Filesystem'));
+    }
+
+    /** @inheritDoc */
+    public function tearDown()
+    {
+        FilesystemRegistry::dropAll();
+
+        parent::tearDown();
+    }
+
     /**
-     * Data provider for `testBeforeSave` test case.
+     * Data provider for {@see PlaceholdersBehaviorTest::testExtractPlaceholders()} test case.
      *
-     * @return array
+     * @return array[]
      */
-    public function beforeSaveProvider()
+    public function extractPlaceholdersProvider(): array
     {
         return [
-            'without placeholders' => [
-                '<p>This is a text</p>',
-                'Documents',
-                2,
+            'no configured fields' => [
+                [],
                 [
-                    'body' => '<p>This is a text</p>',
+                    'body' => '<p>This is a text</p><!-- BE-PLACEHOLDER.10.eyAiZm9vIjogImJhciIgfQ== -->',
                 ],
                 [],
             ],
-            'with placeholders' => [
-                '<p>This is a text</p><!-- BE-PLACEHOLDER 10 eyAiZm9vIjogImJhciIgfQ== -->',
-                'Documents',
-                2,
+            'missing field' => [
+                [],
                 [
-                    'body' => '<p>This is a text</p><!-- BE-PLACEHOLDER 10 eyAiZm9vIjogImJhciIgfQ== -->',
+                    'body' => '<p>This is a text</p><!-- BE-PLACEHOLDER.10.eyAiZm9vIjogImJhciIgfQ== -->',
                 ],
+                ['description'],
+            ],
+            'simple' => [
                 [
                     [
                         'id' => 10,
-                        'type' => 'media',
+                        'params' => [
+                            'body' => [
+                                [
+                                    'offset' => 21,
+                                    'length' => 26,
+                                    'params' => null,
+                                ],
+                            ],
+                        ],
                     ],
-                ]
+                ],
+                [
+                    'body' => '<p>This is a text</p><!-- BE-PLACEHOLDER.10 -->',
+                ],
+            ],
+            'simple, with params' => [
+                [
+                    [
+                        'id' => 10,
+                        'params' => [
+                            'body' => [
+                                [
+                                    'offset' => 21,
+                                    'length' => 51,
+                                    'params' => '{ "foo": "bar" }',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                [
+                    'body' => '<p>This is a text</p><!-- BE-PLACEHOLDER.10.eyAiZm9vIjogImJhciIgfQ== -->',
+                ],
+            ],
+            'multibyte string, multiple fields, with params' => [
+                [
+                    [
+                        'id' => 10,
+                        'params' => [
+                            'description' => [
+                                [
+                                    'offset' => 0,
+                                    'length' => 26,
+                                    'params' => null,
+                                ],
+                                [
+                                    'offset' => 55,
+                                    'length' => 51,
+                                    'params' => '{ "foo": "bar" }',
+                                ],
+                            ],
+                            'body' => [
+                                [
+                                    'offset' => 25,
+                                    'length' => 26,
+                                    'params' => null,
+                                ],
+                            ],
+                        ],
+                    ],
+                    [
+                        'id' => 14,
+                        'params' => [
+                            'body' => [
+                                [
+                                    'offset' => 75,
+                                    'length' => 43,
+                                    'params' => 'not a json',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                [
+                    'title' => '<strong>This won\'t be extracted: <!-- BE-PLACEHOLDER.1 --></strong>',
+                    'description' => '<!-- BE-PLACEHOLDER.10 --><h1>My sweet placeholder</h1><!-- BE-PLACEHOLDER.10.eyAiZm9vIjogImJhciIgfQ== -->',
+                    'body' => '<p>Is this working? 😶</p><!-- BE-PLACEHOLDER.10 --><p>Apparently yes! 👍</p><!-- BE-PLACEHOLDER.14.bm90IGEganNvbg== -->',
+                ],
             ],
         ];
     }
 
     /**
-     * Test bodies with placeholder `save`.
+     * Test {@see PlaceholdersBehavior::extractPlaceholders()}.
      *
-     * @param array $expected Expected result.
-     * @param string $tableName Table.
-     * @param int $id Entity ID.
-     * @param array $data Data.
+     * @param array[] $expected Expected result.
+     * @param array $data Entity data.
+     * @param string[] $fields Fields.
      * @return void
      *
-     * @dataProvider beforeSaveProvider()
-     * @covers ::beforeSave()
+     * @dataProvider extractPlaceholdersProvider()
      * @covers ::extractPlaceholders()
-     * @covers ::getHTMLAttributes()
-     * @covers ::isHTMLAttribute()
      */
-    public function testBeforeSave(string $expected, string $tableName, int $id, array $data)
+    public function testExtractPlaceholders(array $expected, array $data, array $fields = ['description', 'body']): void
     {
-        $table = TableRegistry::getTableLocator()->get($tableName);
+        $entity = new Entity($data);
+        $actual = PlaceholdersBehavior::extractPlaceholders($entity, $fields);
 
-        $objectType = $table
-            ->getAssociation('ObjectTypes')
-            ->get($tableName);
-        $options = [];
-        if (!empty($objectType->get('associations'))) {
-            $options = ['contain' => $objectType->get('associations')];
+        static::assertSame($expected, $actual);
+    }
+
+    /**
+     * Test {@see PlaceholdersBehavior::afterSave()}.
+     *
+     * @return void
+     *
+     * @covers ::afterSave()
+     * @covers ::getAssociation()
+     * @covers ::prepareEntities()
+     */
+    public function testSavePlaceholders(): void
+    {
+        $body = '<!-- BE-PLACEHOLDER.10 --><h1>My sweet placeholder</h1><!-- BE-PLACEHOLDER.10.eyAiZm9vIjogImJhciIgfQ== -->';
+        $expected = [
+            10 => [
+                'body' => [
+                    [
+                        'offset' => 0,
+                        'length' => 26,
+                        'params' => null,
+                    ],
+                    [
+                        'offset' => 55,
+                        'length' => 51,
+                        'params' => '{ "foo": "bar" }',
+                    ],
+                ],
+            ],
+        ];
+
+        $table = $this->getTableLocator()->get('Documents');
+
+        // Save with placeholder in body.
+        $entity = $table->get(2, ['contain' => ['ObjectTypes']]);
+        $entity->set('body', $body);
+        $table->saveOrFail($entity);
+
+        // Reload entity from database, with placeholders.
+        $entity = $table->get(2, ['contain' => ['ObjectTypes', 'Placeholder']]);
+        static::assertSame($body, $entity->get('body'), 'Entity body has been changed');
+        static::assertTrue($entity->has('placeholder'));
+
+        // Run assertions.
+        $placeholders = $entity->get('placeholder');
+        $ids = Hash::extract($placeholders, '{n}.id');
+        static::assertSame(array_keys($expected), $ids);
+        foreach ($placeholders as $placeholder) {
+            $id = Hash::get($placeholder, 'id');
+            $params = Hash::get($placeholder, '_joinData.params');
+
+            static::assertSame($expected[$id], $params);
         }
-        $entity = $table->get($id, $options);
+    }
 
-        $entity = $table->patchEntity($entity, $data);
-        $entity = $table->save($entity);
+    /**
+     * Test {@see PlaceholdersBehavior::afterSave()}.
+     *
+     * @return void
+     *
+     * @covers ::afterSave()
+     * @covers ::getAssociation()
+     * @covers ::prepareEntities()
+     */
+    public function testSavePlaceholdersReplace(): void
+    {
+        $body = '<!-- BE-PLACEHOLDER.10 --><h1>My sweet placeholder</h1><!-- BE-PLACEHOLDER.10.eyAiZm9vIjogImJhciIgfQ== -->';
+        $expected = [
+            10 => [
+                'body' => [
+                    [
+                        'offset' => 0,
+                        'length' => 26,
+                        'params' => null,
+                    ],
+                    [
+                        'offset' => 55,
+                        'length' => 51,
+                        'params' => '{ "foo": "bar" }',
+                    ],
+                ],
+            ],
+        ];
 
-        static::assertEquals($expected, $entity['body']);
+        $table = $this->getTableLocator()->get('Documents');
+
+        // Save hypothetical previous data.
+        $media = $this->getTableLocator()->get('Media');
+        $action = new AddRelatedObjectsAction(['association' => $table->getAssociation('Placeholder')]);
+        $action([
+            'entity' => $table->get(2, ['contain' => ['ObjectTypes']]),
+            'relatedEntities' => [
+                $media->get(10, ['contain' => ['ObjectTypes']])->set(['_joinData' => ['params' => ['description' => []]]]),
+                $media->get(14, ['contain' => ['ObjectTypes']]),
+            ],
+        ]);
+
+        // Save with placeholder in body.
+        $entity = $table->get(2, ['contain' => ['ObjectTypes', 'Placeholder']]);
+        static::assertSame([10, 14], Hash::extract($entity->get('placeholder'), '{n}.id')); // Check that previous placeholders exist.
+        $entity->set('body', $body);
+        $table->saveOrFail($entity);
+
+        // Reload entity from database, with placeholders.
+        $entity = $table->get(2, ['contain' => ['ObjectTypes', 'Placeholder']]);
+        static::assertSame($body, $entity->get('body'), 'Entity body has been changed');
+        static::assertTrue($entity->has('placeholder'));
+
+        // Run assertions.
+        $placeholders = $entity->get('placeholder');
+        $ids = Hash::extract($placeholders, '{n}.id');
+        static::assertSame(array_keys($expected), $ids);
+        foreach ($placeholders as $placeholder) {
+            $id = Hash::get($placeholder, 'id');
+            $params = Hash::get($placeholder, '_joinData.params');
+
+            static::assertSame($expected[$id], $params);
+        }
+    }
+
+    /**
+     * Test {@see PlaceholdersBehavior::afterSave()}.
+     *
+     * @return void
+     *
+     * @covers ::afterSave()
+     * @covers ::getAssociation()
+     * @covers ::prepareEntities()
+     */
+    public function testSavePlaceholdersRemove(): void
+    {
+        $body = '<h1>My sweet placeholder</h1>';
+
+        $table = $this->getTableLocator()->get('Documents');
+
+        // Save hypothetical previous data.
+        $media = $this->getTableLocator()->get('Media');
+        $action = new AddRelatedObjectsAction(['association' => $table->getAssociation('Placeholder')]);
+        $action([
+            'entity' => $table->get(2, ['contain' => ['ObjectTypes']]),
+            'relatedEntities' => [
+                $media->get(10, ['contain' => ['ObjectTypes']])->set(['_joinData' => ['params' => ['description' => []]]]),
+                $media->get(14, ['contain' => ['ObjectTypes']]),
+            ],
+        ]);
+
+        // Save with placeholder in body.
+        $entity = $table->get(2, ['contain' => ['ObjectTypes', 'Placeholder']]);
+        static::assertSame([10, 14], Hash::extract($entity->get('placeholder'), '{n}.id')); // Check that previous placeholders exist.
+        $entity->set('body', $body);
+        $table->saveOrFail($entity);
+
+        // Reload entity from database, with placeholders.
+        $entity = $table->get(2, ['contain' => ['ObjectTypes', 'Placeholder']]);
+        static::assertSame($body, $entity->get('body'), 'Entity body has been changed');
+        static::assertTrue($entity->has('placeholder'));
+
+        // Run assertions.
+        $placeholders = $entity->get('placeholder');
+        $ids = Hash::extract($placeholders, '{n}.id');
+        static::assertSame([], $ids);
+    }
+
+    /**
+     * Test {@see PlaceholdersBehavior::beforeSave()}.
+     *
+     * @return void
+     *
+     * @covers ::beforeSave()
+     * @covers ::getAssociation()
+     * @covers ::ensureNotPlaceholded()
+     */
+    public function testBeforeSaveLockedEntity(): void
+    {
+        $body = '<!-- BE-PLACEHOLDER.10 --><h1>My sweet placeholder</h1>';
+
+        // Save with placeholder in body.
+        $table = $this->getTableLocator()->get('Documents');
+        $entity = $table->get(2, ['contain' => ['ObjectTypes']]);
+        $entity->set('body', $body);
+        $table->saveOrFail($entity);
+
+        $entity = $table->get(2, ['contain' => ['ObjectTypes', 'Placeholder']]);
+        static::assertSame([10], Hash::extract($entity->get('placeholder'), '{n}.id'));
+
+        // Try to delete media.
+        $this->expectException(LockedResourceException::class);
+        $this->expectExceptionMessage('Cannot delete object 10 because it is still placeholded in one object');
+        $table = $this->getTableLocator()->get('Media');
+        $entity = $table->get(10, ['contain' => ['ObjectTypes']]);
+        $entity->set('deleted', true);
+        $table->saveOrFail($entity);
+    }
+
+    /**
+     * Test {@see PlaceholdersBehavior::beforeDelete()}.
+     *
+     * @return void
+     *
+     * @covers ::beforeDelete()
+     * @covers ::getAssociation()
+     * @covers ::ensureNotPlaceholded()
+     */
+    public function testBeforeDeleteLockedEntity(): void
+    {
+        $body = '<!-- BE-PLACEHOLDER.10 --><h1>My sweet placeholder</h1>';
+
+        // Save with placeholder in body.
+        $table = $this->getTableLocator()->get('Documents');
+        $entity = $table->get(2, ['contain' => ['ObjectTypes']]);
+        $entity->set('body', $body);
+        $table->saveOrFail($entity);
+
+        $entity = $table->get(2, ['contain' => ['ObjectTypes', 'Placeholder']]);
+        static::assertSame([10], Hash::extract($entity->get('placeholder'), '{n}.id'));
+
+        // Try to delete media.
+        $this->expectException(LockedResourceException::class);
+        $this->expectExceptionMessage('Cannot delete object 10 because it is still placeholded in one object');
+        $table = $this->getTableLocator()->get('Media');
+        $entity = $table->get(10, ['contain' => ['ObjectTypes']]);
+        $table->deleteOrFail($entity);
     }
 }
