c309d8b94189c2d26fc211393dc064a602a02272
stefanorosanelli <stefano.rosanelli@gmail.com>
2021-07-09 14:59:37 +0200

    #1336 test: drop registry after usage

--- plugins/BEdita/Core/tests/TestCase/Command/CustomPropsCommandTest.php
+++ plugins/BEdita/Core/tests/TestCase/Command/CustomPropsCommandTest.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2021 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
 namespace BEdita\Core\Test\TestCase\Command;
 
 use BEdita\Core\Filesystem\Adapter\LocalAdapter;
@@ -75,6 +86,7 @@ class CustomPropsCommandTest extends TestCase
             'className' => LocalAdapter::class,
         ]);
         $this->exec('custom_props');
+        FilesystemRegistry::dropAll();
         $this->assertOutputContains('Updated 2 users without errors');
         $this->assertExitSuccess();
     }
