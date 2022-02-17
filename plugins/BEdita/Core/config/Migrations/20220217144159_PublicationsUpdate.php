35296b37c58c16d8bb786f5c30a5cb6acbd26d62
dante di domenico <dante@didoda.com>
2022-02-17 16:52:58 +0100

    fix: migration, stats_code only

--- plugins/BEdita/Core/config/Migrations/20220217144159_PublicationsUpdate.php
+++ plugins/BEdita/Core/config/Migrations/20220217144159_PublicationsUpdate.php
@@ -9,19 +9,7 @@ class PublicationsUpdate extends AbstractMigration
     public function up()
     {
         $this->table('publications')
-            ->changeColumn('public_url', 'string', [
-                'comment' => 'the public url',
-                'default' => null,
-                'limit' => null,
-                'null' => true,
-            ])
-            ->changeColumn('staging_url', 'string', [
-                'comment' => 'the staging url',
-                'default' => null,
-                'limit' => null,
-                'null' => true,
-            ])
-            ->changeColumn('stats_code', 'string', [
+            ->changeColumn('stats_code', 'text', [
                 'comment' => 'the code for statistics',
                 'default' => null,
                 'limit' => null,
@@ -36,19 +24,7 @@ class PublicationsUpdate extends AbstractMigration
     public function down()
     {
         $this->table('publications')
-            ->changeColumn('public_url', 'text', [
-                'comment' => 'the public url',
-                'default' => null,
-                'limit' => null,
-                'null' => true,
-            ])
-            ->changeColumn('staging_url', 'text', [
-                'comment' => 'the staging url',
-                'default' => null,
-                'limit' => null,
-                'null' => true,
-            ])
-            ->changeColumn('stats_code', 'text', [
+            ->changeColumn('stats_code', 'string', [
                 'comment' => 'the code for statistics',
                 'default' => null,
                 'limit' => null,
