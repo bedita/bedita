ba743859449aad5d617f056f2b2a6fa69709cdbf
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2022-02-25 10:51:34 +0100

    chore: remove empty line

--- plugins/BEdita/Core/src/Exception/ImmutableResourceException.php
+++ plugins/BEdita/Core/src/Exception/ImmutableResourceException.php
@@ -18,7 +18,6 @@ use Cake\Core\Exception\Exception;
 /**
  * Exception raised when performing delete or update on immutable resource
  */
-
 class ImmutableResourceException extends Exception
 {
     /**
