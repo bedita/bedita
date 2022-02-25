b01ae85f56460dff569056df039dc2c8bb123c6e
stefanorosanelli <stefano.rosanelli@gmail.com>
2022-02-25 15:21:13 +0100

    chore: update annotations

--- plugins/BEdita/API/tests/TestCase/Controller/JsonBaseControllerTest.php
+++ plugins/BEdita/API/tests/TestCase/Controller/JsonBaseControllerTest.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2022 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
 namespace BEdita\API\Test\TestCase\Controller;
 
 use BEdita\API\Controller\JsonBaseController;
@@ -8,7 +19,7 @@ use Cake\TestSuite\TestCase;
 /**
  * BEdita\API\Controller\JsonBaseController Test Case
  *
- * @uses \BEdita\API\Controller\JsonBaseController
+ * @coversDefaultClass  \BEdita\API\Controller\JsonBaseController
  */
 class JsonBaseControllerTest extends TestCase
 {
