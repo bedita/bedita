700f1755f3096a7028ad8a6915ed6207b5dd6905
stefanorosanelli <stefano.rosanelli@gmail.com>
2022-03-28 15:08:37 +0200

    test: `InvalidDataException` test case

--- plugins/BEdita/API/tests/TestCase/Model/Action/UpdateAssociatedActionTest.php
+++ plugins/BEdita/API/tests/TestCase/Model/Action/UpdateAssociatedActionTest.php
@@ -14,9 +14,9 @@
 namespace BEdita\API\Test\TestCase\Model\Action;
 
 use BEdita\API\Model\Action\UpdateAssociatedAction;
+use BEdita\Core\Exception\InvalidDataException;
 use BEdita\Core\Model\Action\SetAssociatedAction;
 use Cake\Datasource\Exception\RecordNotFoundException;
-use Cake\Http\Exception\BadRequestException;
 use Cake\Http\ServerRequest;
 use Cake\ORM\Query;
 use Cake\ORM\TableRegistry;
@@ -179,7 +179,7 @@ class UpdateAssociatedActionTest extends TestCase
                 ],
             ],
             'belongsToMany invalid parameters' => [
-                new BadRequestException([
+                new InvalidDataException([
                     'title' => 'Bad data',
                 ]),
                 'FakeTags',
