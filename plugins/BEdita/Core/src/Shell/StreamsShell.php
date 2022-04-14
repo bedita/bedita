ea18ff7a3a2988de415506de57fa8351e2b1515d
le0m <mainardi.leo@gmail.com>
2022-04-14 14:50:07 +0200

    chore: remove unnecessary check for isDirty()

--- plugins/BEdita/Core/src/Shell/StreamsShell.php
+++ plugins/BEdita/Core/src/Shell/StreamsShell.php
@@ -147,7 +147,7 @@ class StreamsShell extends Shell
 
             // ...and write it back, triggering Stream model's methods to read metadata from file
             $stream->contents = $content;
-            if ($stream->isDirty() && !$this->Streams->save($stream)) {
+            if (!$this->Streams->save($stream)) {
                 $this->err(sprintf('  error updating stream %s (object %d): %s', $stream->uuid, $stream->object_id, print_r($stream->getErrors(), true)));
 
                 return false;
