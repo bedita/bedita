27263331157fbca6f67fe356c945f62e85f2e6cc
stefanorosanelli <stefano.rosanelli@gmail.com>
2022-05-11 13:02:35 +0200

    #1901 chore: improve `process` method

--- plugins/BEdita/API/src/Middleware/CorsMiddleware.php
+++ plugins/BEdita/API/src/Middleware/CorsMiddleware.php
@@ -86,14 +86,7 @@ class CorsMiddleware implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        // if ($request->getMethod() == 'OPTIONS') {
-        //     return $this->buildCors($request, $response);
-        // }
-
-        // $response = $next($request, $response);
-        $response = $handler->handle($request);
-
-        return $this->buildCors($request, $response);
+        return $this->buildCors($request, $handler->handle($request));
     }
 
     /**
