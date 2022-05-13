23a85bdb341577fdc1241d067317addd13a8e501
Edoardo Cavazza <edoardo.cavazza@chialab.it>
2022-05-13 12:07:45 +0200

    feat: add methods to expand and compact priorities list

--- plugins/BEdita/Core/src/Model/Behavior/PriorityBehavior.php
+++ plugins/BEdita/Core/src/Model/Behavior/PriorityBehavior.php
@@ -15,6 +15,7 @@ namespace BEdita\Core\Model\Behavior;
 use Cake\Datasource\EntityInterface;
 use Cake\Event\Event;
 use Cake\ORM\Behavior;
+use Cake\ORM\Entity;
 use Cake\Utility\Hash;
 
 /**
@@ -57,6 +58,7 @@ class PriorityBehavior extends Behavior
      * Set up priorities before an entity is saved.
      * Use current max value + 1 if not set.
      * New values will start at 1.
+     * Other priorities are shifted on priority change.
      *
      * @param \Cake\Event\Event $event Dispatched event.
      * @param \Cake\Datasource\EntityInterface $entity Entity instance.
@@ -64,52 +66,129 @@ class PriorityBehavior extends Behavior
      */
     public function beforeSave(Event $event, EntityInterface $entity)
     {
-        $fields = $this->getConfig('fields');
+        if (empty($config['scope'])) {
+            return;
+        }
 
+        $conditions = $this->_getConditions($entity, $config['scope']);
+        $fields = $this->getConfig('fields');
         foreach ($fields as $field => $config) {
-            if (!empty($entity->get($field)) || empty($config['scope'])) {
+            if ($entity->isDirty($field)) {
+                if ($entity instanceof Entity) {
+                    $actualValue = $entity->get($field);
+                    $previousValue = $entity->getOriginal($field);
+
+                    if ($previousValue < $actualValue) {
+                        $this->compact($field, $actualValue, $previousValue, $conditions);
+                    } elseif ($previousValue > $actualValue) {
+                        $this->expand($field, $actualValue, $previousValue, $conditions);
+                    }
+                }
+                continue;
+            }
+            if (!empty($entity->get($field))) {
                 continue;
             }
 
-            $maxValue = $this->maxValue($entity, $field, (array)$config['scope']);
+            $maxValue = $this->maxValue($field, $conditions);
             $entity->set($field, $maxValue + 1);
         }
     }
 
     /**
-     * Get current max priority on an object relation
+     * Compact other priorities before the entity is deleted.
      *
-     * @param \Cake\Datasource\EntityInterface $entity Entity being saved
-     * @param string $field Priority field name.
-     * @param array $scope Priority scope.
-     * @return int
+     * @param \Cake\Event\Event $event Dispatched event.
+     * @param \Cake\Datasource\EntityInterface $entity Entity instance.
+     * @return void
      */
-    protected function maxValue(EntityInterface $entity, string $field, array $scope): int
+    public function beforeDelete(Event $event, EntityInterface $entity)
+    {
+        if (empty($config['scope'])) {
+            return;
+        }
+
+        $conditions = $this->_getConditions($entity, $config['scope']);
+        $fields = $this->getConfig('fields');
+        foreach ($fields as $field => $config) {
+            if (empty($entity->get($field))) {
+                continue;
+            }
+
+            $this->compact($field, $entity->get($field), null, $conditions);
+        }
+    }
+
+    /**
+     * Get scope conditions from entity.
+     *
+     * @param \Cake\Datasource\EntityInterface $entity Entity instance.
+     * @param string[] $scope A list of scope fields.
+     * @return array A list of conditions.
+     */
+    protected function _getConditions(EntityInterface $entity, array $scope): array
     {
         $conditions = [];
         foreach ($scope as $item) {
             $keyField = sprintf('%s IS', $this->getTable()->aliasField($item));
             $conditions[$keyField] = $entity->get($item);
         }
+
+        return $conditions;
+    }
+
+    /**
+     * Create a gap in the priority list where an item can be inserted or moved.
+     *
+     * @param string $field Field name.
+     * @param int $from The initial priority value to update.
+     * @param int|null $to The final priority value to update.
+     * @param array $conditions A list of scope conditions.
+     * @return void
+     */
+    protected function expand(string $field, int $from, ?int $to = null, array $conditions = []): void
+    {
+        $conditions = $conditions + ["{$field} >=" => $from];
+        if ($to !== null) {
+            $conditions["{$field} <"] = $to;
+        }
+
+        $this->getTable()->updateAll([sprintf('%s = %1$s + 1', $field)], $conditions);
+    }
+
+    /**
+     * Compact priority values.
+     *
+     * @param string $field Field name.
+     * @param int $from The initial priority value to update.
+     * @param int|null $to The final priority value to update.
+     * @param array $conditions A list of scope conditions.
+     * @return void
+     */
+    protected function compact(string $field, int $from, ?int $to = null, array $conditions = []): void
+    {
+        $conditions = $conditions + ["{$field} >" => $from];
+        if ($to !== null) {
+            $conditions["{$field} <="] = $to;
+        }
+
+        $this->getTable()->updateAll([sprintf('%s = %1$s - 1', $field)], $conditions);
+    }
+
+    /**
+     * Get current max priority on an object relation
+     *
+     * @param string $field Priority field name.
+     * @param array $conditions Scope conditions.
+     * @return int
+     */
+    protected function maxValue(string $field, array $conditions): int
+    {
         $query = $this->getTable()->find()->where($conditions);
         $query->select([
             'max_value' => $query->func()->max($this->getTable()->aliasField($field)),
         ]);
 
         return (int)Hash::get($query->toArray(), '0.max_value');
-    }
-
-    /**
-     * Compact other priorities before the entity is deleted.
-     *
-     * @todo Implement this method.
-     * @param \Cake\Event\Event $event Dispatched event.
-     * @param \Cake\Datasource\EntityInterface $entity Entity instance.
-     * @return void
-     * @codeCoverageIgnore
-     */
-    public function beforeDelete(Event $event, EntityInterface $entity)
-    {
-        return;
     }
 }
