cda2a81e4195a15b2ab0ed9f6c4caa411eb3d4b0
nicolo.carpignoli <nicolo.carpignoli@chialab.it>
2022-06-27 09:40:11 +0200

    fix: wrong refactor

--- plugins/BEdita/Core/tests/TestCase/Shell/StreamsShellTest.php
+++ plugins/BEdita/Core/tests/TestCase/Shell/StreamsShellTest.php
@@ -76,7 +76,7 @@ class StreamsShellTest extends ConsoleIntegrationTestCase
         $this->Streams->updateAll(['width' => null], []);
         $this->exec('streams refreshMetadata');
 
-        $query = $this->Streams->find('all')->all();
+        $results = $this->Streams->find('all')->all();
         $data = $results->toList();
 
         foreach ($data as $entry) {
@@ -91,7 +91,7 @@ class StreamsShellTest extends ConsoleIntegrationTestCase
         $this->Streams->updateAll(['width' => 800], []);
         $this->exec('streams refreshMetadata --force');
 
-        $query = $this->Streams->find('all')->all();
+        $results = $this->Streams->find('all')->all();
         $lastData = $results->toList();
 
         foreach ($lastData as $entry) {
