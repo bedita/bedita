3b5fd5974d9d914af2b662be97d710517d1691de
dante di domenico <dante@didoda.com>
2022-12-05 08:31:20 +0100

    fix: migration, to not break tests

--- plugins/BEdita/Core/config/Migrations/20221201092959_AddChildrenOrder.php
+++ plugins/BEdita/Core/config/Migrations/20221201092959_AddChildrenOrder.php
@@ -12,6 +12,7 @@
  */
 
 use BEdita\Core\Utility\Resources;
+use Cake\ORM\TableRegistry;
 use Migrations\AbstractMigration;
 
 class AddChildrenOrder extends AbstractMigration
@@ -34,16 +35,18 @@ class AddChildrenOrder extends AbstractMigration
                 'core_type' => 1,
             ],
         ],
-        'properties' => [
-            [
-                'name' => 'children_order',
-                'object' => 'folders',
-                'property' => 'children_order',
-                'description' => 'Folders children order',
-                'enabled' => 1,
-                'is_nullable' => 1,
-            ],
-        ],
+        // this does not work in tests, for an issue with static_properties temporary table
+        // we instead use a sql insert/delete query
+        // 'properties' => [
+        //     [
+        //         'name' => 'children_order',
+        //         'object' => 'folders',
+        //         'property' => 'children_order',
+        //         'description' => 'Folders children order',
+        //         'enabled' => 1,
+        //         'is_nullable' => 1,
+        //     ],
+        // ],
     ];
 
     /**
@@ -55,6 +58,27 @@ class AddChildrenOrder extends AbstractMigration
             ['create' => $this->create],
             ['connection' => $this->getAdapter()->getCakeConnection()]
         );
+        $objectTypeId = TableRegistry::getTableLocator()->get('ObjectTypes')->get('folders')->id;
+        $propertyTypeId = TableRegistry::getTableLocator()->get('PropertyTypes')
+            ->find()
+            ->select(['id'])
+            ->where(['name' => 'children_order'])
+            ->firstOrFail()
+            ->id;
+        $d = new \DateTime();
+        $d = $d->format('Y-m-d\TH:i:s+00:00');
+        $sql = 'INSERT INTO properties (';
+        $sql .= 'name, object_type_id, property_type_id, created, modified,';
+        $sql .= 'description, enabled, is_nullable, is_static';
+        $sql .= ') VALUES (';
+        $sql .= sprintf(
+            '"children_order", %d, %d, "%s", "%s", "Folders children order", 1, 1, 0)',
+            $objectTypeId,
+            $propertyTypeId,
+            $d,
+            $d
+        );
+        $this->query($sql);
     }
 
     /**
@@ -62,10 +86,12 @@ class AddChildrenOrder extends AbstractMigration
      */
     public function down()
     {
-        Resources::save(
-            ['remove' => ['properties' => $this->create['properties']]],
-            ['connection' => $this->getAdapter()->getCakeConnection()]
-        );
+        // Resources::save(
+        //     ['remove' => ['properties' => $this->create['properties']]],
+        //     ['connection' => $this->getAdapter()->getCakeConnection()]
+        // );
+        $sql = 'DELETE FROM properties WHERE name = "children_order"';
+        $this->query($sql);
         Resources::save(
             ['remove' => ['property_types' => $this->create['property_types']]],
             ['connection' => $this->getAdapter()->getCakeConnection()]
