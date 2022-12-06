c5b27ed875e1dc77b0b23101df66557701fd4caf
dante di domenico <dante@didoda.com>
2022-12-06 13:38:28 +0100

    refactor: use transaction in migration

--- plugins/BEdita/Core/config/Migrations/20221201092959_AddChildrenOrder.php
+++ plugins/BEdita/Core/config/Migrations/20221201092959_AddChildrenOrder.php
@@ -11,8 +11,8 @@
  * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
  */
 
-use BEdita\Core\Utility\Resources;
 use Migrations\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
 
 class AddChildrenOrder extends AbstractMigration
 {
@@ -21,67 +21,68 @@ class AddChildrenOrder extends AbstractMigration
      */
     public function up()
     {
-        $fields = [
-            'name' => 'string',
-            'params' => 'string',
-            'core_type' => 'boolean',
-        ];
-        $this->getQueryBuilder()
-            ->insert(array_keys($fields), array_values($fields))
-            ->into('property_types')
-            ->values([
-                'name' => 'children_order',
-                'params' => json_encode([
-                    'type' => 'string',
-                    'enum' => [
-                        'position',
-                        '-position',
-                        'title',
-                        '-title',
-                        'modified',
-                        '-modified',
-                    ],
-                ]),
-                'core_type' => 1,
-            ])
-            ->execute();
-
-        $objectTypeId = (int)$this->getQueryBuilder()
-            ->select(['id'])
-            ->from(['object_types'])
-            ->where(['name' => 'folders'])
-            ->execute()
-            ->fetch()[0];
-        $propertyTypesId = (int)$this->getQueryBuilder()
-            ->select(['id'])
-            ->from(['property_types'])
-            ->where(['name' => 'children_order'])
-            ->execute()
-            ->fetch()[0];
-        $fields = [
-            'name' => 'string',
-            'object_type_id' => 'int',
-            'property_type_id' => 'int',
-            'created' => 'datetime',
-            'modified' => 'datetime',
-            'enabled' => 'boolean',
-            'is_nullable' => 'boolean',
-            'is_static' => 'boolean',
-        ];
-        $this->getQueryBuilder()
-            ->insert(array_keys($fields), array_values($fields))
-            ->into('properties')
-            ->values([
-                'name' => 'children_order',
-                'object_types_id' => $objectTypeId,
-                'property_type_id' => $propertyTypesId,
-                'created' => date('Y-m-d H:i:s'),
-                'modified' => date('Y-m-d H:i:s'),
-                'enabled' => 1,
-                'is_nullable' => 1,
-                'is_static' => 0,
-            ])
-            ->execute();
+        $this->transactional(function () {
+            $fields = [
+                'name' => 'string',
+                'params' => 'string',
+                'core_type' => 'boolean',
+            ];
+            $this->getQueryBuilder()
+                ->insert(array_keys($fields), array_values($fields))
+                ->into('property_types')
+                ->values([
+                    'name' => 'children_order',
+                    'params' => json_encode([
+                        'type' => 'string',
+                        'enum' => [
+                            'position',
+                            '-position',
+                            'title',
+                            '-title',
+                            'modified',
+                            '-modified',
+                        ],
+                    ]),
+                    'core_type' => 1,
+                ])
+                ->execute();
+            $objectTypeId = (int)$this->getQueryBuilder()
+                ->select(['id'])
+                ->from(['object_types'])
+                ->where(['name' => 'folders'])
+                ->execute()
+                ->fetch()[0];
+            $propertyTypeId = (int)$this->getQueryBuilder()
+                ->select(['id'])
+                ->from(['property_types'])
+                ->where(['name' => 'children_order'])
+                ->execute()
+                ->fetch()[0];
+            $fields = [
+                'name' => 'string',
+                'object_type_id' => 'int',
+                'property_type_id' => 'int',
+                'created' => 'datetime',
+                'modified' => 'datetime',
+                'enabled' => 'boolean',
+                'is_nullable' => 'boolean',
+                'is_static' => 'boolean',
+            ];
+            $this->getQueryBuilder()
+                ->insert(array_keys($fields), array_values($fields))
+                ->into('properties')
+                ->values([
+                    'name' => 'children_order',
+                    'object_type_id' => $objectTypeId,
+                    'property_type_id' => $propertyTypeId,
+                    'created' => date('Y-m-d H:i:s'),
+                    'modified' => date('Y-m-d H:i:s'),
+                    'enabled' => 1,
+                    'is_nullable' => 1,
+                    'is_static' => 0,
+                ])
+                ->execute();
+        });
     }
 
     /**
@@ -100,5 +101,31 @@ class AddChildrenOrder extends AbstractMigration
             ->where(['name' => 'children_order'])
             ->limit(1)
             ->execute();
+    }
+
+    /**
+     * Execute multiple queries wrapped within a transaction, if needed.
+     *
+     * @param callable $cb Callable.
+     * @return void
+     */
+    protected function transactional(callable $cb): void
+    {
+        $adapter = $this->getAdapter();
+        if (!$adapter instanceof MysqlAdapter) {
+            // Only in MySQL DDL statements cause an implicit commit.
+            // For other drivers, just work with the currently active transaction.
+            $cb();
+
+            return;
+        }
+
+        $adapter->beginTransaction();
+        try {
+            $cb();
+            $adapter->commitTransaction();
+        } catch (Throwable $e) {
+            $adapter->rollbackTransaction();
+        }
     }
 }
