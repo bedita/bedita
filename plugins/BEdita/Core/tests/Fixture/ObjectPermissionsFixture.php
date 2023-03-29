d035fa3b765072764eb031a245ebb52990278d99
dante di domenico <dante@didoda.com>
2023-03-29 15:08:24 +0200

    fix: update object permissions fixture

--- plugins/BEdita/Core/tests/Fixture/ObjectPermissionsFixture.php
+++ plugins/BEdita/Core/tests/Fixture/ObjectPermissionsFixture.php
@@ -18,7 +18,8 @@ class ObjectPermissionsFixture extends TestFixture
             'id' => 1,
             'object_id' => 1,
             'role_id' => 1,
-            'params' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+            'created_by' => 1,
+            'created' => '2023-03-29 15:08',
         ],
     ];
 }
