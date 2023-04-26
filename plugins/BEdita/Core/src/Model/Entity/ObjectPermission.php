fb1e3f87ee8f85463f7159d7aab7fc0c8a4855fb
batopa <batopa@gmail.com>
2023-04-26 07:02:20 +0200

    feat: hide object_permissions associations from json api response

--- plugins/BEdita/Core/src/Model/Entity/ObjectPermission.php
+++ plugins/BEdita/Core/src/Model/Entity/ObjectPermission.php
@@ -22,18 +22,21 @@ class ObjectPermission extends Entity implements JsonApiSerializable
     use JsonApiTrait;
 
     /**
-     * Fields that can be mass assigned using newEntity() or patchEntity().
-     *
-     * Note that when '*' is set to true, this allows all unspecified fields to
-     * be mass assigned. For security purposes, it is advised to set '*' to false
-     * (or remove it), and explicitly make individual fields accessible as needed.
-     *
-     * @var array
+     * @inheritDoc
      */
     protected $_accessible = [
         '*' => true,
         'id' => false,
         'created' => false,
         'created_by' => false,
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    protected $_hidden = [
+        'created_by_user',
+        'object',
+        'role',
     ];
 }
