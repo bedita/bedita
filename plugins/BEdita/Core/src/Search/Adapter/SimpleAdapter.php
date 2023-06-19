6cf2f2d4aea196091928fbf242c65cc929cb0b76
batopa <batopa@gmail.com>
2023-06-19 15:58:05 +0200

    test: SimpleAdapter tests

---
+++ plugins/BEdita/Core/src/Search/Adapter/SimpleAdapter.php
@@ -0,0 +1,150 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2023 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+namespace BEdita\Core\Search\Adapter;
+
+use BEdita\Core\Exception\BadFilterException;
+use BEdita\Core\Search\BaseAdapter;
+use Cake\Database\Expression\FunctionExpression;
+use Cake\Database\Expression\QueryExpression;
+use Cake\Datasource\EntityInterface;
+use Cake\ORM\Query;
+use Cake\Utility\Hash;
+use Cake\Validation\Validator;
+
+/**
+ * Simple search adapter.
+ * Execute search via `LIKE` query.
+ *
+ * @since 5.14.0
+ */
+class SimpleAdapter extends BaseAdapter
+{
+    /**
+     * Get validator.
+     *
+     * @param array $config Search configuration
+     * @return \Cake\Validation\Validator
+     */
+    public function getValidator(array $config = []): Validator
+    {
+        $validator = new Validator();
+
+        return $validator
+            ->isArray('words')
+            ->add('words', 'checkWords', [
+                'rule' => function ($value) use ($config) {
+                    $value = (array)$value;
+                    $minLength = Hash::get($config, 'minLength');
+                    $maxWords = Hash::get($config, 'maxWords');
+                    if (count($value) === 0) {
+                        return __d('bedita', 'query strings must be at least {0} characters long', $minLength);
+                    }
+
+                    if ($maxWords > 0 && count($value) > $maxWords) {
+                        return __d('bedita', 'query string too long');
+                    }
+
+                    return true;
+                },
+            ]);
+    }
+
+    /**
+     * Prepare text before search.
+     *
+     * @param string $text The text to search
+     * @param array $options The search options
+     * @param array $config Search configuration
+     * @return array
+     */
+    protected function prepareText(string $text, array $options = [], array $config = []): array
+    {
+        $minLength = Hash::get($config, 'minLength');
+        $words = [$text];
+        if (filter_var(Hash::get($options, 'exact'), FILTER_VALIDATE_BOOLEAN) !== true) {
+            $words = preg_split('/\W+/', $text); // Split words.
+        }
+        $words = array_unique(array_map( // Escape `%` and `\` characters in words.
+            function ($word) {
+                return str_replace(
+                    ['%', '\\'],
+                    ['\\%', '\\\\'],
+                    mb_strtolower($word)
+                );
+            },
+            array_filter( // Filter out words that are too short.
+                $words,
+                function ($word) use ($minLength) {
+                    return mb_strlen($word) >= $minLength;
+                }
+            )
+        ));
+
+        return $words;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function search(Query $query, string $text, array $options = [], array $config = []): Query
+    {
+        $words = $this->prepareText($text, $options, $config);
+        $errors = $this->getValidator($config)->validate(compact('words'));
+        if (!empty($errors)) {
+            throw new BadFilterException([
+                'title' => __d('bedita', 'Invalid data'),
+                'detail' => Hash::get($errors, 'text.0'),
+            ]);
+        }
+
+        $tableFields = (array)Hash::get($config, 'fields');
+
+        // Concat all fields into a single, lower-cased string.
+        $fields = [];
+        /** @var \Cake\ORM\Table $table */
+        $table = $query->getRepository();
+        foreach (array_keys($tableFields) as $field) {
+            $fields[] = $query->func()->coalesce([
+                $table->aliasField($field) => 'identifier',
+                '',
+            ]);
+            $fields[] = ' '; // Add a spacer.
+        }
+        array_pop($fields); // Remove last spacer.
+        $field = new FunctionExpression('LOWER', [$query->func()->concat($fields)]);
+
+        // Build query conditions.
+        return $query
+            ->where(function (QueryExpression $exp) use ($field, $words) {
+                foreach ($words as $word) {
+                    $exp->like(
+                        $field,
+                        sprintf('%%%s%%', $word)
+                    );
+                }
+
+                return $exp;
+            });
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @codeCoverageIgnore
+     */
+    public function indexResource(EntityInterface $entity, string $operation): void
+    {
+    }
+}
---
+++ plugins/BEdita/Core/tests/TestCase/Search/Adapter/SimpleAdapterTest.php
@@ -0,0 +1,208 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2023 ChannelWeb Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+namespace BEdita\Core\Test\TestCase\Search\Adapter;
+
+use BEdita\Core\Exception\BadFilterException;
+use BEdita\Core\ORM\Inheritance\Table;
+use BEdita\Core\Search\Adapter\SimpleAdapter;
+use Cake\TestSuite\TestCase;
+
+/**
+ * @coversDefaultClass \BEdita\Core\Search\Adapter\SimpleAdapter
+ */
+class SimpleAdapterTest extends TestCase
+{
+    /**
+     * SimpleAdapter instance
+     *
+     * @var \BEdita\Core\Search\Adapter\SimpleAdapter
+     */
+    protected $adapter = null;
+
+    /**
+     * @inheritDoc
+     */
+    protected $fixtures = [
+        'plugin.BEdita/Core.FakeAnimals',
+        'plugin.BEdita/Core.FakeMammals',
+        'plugin.BEdita/Core.FakeFelines',
+        'plugin.BEdita/Core.FakeSearches',
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->adapter = new SimpleAdapter();
+        $this->fetchTable('FakeMammals', ['className' => Table::class])
+            ->setDisplayField('name')
+            ->extensionOf('FakeAnimals');
+        $this->fetchTable('FakeFelines', ['className' => Table::class])
+            ->setDisplayField('name')
+            ->extensionOf('FakeMammals');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->adapter = null;
+    }
+
+    /**
+     * Data provider for `testSearch` test case.
+     *
+     * @return array
+     */
+    public function searchProvider()
+    {
+        return [
+            'basic' => [
+                [
+                    2 => 'koala',
+                ],
+                'ala',
+            ],
+            'two words' => [
+                [],
+                'koala eagle',
+            ],
+            'two words, different fields' => [
+                [
+                    1 => 'cat',
+                ],
+                'eutheria cat',
+                [],
+                'FakeMammals',
+                [
+                    'name' => 1,
+                    'subclass' => 1,
+                ],
+            ],
+            'short words' => [
+                new BadFilterException([
+                    'title' => 'Invalid data',
+                    'detail' => 'query strings must be at least 3 characters long',
+                ]),
+                'I am me',
+            ],
+            'too many words' => [
+                new BadFilterException([
+                    'title' => 'Invalid data',
+                    'detail' => 'query string too long',
+                ]),
+                'this query string contains too many words thus will be rejected to avoid denial of service',
+            ],
+            'search hyphen' => [
+                [
+                    1 => 'hippo-tiger',
+                ],
+                'hippo-tiger',
+                [],
+                'FakeSearches',
+            ],
+            'search underscore' => [
+                [
+                    2 => 'lion_snake',
+                ],
+                'lion_snake',
+                [],
+                'FakeSearches',
+            ],
+            'search underscore 2' => [
+                [
+                   2 => 'lion_snake',
+                ],
+                'li_n',
+                [],
+                'FakeSearches',
+            ],
+            'search case' => [
+                [
+                    1 => 'hippo-tiger',
+                ],
+                'HIPPO',
+                [],
+                'FakeSearches',
+            ],
+            'exact false' => [
+                [
+                    3 => 'big mouse',
+                    4 => 'mouse big',
+                ],
+                'big mouse',
+                [
+                    'exact' => 0,
+                ],
+                'FakeSearches',
+            ],
+            'exact' => [
+                [
+                    3 => 'big mouse',
+                ],
+                'big mouse',
+                [
+                    'exact' => 1,
+                ],
+                'FakeSearches',
+            ],
+        ];
+    }
+
+    /**
+     * Test search.
+     *
+     * @param array|\Exception $expected Expected result
+     * @param string $text Text to search
+     * @param array $options Search options
+     * @param string $tableName Table name
+     * @return void
+     * @dataProvider searchProvider()
+     * @covers ::search()
+     * @covers ::prepareText()
+     * @covers ::getValidator()
+     */
+    public function testSearch($expected, string $text, array $options = [], $tableName = 'FakeAnimals', array $fields = [])
+    {
+        if ($expected instanceof \Exception) {
+            $this->expectException(get_class($expected));
+            $this->expectExceptionCode($expected->getCode());
+            $this->expectExceptionMessage($expected->getMessage());
+        }
+
+        $table = $this->fetchTable($tableName);
+        $query = $table->find();
+
+        $fields += ['name' => 1];
+
+        $config = [
+            'minLength' => 3,
+            'maxWords' => 10,
+        ] + compact('fields');
+
+        $result = $this->adapter
+            ->search($query, $text, $options, $config)
+            ->find('list')
+            ->toArray();
+
+        static::assertEquals($expected, $result);
+    }
+}
