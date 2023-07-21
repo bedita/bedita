bdf3fead3f95c0fdf27e5c54b45f9fd581c38a4b
stefanorosanelli <stefano.rosanelli@gmail.com>
2023-07-21 11:26:02 +0200

    chore: typo [ci skip]

--- plugins/BEdita/Core/src/Model/Entity/Property.php
+++ plugins/BEdita/Core/src/Model/Entity/Property.php
@@ -187,7 +187,7 @@ class Property extends Entity implements JsonApiSerializable
         }
         $spec = array_values(array_diff(array_keys($schema), ['type']));
         // only if `contentMediaType` is present we can assume that
-        // we have a translatable string by defatul (text/plain, text/html...)
+        // we have a translatable string by default (text/plain, text/html...)
         $item = (string)Hash::get($spec, '0');
 
         return $item === 'contentMediaType' && count($spec) === 1;
