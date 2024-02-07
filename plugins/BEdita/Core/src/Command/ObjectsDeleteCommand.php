a584d9f516326751bc22f719fa76fd379bb79c55
dante di domenico <dante@didoda.com>
2024-02-07 11:50:05 +0100

    feat: objects_delete command

---
+++ plugins/BEdita/Core/src/Command/ObjectsDeleteCommand.php
@@ -0,0 +1,104 @@
+<?php
+declare(strict_types=1);
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2024 Atlas Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\Core\Command;
+
+use Cake\Command\Command;
+use Cake\Console\Arguments;
+use Cake\Console\ConsoleIo;
+use Cake\Console\ConsoleOptionParser;
+use Cake\Database\Expression\QueryExpression;
+use Cake\I18n\FrozenDate;
+use Cake\ORM\Locator\LocatorAwareTrait;
+
+/**
+ * ObjectsDelete command.
+ */
+class ObjectsDeleteCommand extends Command
+{
+    use LocatorAwareTrait;
+
+    /**
+     * @inheritDoc
+     */
+    public function buildOptionParser(ConsoleOptionParser $parser): ConsoleOptionParser
+    {
+        return $parser
+            ->addOption('since', [
+                'help' => 'Delete objects in trash since this date',
+                'required' => false,
+            ])
+            ->addOption('type', [
+                'help' => 'Delete objects in trash by type',
+                'required' => false,
+            ]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function execute(Arguments $args, ConsoleIo $io)
+    {
+        $since = $args->getOption('since') ?? '-1 month';
+        $type = $args->getOption('type');
+        $message = 'Deleting from trash objects, since ' . $since;
+        $message .= $type ? ', for type ' . $type : '';
+        $io->info($message);
+        $conditions = ['deleted' => true, 'locked' => false, 'modified <' => new FrozenDate($since)];
+        $deleted = $errors = 0;
+        foreach ($this->objectsIterator($type, $conditions) as $object) {
+            try {
+                $io->verbose(sprintf('Deleting object %s', $object->id));
+                $object->getTable()->deleteOrFail($object);
+            } catch (\Throwable $e) {
+                $io->error(sprintf('Error deleting object %s: %s', $object->id, $e->getMessage()));
+                $errors++;
+                continue;
+            }
+            $deleted++;
+        }
+        $io->success(sprintf('Deleted from trash %d objects [%d errors]', $deleted, $errors));
+        $io->info('Done');
+
+        return self::CODE_SUCCESS;
+    }
+
+    /**
+     * Get objects as iterable.
+     *
+     * @param string|null $type The object type
+     * @param array $conditions The conditions
+     * @return iterable
+     */
+    private function objectsIterator(?string $type, array $conditions): iterable
+    {
+        $table = $this->fetchTable('Objects');
+        $query = empty($type) ? $table->find() : $table->find('type', [$type]);
+        $query = $query->where($conditions)->limit(200);
+        $lastId = 0;
+        while (true) {
+            $q = clone $query;
+            $q = $q->where(fn (QueryExpression $exp): QueryExpression => $exp->gt($table->aliasField('id'), $lastId));
+            $results = $q->all();
+            if ($results->isEmpty()) {
+                break;
+            }
+            foreach ($results as $entity) {
+                $lastId = $entity->id;
+
+                yield $entity;
+            }
+        }
+    }
+}
---
+++ plugins/BEdita/Core/tests/TestCase/Command/ObjectsDeleteCommandTest.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2024 Atlas Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\Core\Test\TestCase\Command;
+
+use Cake\TestSuite\ConsoleIntegrationTestTrait;
+use Cake\TestSuite\TestCase;
+
+/**
+ * {@see BEdita\Core\Command\ObjectsDeleteCommand} Test Case
+ *
+ * @coversDefaultClass \BEdita\Core\Command\ObjectsDeleteCommand
+ */
+class ObjectsDeleteCommandTest extends TestCase
+{
+    use ConsoleIntegrationTestTrait;
+
+    /**
+     * @inheritDoc
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->useCommandRunner();
+    }
+
+    /**
+     * Test buildOptionParser method
+     *
+     * @return void
+     * @covers ::buildOptionParser()
+     */
+    public function testBuildOptionParser()
+    {
+        $this->exec('objects_delete --help');
+        $this->assertOutputContains('Delete objects in trash since this date');
+        $this->assertOutputContains('Delete objects in trash by type');
+    }
+
+    /**
+     * Test `execute` method
+     *
+     * @return void
+     * @covers ::execute()
+     */
+    public function testExecute(): void
+    {
+        $this->exec('objects_delete --type documents');
+        $this->assertOutputContains('Deleting from trash objects, since -1 month, for type documents');
+        $this->assertOutputContains('Deleted from trash 0 objects [0 errors]');
+        $this->assertOutputContains('Done');
+        $this->assertExitSuccess();
+    }
+}
