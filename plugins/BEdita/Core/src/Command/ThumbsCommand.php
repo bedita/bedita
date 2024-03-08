d472fcada6d559f1afadbabfdda7ebd5cdac84c1
dante di domenico <dante@didoda.com>
2024-03-08 07:50:15 +0100

    feat: thumbs command + event handler

---
+++ plugins/BEdita/Core/src/Command/ThumbsCommand.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2024 Atlas Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\Core\Command;
+
+use BEdita\Core\Event\ImageThumbsHandler;
+use BEdita\Core\Model\Entity\Stream;
+use Cake\Collection\Collection;
+use Cake\Command\Command;
+use Cake\Console\Arguments;
+use Cake\Console\ConsoleIo;
+use Cake\Console\ConsoleOptionParser;
+use Cake\Core\Configure;
+use Cake\Utility\Hash;
+
+/**
+ * Command to update/create thumbnails for all images.
+ */
+class ThumbsCommand extends Command
+{
+    /**
+     * @inheritDoc
+     */
+    public function buildOptionParser(ConsoleOptionParser $parser): ConsoleOptionParser
+    {
+        $parser = parent::buildOptionParser($parser);
+        $parser->addOption('id', ['help' => 'Image id']);
+
+        return $parser;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function execute(Arguments $args, ConsoleIo $io)
+    {
+        $io->out('=====> <info>Update image thumbs...</info>');
+
+        $Images = $this->fetchTable('Images');
+        $query = $Images->find()
+            ->contain('Streams');
+
+        if ($args->getOption('id')) {
+            $query = $query->where(['id' => $args->getOption('id')]);
+        }
+
+        $handler = new ImageThumbsHandler();
+        $presets = $this->presets();
+        Configure::delete('AppCacheInvalidation'); // disable app cache invalidation
+
+        $entities = $query->toArray();
+        $count = 0;
+        foreach ($entities as $image) {
+            $stream = Hash::get($image, 'streams.0');
+
+            if (!$stream instanceof Stream) {
+                continue;
+            }
+            $count++;
+            $handler->updateThumbs($image, $stream, $presets);
+        }
+        $io->out('=====> <success>Thumbs updated</success>');
+        $io->out(sprintf('=====> <info>Updated %d images</info>', $count));
+    }
+
+    /**
+     * Retrieve thumbnail presets without 'async' generators
+     *
+     * @return array
+     */
+    protected function presets(): array
+    {
+        $collection = new Collection((array)Configure::read('Thumbnails.presets'));
+
+        return $collection->map(function (array $preset) {
+            unset($preset['generator']); // remove 'async' generators
+
+            return $preset;
+        })->toArray();
+    }
+}
---
+++ plugins/BEdita/Core/src/Event/ImageThumbsHandler.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2024 Atlas Srl, Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\Core\Event;
+
+use BEdita\Core\Filesystem\Thumbnail;
+use BEdita\Core\Model\Entity\ObjectEntity;
+use BEdita\Core\Model\Entity\Stream;
+use Cake\Core\Configure;
+use Cake\Event\Event;
+use Cake\Event\EventListenerInterface;
+use Cake\Log\LogTrait;
+use Cake\ORM\Locator\LocatorAwareTrait;
+use Cake\Utility\Hash;
+
+/**
+ * Event listener for image thumbs events.
+ */
+class ImageThumbsHandler implements EventListenerInterface
+{
+    use LogTrait;
+    use LocatorAwareTrait;
+
+    /**
+     * @inheritDoc
+     */
+    public function implementedEvents(): array
+    {
+        return [
+            'Associated.afterSave' => 'afterSaveAssociated',
+        ];
+    }
+
+    /**
+     * Handle 'Associated.afterSave' and create thumbs using presets on streams
+     *
+     * @param \Cake\Event\Event $event Dispatched event.
+     * @return void
+     */
+    public function afterSaveAssociated(Event $event): void
+    {
+        $data = $event->getData();
+        $stream = Hash::get($data, 'entity');
+        if (!$stream instanceof Stream) {
+            return;
+        }
+        $image = Hash::get($data, 'relatedEntities.0');
+        $type = (string)$image->get('type');
+        if ($type !== 'images') {
+            return;
+        }
+        $presets = (array)Configure::read('Thumbnails.presets');
+        $this->updateThumbs($image, $stream, $presets);
+    }
+
+    /**
+     * Update all preset thumbnails of a stream
+     *
+     * @param \BEdita\Core\Model\Entity\ObjectEntity $image The image object.
+     * @param \BEdita\Core\Model\Entity\Stream $stream The stream resource.
+     * @param array $presets Preset configurations.
+     * @return void
+     */
+    public function updateThumbs(ObjectEntity $image, Stream $stream, array $presets): void
+    {
+        $result = [];
+        foreach ($presets as $key => $value) {
+            $info = Thumbnail::get($stream, $value);
+            unset($info['ready']);
+            $result[$key] = $info;
+        }
+        $extra = (array)$image->get('extra');
+        $extra['thumbs'] = $result;
+        $image->set('extra', $extra);
+        $this->fetchTable('Images')->saveOrFail($image);
+    }
+}
