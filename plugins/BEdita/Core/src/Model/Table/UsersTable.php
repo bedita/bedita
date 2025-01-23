f1219dab2a25a5bd8a33bf2389a99d400a40eff4
Paolo Santarsiero <57118410+paolosantarsiero@users.noreply.github.com>
2025-01-23 09:43:04 +0100

    Update plugins/BEdita/Core/src/Model/Table/UsersTable.php
    refactor: improve query
    
    Co-authored-by: Paolo Cuffiani <paolo.cuffiani@chialab.it>

--- plugins/BEdita/Core/src/Model/Table/UsersTable.php
+++ plugins/BEdita/Core/src/Model/Table/UsersTable.php
@@ -292,16 +292,10 @@ class UsersTable extends Table
         return $query->innerJoinWith('ExternalAuth', function (Query $query) use ($options) {
             $query = $query->find('authProvider', $options);
             if (!empty($options['username'])) {
-                if (is_array($options['username'])) {
-                    $query = $query->where(fn(QueryExpression $exp) => $exp->in(
-                        $this->ExternalAuth->aliasField('provider_username'),
-                        $options['username']
-                    ));
-                } else {
-                    $query = $query->where([
-                        $this->ExternalAuth->aliasField('provider_username') => $options['username'],
-                    ]);
-                }
+                $query = $query->where(fn (QueryExpression $exp) => $exp->in(
+                    $this->ExternalAuth->aliasField('provider_username'),
+                    (array)$options['username'],
+                ));
             }
 
             return $query;
