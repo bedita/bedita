7ca803bc1044e03671f64d4f5ef92830e8c50dfa
Vavaste <45969047+Vavaste@users.noreply.github.com>
2025-06-30 09:51:53 +0200

    Add support for 'nin' operator in fieldsFilter (#2142)
    * feat: add nin operator in query builder
    
    * feat: add in operator + refactor
    
    * chore: add test case
    
    ---------
    
    Co-authored-by: Stefano Vavassori <stefano.vavassori@codestorm.it>

--- plugins/BEdita/Core/src/ORM/QueryFilterTrait.php
+++ plugins/BEdita/Core/src/ORM/QueryFilterTrait.php
@@ -90,8 +90,10 @@ trait QueryFilterTrait
                         $in[] = $value;
                         continue;
                     }
+
                     $exp = $this->operatorExpression($exp, $operator, $field, $value);
                 }
+
                 if (!empty($in)) {
                     $exp = $exp->in($field, $in);
                 }
@@ -148,6 +150,16 @@ trait QueryFilterTrait
             case 'ge':
             case '>=':
                 $exp = $exp->gte($field, $value);
+                break;
+            case 'notin':
+            case 'nin':
+                $values = is_string($value) ? explode(',', $value) : (array)$value;
+                $exp = $exp->notIn($field, $values);
+                break;
+
+            case 'in':
+                $values = is_string($value) ? explode(',', $value) : (array)$value;
+                $exp = $exp->in($field, $values);
                 break;
 
             case 'null':
