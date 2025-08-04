7169202e0825507d76d097db7d95f9d37c2e1a79
Dante Di Domenico <dante@didoda.com>
2025-08-04 16:26:24 +0200

    Validate entity before saving in `Resources::create` (#2148)
    * fix: validate entity before saving
    
    * fix: remove unuseful line
    
    * chore: refactor
    
    * fix: set after validation
    
    * fix: tests
    
    * tests: add test for invalid category

--- plugins/BEdita/Core/tests/TestCase/Utility/ResourcesTest.php
+++ plugins/BEdita/Core/tests/TestCase/Utility/ResourcesTest.php
@@ -21,6 +21,7 @@ use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
 use Cake\Utility\Hash;
 use Cake\Utility\Inflector;
+use InvalidArgumentException;
 
 /**
  * {@see \BEdita\Core\Utility\Resources} Test Case
@@ -68,7 +69,7 @@ class ResourcesTest extends TestCase
                 'roles',
                 [
                     [
-                        'name' => 'new role',
+                        'name' => 'new_role',
                     ],
                 ],
             ],
@@ -76,7 +77,7 @@ class ResourcesTest extends TestCase
                 'applications',
                 [
                     [
-                        'name' => 'new app',
+                        'name' => 'new_app',
                     ],
                 ],
             ],
@@ -224,6 +225,38 @@ class ResourcesTest extends TestCase
     {
         $result = Resources::create($type, $data);
         static::assertEquals(count($data), count($result));
+    }
+
+    /**
+     * Test `create` method with invalid name.
+     *
+     * @covers ::create()
+     */
+    public function testCreateInvalidCategory(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid resource data: {"name":{"regex":"The provided value is invalid"}}');
+        Resources::create('categories', [
+            [
+                'name' => 'società',
+            ],
+        ]);
+    }
+
+    /**
+     * Test `create` method with invalid name.
+     *
+     * @covers ::create()
+     */
+    public function testCreateInvalidRole(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Invalid resource data: {"name":{"regex":"The provided value is invalid"}}');
+        Resources::create('roles', [
+            [
+                'name' => 'invalid name',
+            ],
+        ]);
     }
 
     /**
