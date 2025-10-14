6b74195fda93584881004e966d51e3ea70e987ec
dante di domenico <dante@didoda.com>
2025-10-14 11:56:45 +0200

    feat: bulk edit

---
+++ plugins/BEdita/API/src/Controller/BulkController.php
@@ -0,0 +1,99 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2025 Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\API\Controller;
+
+use Cake\Http\Exception\MethodNotAllowedException;
+use Cake\Http\Response;
+use Cake\Utility\Inflector;
+use Throwable;
+
+/**
+ * Controller for `/bulk` endpoint.
+ *
+ * @since 5.43.0
+ */
+class BulkController extends JsonBaseController
+{
+    /**
+     * Allowed operations
+     *
+     * @var array<string>
+     */
+    protected array $allowedOperations = ['edit'];
+
+    /**
+     * {@inheritDoc}
+     *
+     * @codeCoverageIgnore
+     */
+    protected function checkAcceptable(): void
+    {
+    }
+
+    /**
+     * Perform bulk operations
+     *
+     * @param string|null $operation Operation to perform
+     * @return \Cake\Http\Response
+     */
+    public function index(?string $operation): Response
+    {
+        $this->request->allowMethod(['patch', 'post']);
+        if (!in_array($operation, $this->allowedOperations, true)) {
+            throw new MethodNotAllowedException(sprintf('Operation %s not allowed', $operation));
+        }
+
+        return $this->{$operation}();
+    }
+
+    /**
+     * Edit multiple resources
+     *
+     * @return \Cake\Http\Response
+     */
+    protected function edit(): Response
+    {
+        $ids = (array)$this->request->getData('ids');
+        $payload = (array)$this->request->getData('data');
+        $objectsTable = $this->fetchTable('Objects');
+        $saved = [];
+        $errors = [];
+        foreach ($ids as $id) {
+            try {
+                $connection = $objectsTable->getConnection();
+                $connection->transactional(function () use ($id, $payload, $objectsTable, &$saved) {
+                    $entity = $objectsTable->get($id);
+                    $type = $entity->get('type');
+                    $typesTable = $this->fetchTable(Inflector::camelize($type));
+                    $entity = $typesTable->get($id);
+                    $entity = $typesTable->patchEntity($entity, $payload);
+                    $typesTable->saveOrFail($entity);
+                    $saved[] = (int)$id;
+                });
+            } catch (Throwable $e) {
+                $errors[] = [
+                    'id' => (int)$id,
+                    'message' => $e->getMessage(),
+                ];
+            }
+        }
+        $data = compact('errors', 'saved');
+        $this->set($data);
+        $this->setSerialize(['data']);
+
+        return $this->response->withStringBody(json_encode($data));
+    }
+}
---
+++ plugins/BEdita/API/tests/TestCase/Controller/BulkControllerTest.php
@@ -0,0 +1,117 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * BEdita, API-first content management framework
+ * Copyright 2025 Chialab Srl
+ *
+ * This file is part of BEdita: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * See LICENSE.LGPL or <http://gnu.org/licenses/lgpl-3.0.html> for more details.
+ */
+
+namespace BEdita\API\Test\TestCase\Controller;
+
+use BEdita\API\TestSuite\IntegrationTestCase;
+use Cake\Utility\Hash;
+
+/**
+ * @coversDefaultClass \BEdita\API\Controller\BulkController
+ */
+class BulkControllerTest extends IntegrationTestCase
+{
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    protected $fixtures = [
+        'plugin.BEdita/Core.Objects',
+    ];
+
+    /**
+     * Test index method on GET /bulk.
+     *
+     * @return void
+     * @covers ::index()
+     */
+    public function testIndex404(): void
+    {
+        $this->configRequestHeaders('GET', $this->getUserAuthHeader());
+        $this->get('/bulk');
+        $this->assertResponseCode(404);
+    }
+
+    /**
+     * Test index method on GET /bulk/edit.
+     *
+     * @return void
+     * @covers ::index()
+     */
+    public function testIndex405(): void
+    {
+        $this->configRequestHeaders('GET', $this->getUserAuthHeader());
+        $this->get('/bulk/edit');
+        $this->assertResponseCode(405);
+    }
+
+    /**
+     * Test index method on POST /bulk/edit.
+     *
+     * @return void
+     * @covers ::index()
+     * @covers ::edit()
+     */
+    public function testEdit(): void
+    {
+        // object 2 is locked, status cannot be changed
+        $o1 = $this->fetchTable('Objects')->get(2);
+        $firstOriginalStatus = $o1->get('status');
+        $this->assertEquals('on', $firstOriginalStatus);
+        $o2 = $this->fetchTable('Objects')->get(3);
+        $secondOriginalStatus = $o2->get('status');
+        $this->assertEquals('draft', $secondOriginalStatus);
+        $authHeader = $this->getUserAuthHeader();
+        $authHeader['Content-Type'] = 'application/json';
+        $this->configRequestHeaders('POST', $authHeader);
+        $this->post('/bulk/edit', json_encode([
+            'ids' => [2, 3],
+            'data' => [
+                'status' => 'off',
+            ],
+        ]));
+        $this->assertResponseCode(200);
+        // check response content
+        $response = (array)json_decode((string)$this->_response->getBody(), true);
+        $this->assertArrayHasKey('saved', $response);
+        $this->assertArrayHasKey('errors', $response);
+        $this->assertEquals([3], Hash::get($response, 'saved'));
+        $this->assertCount(1, Hash::get($response, 'errors'));
+        $this->assertEquals(2, Hash::get($response, 'errors.0.id'));
+        $this->assertEquals('Operation not allowed on "locked" objects', Hash::get($response, 'errors.0.message'));
+        // check objects status
+        $o1 = $this->fetchTable('Objects')->get(2);
+        $firstStatus = $o1->get('status');
+        $this->assertEquals($firstOriginalStatus, $firstStatus);
+        $o2 = $this->fetchTable('Objects')->get(3);
+        $secondStatus = $o2->get('status');
+        $this->assertEquals('off', $secondStatus);
+        // restore objects status
+        $authHeader = $this->getUserAuthHeader();
+        $authHeader['Content-Type'] = 'application/json';
+        $this->configRequestHeaders('POST', $authHeader);
+        $this->post('/bulk/edit', json_encode([
+            'ids' => [3],
+            'data' => [
+                'status' => 'draft',
+            ],
+        ]));
+        $this->assertResponseCode(200);
+        $o2 = $this->fetchTable('Objects')->get(3);
+        $secondStatus = $o2->get('status');
+        $this->assertEquals('draft', $secondStatus);
+    }
+}
