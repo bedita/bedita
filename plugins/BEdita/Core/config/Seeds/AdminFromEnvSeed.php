f74af29b46dc9a21aa73a47635967720a4935f2f
Alberto Pagliarini <batopa@gmail.com>
2025-10-27 14:30:36 +0100

    refactor: remove deprecations in seeds

--- plugins/BEdita/Core/config/Seeds/AdminFromEnvSeed.php
+++ plugins/BEdita/Core/config/Seeds/AdminFromEnvSeed.php
@@ -1,29 +1,27 @@
 <?php
-use Cake\Auth\WeakPasswordHasher;
-use Migrations\AbstractSeed;
+use Authentication\PasswordHasher\LegacyPasswordHasher;
+use Migrations\BaseSeed;
 
 /**
  * Updates main admin user from env vars:
  *  - 'username' => BEDITA_ADMIN_USR
  *  - 'password' => BEDITA_ADMIN_PWD
  */
-class AdminFromEnvSeed extends AbstractSeed
+class AdminFromEnvSeed extends BaseSeed
 {
     /**
      * {@inheritDoc}
      */
-    public function run()
+    public function run(): void
     {
         $username = getenv('BEDITA_ADMIN_USR');
         $password = getenv('BEDITA_ADMIN_PWD');
         if (empty($username) || empty($password)) {
-            echo "Mandatory environment variables missing: BEDITA_ADMIN_USR, BEDITA_ADMIN_PWD\n";
-            echo 'No data seeded!';
-
-            return -1;
+            $this->io->error('Mandatory environment variables missing: BEDITA_ADMIN_USR, BEDITA_ADMIN_PWD');
+            $this->io->abort('No data seeded!');
         }
 
-        $hash = (new WeakPasswordHasher(['hashType' => 'md5']))->hash($password);
+        $hash = (new LegacyPasswordHasher(['hashType' => 'md5']))->hash('password1');
         $query = sprintf("UPDATE users set username='%s', password_hash='%s' WHERE id=1", $username, $hash);
         $this->query($query);
     }
