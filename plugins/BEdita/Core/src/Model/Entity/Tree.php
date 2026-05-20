41944b3b685bc1f15ad01e63c83194b103c5ebaa
le0m <mainardi.leo@gmail.com>
2026-05-20 15:17:28 +0200

    refactor: use PriorityBehavior in TreesTable, to handle ordering entities

--- plugins/BEdita/Core/src/Model/Entity/Tree.php
+++ plugins/BEdita/Core/src/Model/Entity/Tree.php
@@ -26,8 +26,7 @@ use Cake\ORM\TableRegistry;
  * @property int|null $parent_id
  * @property int $root_id
  * @property int|null $parent_node_id
- * @property int $tree_left
- * @property int $tree_right
+ * @property int $priority
  * @property int $depth_level
  * @property bool $menu
  * @property bool $canonical
