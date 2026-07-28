bedita/core

refactor: replace uses of tree_left/tree_right with adjacency list's methods

config/Migrations/20260525160515_DropTreesDepthLevel.php
@@ -1,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class DropTreesDepthLevel extends AbstractMigration
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function up(): void
+    {
+        $this->table('trees')
+            ->removeColumn('depth_level')
+            ->update();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function down(): void
+    {
+        // TODO: restore `depth_level`
+    }
+}

src/Model/Entity/Folder.php
@@ -15,9 +15,11 @@ declare(strict_types=1);
 
 namespace BEdita\Core\Model\Entity;
 
+use BEdita\Core\Model\Behavior\AdjacencyListBehavior;
 use BEdita\Core\Model\Table\RolesTable;
 use BEdita\Core\Utility\LoggedUser;
 use Cake\Datasource\Exception\RecordNotFoundException;
+use Cake\ORM\Query;
 use Cake\ORM\Table;
 use Cake\ORM\TableRegistry;
 use Cake\Utility\Hash;
@@ -76,38 +78,29 @@ class Folder extends ObjectEntity
     protected function getInheritedRolesPermissions(): array
     {
         $Trees = TableRegistry::getTableLocator()->get('Trees');
-        /** @var \BEdita\Core\Model\Entity\Tree $node */
+        /** @var \BEdita\Core\Model\Entity\Tree|null $node */
         $node = $Trees->find()->where(['object_id' => $this->id])->first();
-        $permission = [];
-
-        if ($node !== null) {
-            $permission = $this->getTable()->Permissions
-                ->find()
-                ->disableHydration()
-                ->select(['tree_left' => 'Trees.tree_left', 'name' => 'Roles.name'])
-                ->contain('Roles')
-                ->innerJoin(
-                    ['Trees' => 'trees'],
-                    [
-                        'Trees.object_id = Permissions.object_id',
-                        'Trees.tree_left <' => $node->tree_left,
-                        'Trees.tree_right >' => $node->tree_right,
-                    ],
-                    [
-                        'Trees.object_id' => 'integer',
-                        'Trees.tree_left' => 'integer',
-                        'Trees.tree_right' => 'integer',
-                    ]
-                )
-                ->order(['Trees.tree_left' => 'DESC'])
-                ->toArray();
+        if ($node === null) {
+            return [];
         }
+
+        $query = $Trees->find('ancestors', ['for' => $node->id]);
+        $level = $Trees->getAssociation('Descendants')->junction()->aliasField(AdjacencyListBehavior::CTE_FIELD_LEVEL);
+        $permission = $query
+            ->disableHydration()
+            ->innerJoinWith('Objects.Permissions.Roles')
+            ->select([
+                'level' => $level,
+                'name' => 'Roles.name',
+            ])
+            ->orderAsc($level)
+            ->toArray();
 
         if (empty($permission)) {
             return [];
         }
 
-        $roles = current(Hash::combine($permission, '{n}.name', '{n}.name', '{n}.tree_left'));
+        $roles = current(Hash::combine($permission, '{n}.name', '{n}.name', '{n}.level'));
 
         return ['roles' => array_values($roles), 'inherited' => true];
     }
@@ -129,25 +122,19 @@ class Folder extends ObjectEntity
         }
 
         $Trees = TableRegistry::getTableLocator()->get('Trees');
-        $descendantPermitted = $Trees->selectQuery()
+        /** @var \BEdita\Core\Model\Entity\Tree|null $node */
+        $node = $Trees->find()->where(['object_id' => $this->id])->first();
+        if ($node === null) {
+            return false;
+        }
+
+        $descendantPermitted = $Trees->find('descendants', ['for' => $node->id])
             ->disableHydration()
+            ->innerJoinWith(
+                'Objects.Permissions',
+                fn (Query $q): Query => $q->where(['Permissions.role_id IN' => $roleIds]),
+            )
             ->select(['existing' => 1])
-            ->from(['t1' => 'trees'], true)
-            ->innerJoin(
-                ['t2' => 'trees'],
-                [
-                    't2.tree_left > t1.tree_left',
-                    't2.tree_right < t1.tree_right',
-                ],
-            )
-            ->innerJoin(
-                ['op' => 'object_permissions'],
-                [
-                    'op.object_id = t2.object_id',
-                    'op.role_id IN' => Hash::extract($user, 'roles.{n}.id'),
-                ],
-            )
-            ->where(['t1.object_id' => $this->id], ['t1.object_id' => 'integer'])
             ->limit(1)
             ->first();
 
@@ -281,6 +268,7 @@ class Folder extends ObjectEntity
                     'keyField' => 'id',
                     'valueField' => 'object_id',
                 ])
+                ->all()
                 ->toArray();
         } catch (RecordNotFoundException $previous) {
             throw new \RuntimeException(__d('bedita', 'Folder "{0}" is not on the tree.', $this->id), 0, $previous);
@@ -302,9 +290,15 @@ class Folder extends ObjectEntity
         }
 
         try {
-            return TableRegistry::getTableLocator()->get('Trees')
-                ->find('pathNodes', [$this->id])
-                ->select(['id' => 'object_id', 'menu', 'params', 'slug'], true)
+            $Trees = TableRegistry::getTableLocator()->get('Trees');
+
+            return $Trees->find('pathNodes', [$this->id])
+                ->select([
+                    'id' => $Trees->aliasField('object_id'),
+                    $Trees->aliasField('menu'),
+                    $Trees->aliasField('params'),
+                    $Trees->aliasField('slug'),
+                ], true)
                 ->disableHydration()
                 ->toArray();
         } catch (RecordNotFoundException $previous) {

src/Model/Table/FoldersTable.php
@@ -58,7 +58,7 @@ class FoldersTable extends ObjectsTable
             'targetForeignKey' => 'object_id',
             'finder' => 'available',
             'sort' => [
-                'Trees.tree_left' => 'asc',
+                'Trees.priority' => 'asc',
             ],
             'cascadeCallbacks' => true,
         ]);
@@ -144,12 +144,12 @@ class FoldersTable extends ObjectsTable
             ->firstOrFail();
 
         $deletedParents = $this->find()
-            ->innerJoinWith('TreeNodes', function (Query $query) use ($node) {
-                return $query->where(function (QueryExpression $exp) use ($node) {
-                    return $exp
-                        ->lt($this->TreeNodes->aliasField('tree_left'), $node->get('tree_left'))
-                        ->gt($this->TreeNodes->aliasField('tree_right'), $node->get('tree_right'));
-                });
+            ->where(function (QueryExpression $exp) use ($node): QueryExpression {
+                return $exp->in(
+                    $this->aliasField('id'),
+                    $this->TreeNodes->find('ancestors', ['for' => $node->id])
+                        ->select(['object_id']),
+                );
             })
             ->where([$this->aliasField('deleted') => true])
             ->count();
@@ -271,7 +271,7 @@ class FoldersTable extends ObjectsTable
                     return $exp->isNull($this->TreeNodes->aliasField('parent_id'));
                 });
             })
-            ->order('TreeNodes.tree_left');
+            ->order('TreeNodes.priority');
     }
 
     /**
@@ -296,13 +296,8 @@ class FoldersTable extends ObjectsTable
             ->firstOrFail();
 
         $descendantsToUpdate = $this->TreeNodes
-            ->find()
-            ->select(['object_id'])
-            ->where(function (QueryExpression $exp) use ($parentNode) {
-                return $exp
-                    ->gt($this->TreeNodes->aliasField('tree_left'), $parentNode->get('tree_left'))
-                    ->lt($this->TreeNodes->aliasField('tree_right'), $parentNode->get('tree_right'));
-            });
+            ->find('descendants', ['for' => $parentNode->id])
+            ->select(['object_id']);
 
         // Update deleted field of descendants
         $this->updateAll(
@@ -358,7 +353,7 @@ class FoldersTable extends ObjectsTable
             default:
                 Log::warning(sprintf('Malformed property `children_order` "%s" for children sorting of folder #%d, defaulting to tree position', $order, $folder->id));
 
-                return [$this->Children->junction()->aliasField('tree_left') => 'ASC'];
+                return [$this->Children->junction()->aliasField('priority') => 'ASC'];
         }
     }
 }

src/Model/Table/ObjectsTable.php
@@ -396,13 +396,8 @@ class ObjectsTable extends Table
         return $query->where(function (QueryExpression $exp) use ($parentNode): QueryExpression {
             return $exp->in(
                 $this->aliasField('id'),
-                $this->TreeNodes->find()
-                    ->select(['object_id'])
-                    ->where(function (QueryExpression $exp) use ($parentNode) {
-                        return $exp
-                            ->gt($this->TreeNodes->aliasField('tree_left'), $parentNode->get('tree_left'))
-                            ->lt($this->TreeNodes->aliasField('tree_right'), $parentNode->get('tree_right'));
-                    })
+                $this->TreeNodes->find('descendants', ['for' => $parentNode->id])
+                    ->select(['object_id']),
             );
         });
     }
@@ -419,12 +414,10 @@ class ObjectsTable extends Table
         $parentId = $this->getId((string)Hash::get($options, '0'));
 
         return $query
-            ->innerJoinWith('TreeNodes', function (Query $query) use ($parentId) {
-                return $query->where([
-                    $this->TreeNodes->aliasField('parent_id') => $parentId,
-                ]);
-            })
-            ->order($this->TreeNodes->aliasField('tree_left'));
+            ->innerJoinWith('TreeNodes', fn (Query $query): Query => $query->where([
+                $this->TreeNodes->aliasField('parent_id') => $parentId,
+            ]))
+            ->order($this->TreeNodes->aliasField('priority'));
     }
 
     /**

src/Model/Table/TreesTable.php
@@ -15,10 +15,10 @@ declare(strict_types=1);
 namespace BEdita\Core\Model\Table;
 
 use BEdita\Core\Exception\LockedResourceException;
+use BEdita\Core\Model\Behavior\AdjacencyListBehavior;
 use BEdita\Core\Model\Entity\Tree;
 use BEdita\Core\Model\Validation\Validation;
 use Cake\Core\Configure;
-use Cake\Database\Driver\Mysql;
 use Cake\Database\Expression\QueryExpression;
 use Cake\Database\Schema\TableSchemaInterface;
 use Cake\Datasource\EntityInterface;
@@ -31,6 +31,7 @@ use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Utility\Text;
 use Cake\Validation\Validator;
+use RuntimeException;
 
 /**
  * Trees Model
@@ -91,7 +92,7 @@ class TreesTable extends Table
         ]);
 
         $this->addBehavior('BEdita/Core.AdjacencyList', [
-            'parentAssociation' => 'ParentObjects',
+            'parentAssociation' => 'ParentNode',
         ]);
         $this->addBehavior('BEdita/Core.Priority', ['fields' => ['priority' => ['scope' => ['parent_id']]]]);
     }
@@ -258,13 +259,19 @@ class TreesTable extends Table
             $position = $entity->get('position');
             $parentId = $entity->get('parent_id');
 
-            $siblings = $this->find()->where(fn (QueryExpression $exp): QueryExpression => $exp->eq($this->aliasField('parent_id'), $parentId));
+            $siblings = $this->find()->where(
+                fn (QueryExpression $exp): QueryExpression => $parentId === null
+                    ? $exp->isNull($this->aliasField('parent_id'))
+                    : $exp->eq($this->aliasField('parent_id'), $parentId),
+            );
             if (!$entity->isNew()) {
-                $siblings = $siblings->where(fn (QueryExpression $exp): QueryExpression => $exp->notEq($this->aliasField('id'), $entity->id));
+                $siblings = $siblings->where(
+                    fn (QueryExpression $exp): QueryExpression => $exp->notEq($this->aliasField('id'), $entity->id),
+                );
             }
 
             $count = $siblings->count();
-            $max = $entity->isNew() ? $count + 1 : max(1, $count);
+            $max = $count + 1;
 
             if ($position === 'first') {
                 $priority = 1;
@@ -292,6 +299,10 @@ class TreesTable extends Table
      */
     public function beforeSave(EventInterface $event, Tree $entity)
     {
+        if ($entity->get('parent_id') === $entity->get('object_id')) {
+            throw new RuntimeException('Cannot set a folder as its own parent');
+        }
+
         if ($entity->isNew() || !$entity->isDirty('parent_id')) {
             return;
         }
@@ -410,30 +421,16 @@ class TreesTable extends Table
             throw new BadRequestException(__d('bedita', 'Missing required parameter "{0}"', 'object id'));
         }
 
-        $lft = $this->aliasField('tree_left');
-        $rgt = $this->aliasField('tree_right');
         $node = $this->find()
-            ->select([$lft, $rgt])
-            ->where(['object_id' => $options[0]])
+            ->select([$this->aliasField('id')])
+            ->where([$this->aliasField('object_id') => $options[0]])
             ->disableHydration()
             ->firstOrFail();
 
-        $index = 'trees_nsm_idx';
-        if ($query->getConnection()->getDriver() instanceof Mysql && in_array($index, $this->getSchema()->indexes())) {
-            $query = $query->modifier([sprintf(
-                '/*+ INDEX(%s %s) */',
-                $this->getAlias(),
-                $index,
-            )]);
-        }
+        $query = $query->find('ancestors', ['for' => $node['id'], 'includeSelf' => true]);
+        $level = $this->getAssociation('Descendants')->junction()->aliasField(AdjacencyListBehavior::CTE_FIELD_LEVEL);
 
-        return $query
-            ->where(
-                fn (QueryExpression $exp): QueryExpression => $exp
-                    ->lte($lft, $node['tree_left'])
-                    ->gte($rgt, $node['tree_right'])
-            )
-            ->orderAsc($lft);
+        return $query->orderDesc($level);
     }
 
     /**

bedita/bedita

(cherry picked from commit b8c846be90810a0cfa8fffcb9b9c88d921805707)

plugins/BEdita/Core/config/Migrations/20260525160515_DropTreesDepthLevel.php
@@ -1,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\BaseMigration;
+
+class DropTreesDepthLevel extends BaseMigration
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function up(): void
+    {
+        $this->table('trees')
+            ->removeColumn('depth_level')
+            ->update();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function down(): void
+    {
+        // TODO: restore `depth_level`
+    }
+}

plugins/BEdita/Core/src/Model/Entity/Folder.php
@@ -14,9 +14,11 @@ declare(strict_types=1);
  */
 namespace BEdita\Core\Model\Entity;
 
+use BEdita\Core\Model\Behavior\AdjacencyListBehavior;
 use BEdita\Core\Model\Table\RolesTable;
 use BEdita\Core\Utility\LoggedUser;
 use Cake\Datasource\Exception\RecordNotFoundException;
+use Cake\ORM\Query;
 use Cake\ORM\Table;
 use Cake\ORM\TableRegistry;
 use Cake\Utility\Hash;
@@ -76,38 +78,29 @@ class Folder extends ObjectEntity
     protected function getInheritedRolesPermissions(): array
     {
         $Trees = TableRegistry::getTableLocator()->get('Trees');
-        /** @var \BEdita\Core\Model\Entity\Tree $node */
+        /** @var \BEdita\Core\Model\Entity\Tree|null $node */
         $node = $Trees->find()->where(['object_id' => $this->id])->first();
-        $permission = [];
-
-        if ($node !== null) {
-            $permission = $this->getTable()->Permissions
-                ->find()
-                ->disableHydration()
-                ->select(['tree_left' => 'Trees.tree_left', 'name' => 'Roles.name'])
-                ->contain('Roles')
-                ->innerJoin(
-                    ['Trees' => 'trees'],
-                    [
-                        'Trees.object_id = Permissions.object_id',
-                        'Trees.tree_left <' => $node->tree_left,
-                        'Trees.tree_right >' => $node->tree_right,
-                    ],
-                    [
-                        'Trees.object_id' => 'integer',
-                        'Trees.tree_left' => 'integer',
-                        'Trees.tree_right' => 'integer',
-                    ],
-                )
-                ->orderBy(['Trees.tree_left' => 'DESC'])
-                ->toArray();
+        if ($node === null) {
+            return [];
         }
+
+        $query = $Trees->find('ancestors', ['for' => $node->id]);
+        $level = $Trees->getAssociation('Descendants')->junction()->aliasField(AdjacencyListBehavior::CTE_FIELD_LEVEL);
+        $permission = $query
+            ->disableHydration()
+            ->innerJoinWith('Objects.Permissions.Roles')
+            ->select([
+                'level' => $level,
+                'name' => 'Roles.name',
+            ])
+            ->orderByAsc($level)
+            ->toArray();
 
         if (empty($permission)) {
             return [];
         }
 
-        $roles = current(Hash::combine($permission, '{n}.name', '{n}.name', '{n}.tree_left'));
+        $roles = current(Hash::combine($permission, '{n}.name', '{n}.name', '{n}.level'));
 
         return ['roles' => array_values($roles), 'inherited' => true];
     }
@@ -129,25 +122,19 @@ class Folder extends ObjectEntity
         }
 
         $Trees = TableRegistry::getTableLocator()->get('Trees');
-        $descendantPermitted = $Trees->selectQuery()
+        /** @var \BEdita\Core\Model\Entity\Tree|null $node */
+        $node = $Trees->find()->where(['object_id' => $this->id])->first();
+        if ($node === null) {
+            return false;
+        }
+
+        $descendantPermitted = $Trees->find('descendants', ['for' => $node->id])
             ->disableHydration()
+            ->innerJoinWith(
+                'Objects.Permissions',
+                fn (Query $q): Query => $q->where(['Permissions.role_id IN' => $roleIds]),
+            )
             ->select(['existing' => 1])
-            ->from(['t1' => 'trees'], true)
-            ->innerJoin(
-                ['t2' => 'trees'],
-                [
-                    't2.tree_left > t1.tree_left',
-                    't2.tree_right < t1.tree_right',
-                ],
-            )
-            ->innerJoin(
-                ['op' => 'object_permissions'],
-                [
-                    'op.object_id = t2.object_id',
-                    'op.role_id IN' => Hash::extract($user, 'roles.{n}.id'),
-                ],
-            )
-            ->where(['t1.object_id' => $this->id], ['t1.object_id' => 'integer'])
             ->limit(1)
             ->first();
 
@@ -299,9 +286,15 @@ class Folder extends ObjectEntity
         }
 
         try {
-            return TableRegistry::getTableLocator()->get('Trees')
-                ->find('pathNodes', objectId: $this->id)
-                ->select(['id' => 'object_id', 'menu', 'params', 'slug'], true)
+            $Trees = TableRegistry::getTableLocator()->get('Trees');
+
+            return $Trees->find('pathNodes', objectId: $this->id)
+                ->select([
+                    'id' => $Trees->aliasField('object_id'),
+                    $Trees->aliasField('menu'),
+                    $Trees->aliasField('params'),
+                    $Trees->aliasField('slug'),
+                ], true)
                 ->disableHydration()
                 ->toArray();
         } catch (RecordNotFoundException $previous) {

plugins/BEdita/Core/src/Model/Table/FoldersTable.php
@@ -59,7 +59,7 @@ class FoldersTable extends ObjectsTable
             'targetForeignKey' => 'object_id',
             'finder' => 'available',
             'sort' => [
-                'Trees.tree_left' => 'asc',
+                'Trees.priority' => 'asc',
             ],
             'cascadeCallbacks' => true,
         ]);
@@ -149,12 +149,12 @@ class FoldersTable extends ObjectsTable
             ->firstOrFail();
 
         $deletedParents = $this->find()
-            ->innerJoinWith('TreeNodes', function (SelectQuery $query) use ($node) {
-                return $query->where(function (QueryExpression $exp) use ($node) {
-                    return $exp
-                        ->lt($this->TreeNodes->aliasField('tree_left'), $node->get('tree_left'))
-                        ->gt($this->TreeNodes->aliasField('tree_right'), $node->get('tree_right'));
-                });
+            ->where(function (QueryExpression $exp) use ($node): QueryExpression {
+                return $exp->in(
+                    $this->aliasField('id'),
+                    $this->TreeNodes->find('ancestors', ['for' => $node->id])
+                        ->select(['object_id']),
+                );
             })
             ->where([$this->aliasField('deleted') => true])
             ->count();
@@ -277,7 +277,7 @@ class FoldersTable extends ObjectsTable
                     return $exp->isNull($this->TreeNodes->aliasField('parent_id'));
                 });
             })
-            ->orderBy('TreeNodes.tree_left');
+            ->orderBy('TreeNodes.priority');
     }
 
     /**
@@ -302,13 +302,8 @@ class FoldersTable extends ObjectsTable
             ->firstOrFail();
 
         $descendantsToUpdate = $this->TreeNodes
-            ->find()
-            ->select(['object_id'])
-            ->where(function (QueryExpression $exp) use ($parentNode) {
-                return $exp
-                    ->gt($this->TreeNodes->aliasField('tree_left'), $parentNode->get('tree_left'))
-                    ->lt($this->TreeNodes->aliasField('tree_right'), $parentNode->get('tree_right'));
-            });
+            ->find('descendants', ['for' => $parentNode->id])
+            ->select(['object_id']);
 
         // Update deleted field of descendants
         /** @var \BEdita\Core\Model\Behavior\ObjectTypeBehavior $objectTypeBehavior */
@@ -368,7 +363,7 @@ class FoldersTable extends ObjectsTable
             default:
                 Log::warning(sprintf('Malformed property `children_order` "%s" for children sorting of folder #%d, defaulting to tree position', $order, $folder->id));
 
-                return [$this->Children->junction()->aliasField('tree_left') => 'ASC'];
+                return [$this->Children->junction()->aliasField('priority') => 'ASC'];
         }
     }
 }

plugins/BEdita/Core/src/Model/Table/ObjectsTable.php
@@ -419,13 +419,8 @@ class ObjectsTable extends Table
         return $query->where(function (QueryExpression $exp) use ($parentNode): QueryExpression {
             return $exp->in(
                 $this->aliasField('id'),
-                $this->TreeNodes->find()
-                    ->select(['object_id'])
-                    ->where(function (QueryExpression $exp) use ($parentNode) {
-                        return $exp
-                            ->gt($this->TreeNodes->aliasField('tree_left'), $parentNode->get('tree_left'))
-                            ->lt($this->TreeNodes->aliasField('tree_right'), $parentNode->get('tree_right'));
-                    }),
+                $this->TreeNodes->find('descendants', ['for' => $parentNode->id])
+                    ->select(['object_id']),
             );
         });
     }
@@ -449,7 +444,7 @@ class ObjectsTable extends Table
                     $this->TreeNodes->aliasField('parent_id') => $parentId,
                 ]);
             })
-            ->orderBy($this->TreeNodes->aliasField('tree_left'));
+            ->orderBy($this->TreeNodes->aliasField('priority'));
     }
 
     /**

plugins/BEdita/Core/src/Model/Table/TreesTable.php
@@ -16,10 +16,10 @@ namespace BEdita\Core\Model\Table;
 
 use ArrayObject;
 use BEdita\Core\Exception\LockedResourceException;
+use BEdita\Core\Model\Behavior\AdjacencyListBehavior;
 use BEdita\Core\Model\Entity\Tree;
 use BEdita\Core\Model\Validation\Validation;
 use Cake\Core\Configure;
-use Cake\Database\Driver\Mysql;
 use Cake\Database\Expression\QueryExpression;
 use Cake\Datasource\EntityInterface;
 use Cake\Event\EventInterface;
@@ -31,6 +31,7 @@ use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Utility\Text;
 use Cake\Validation\Validator;
+use RuntimeException;
 use stdClass;
 
 /**
@@ -99,7 +100,7 @@ class TreesTable extends Table
         ]);
 
         $this->addBehavior('BEdita/Core.AdjacencyList', [
-            'parentAssociation' => 'ParentObjects',
+            'parentAssociation' => 'ParentNode',
         ]);
         $this->addBehavior('BEdita/Core.Priority', ['fields' => ['priority' => ['scope' => ['parent_id']]]]);
     }
@@ -266,13 +267,19 @@ class TreesTable extends Table
             $position = $entity->get('position');
             $parentId = $entity->get('parent_id');
 
-            $siblings = $this->find()->where(fn (QueryExpression $exp): QueryExpression => $exp->eq($this->aliasField('parent_id'), $parentId));
+            $siblings = $this->find()->where(
+                fn (QueryExpression $exp): QueryExpression => $parentId === null
+                    ? $exp->isNull($this->aliasField('parent_id'))
+                    : $exp->eq($this->aliasField('parent_id'), $parentId),
+            );
             if (!$entity->isNew()) {
-                $siblings = $siblings->where(fn (QueryExpression $exp): QueryExpression => $exp->notEq($this->aliasField('id'), $entity->id));
+                $siblings = $siblings->where(
+                    fn (QueryExpression $exp): QueryExpression => $exp->notEq($this->aliasField('id'), $entity->id),
+                );
             }
 
             $count = $siblings->count();
-            $max = $entity->isNew() ? $count + 1 : max(1, $count);
+            $max = $count + 1;
 
             if ($position === 'first') {
                 $priority = 1;
@@ -300,6 +307,10 @@ class TreesTable extends Table
      */
     public function beforeSave(EventInterface $event, Tree $entity)
     {
+        if ($entity->get('parent_id') === $entity->get('object_id')) {
+            throw new RuntimeException('Cannot set a folder as its own parent');
+        }
+
         if ($entity->isNew() || !$entity->isDirty('parent_id')) {
             return;
         }
@@ -409,30 +420,16 @@ class TreesTable extends Table
      */
     protected function findPathNodes(SelectQuery $query, int $objectId): SelectQuery
     {
-        $lft = $this->aliasField('tree_left');
-        $rgt = $this->aliasField('tree_right');
         $node = $this->find()
-            ->select([$lft, $rgt])
-            ->where(['object_id' => $objectId])
+            ->select([$this->aliasField('id')])
+            ->where([$this->aliasField('object_id') => $objectId])
             ->disableHydration()
             ->firstOrFail();
 
-        $index = 'trees_nsm_idx';
-        if ($query->getConnection()->getDriver() instanceof Mysql && in_array($index, $this->getSchema()->indexes())) {
-            $query = $query->modifier([sprintf(
-                '/*+ INDEX(%s %s) */',
-                $this->getAlias(),
-                $index,
-            )]);
-        }
+        $query = $query->find('ancestors', ['for' => $node['id'], 'includeSelf' => true]);
+        $level = $this->getAssociation('Descendants')->junction()->aliasField(AdjacencyListBehavior::CTE_FIELD_LEVEL);
 
-        return $query
-            ->where(
-                fn(QueryExpression $exp): QueryExpression => $exp
-                    ->lte($lft, $node['tree_left'])
-                    ->gte($rgt, $node['tree_right']),
-            )
-            ->orderByAsc($lft);
+        return $query->orderByDesc($level);
     }
 
     /**